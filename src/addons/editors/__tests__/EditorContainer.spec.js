eafe0020fcce1db6f042a1d5f6786190ecd60450
malonecj <malonecj@gmail.com>
2015-04-13 18:32:19 +0100

    start of keyboard events tests

--- src/addons/editors/__tests__/EditorContainer.spec.js
+++ src/addons/editors/__tests__/EditorContainer.spec.js
@@ -13,58 +13,76 @@ describe('Editor Container Tests', () => {
     width : 100
   };
 
-  beforeEach(() => {
-
-
-    var cellMetaData = {
-      selected : {
-        idx : 0,
-        rowIdx :0
-      }
+  var cellMetaData = {
+    selected : {
+      idx : 0,
+      rowIdx :0
     }
-    var rowData={
-      col1 : 'I',
-      col2 : 'love',
-      col3 : 'Testing'
-    }
+  };
 
-    component = TestUtils.renderIntoDocument(<EditorContainer
-      rowData={rowData}
-      value={'Adwolf'}
-      cellMetaData={cellMetaData}
-      column={fakeColumn}
-      height={50}/>);
+  var rowData={
+    col1 : 'I',
+    col2 : 'love',
+    col3 : 'Testing'
+  }
+
+  describe("Basic render tests", () => {
+    beforeEach(() => {
+
+      component = TestUtils.renderIntoDocument(<EditorContainer
+        rowData={rowData}
+        value={'Adwolf'}
+        cellMetaData={cellMetaData}
+        column={fakeColumn}
+        height={50}/>);
+      });
+
+      it('should create a new EditorContainer instance', () => {
+        expect(component).toBeDefined();
+      });
+
+      it('should render a simpleTextEditor if no column.editor property', () => {
+        var Editor = TestUtils.findRenderedComponentWithType(component, SimpleTextEditor)
+        expect(Editor).toBeDefined();
+      });
+
+      it('should select the text of the default input when the editor is rendered', () => {
+
+        function isTextSelected(input) {
+          if (typeof input.selectionStart == "number") {
+            return input.selectionStart == 0 && input.selectionEnd == input.value.length;
+          } else if (typeof document.selection != "undefined") {
+            input.focus();
+            return document.selection.createRange().text == input.value;
+          }
+        }
+
+        var Editor = TestUtils.findRenderedComponentWithType(component, SimpleTextEditor)
+        expect(isTextSelected(Editor.getInputNode())).toBeDefined();
+      });
+
+      it('should render the editor with the correct properties', () => {
+        var Editor = TestUtils.findRenderedComponentWithType(component, SimpleTextEditor)
+        expect(Editor.props.value).toEqual('Adwolf');
+        expect(Editor.props.column).toEqual(fakeColumn);
+      });
+
+
   });
 
-  it('should create a new EditorContainer instance', () => {
-    expect(component).toBeDefined();
-  });
+  describe("keyboard events", () => {
 
-  it('should render a simpleTextEditor if no column.editor property', () => {
-    var Editor = TestUtils.findRenderedComponentWithType(component, SimpleTextEditor)
-    expect(Editor).toBeDefined();
-  });
+      beforeEach(() => {
+        cellMetaData.onCommit = function(value){};
+        spyOn(cellMetaData, 'onCommit');
+      });
 
-  it('should select the text of the default input when the editor is rendered', () => {
-
-    function isTextSelected(input) {
-      if (typeof input.selectionStart == "number") {
-        return input.selectionStart == 0 && input.selectionEnd == input.value.length;
-      } else if (typeof document.selection != "undefined") {
-        input.focus();
-        return document.selection.createRange().text == input.value;
-      }
-    }
-
-    var Editor = TestUtils.findRenderedComponentWithType(component, SimpleTextEditor)
-    expect(isTextSelected(Editor.getInputNode())).toBeDefined();
-  });
-
-  it('should render the editor with the correct properties', () => {
-    var Editor = TestUtils.findRenderedComponentWithType(component, SimpleTextEditor)
-    expect(Editor.props.value).toEqual('Adwolf');
-    expect(Editor.props.column).toEqual(fakeColumn);
-  });
+      it('hitting enter should call commit of cellMetaData', () => {
+        var Editor = TestUtils.findRenderedComponentWithType(component, SimpleTextEditor)
+        TestUtils.Simulate.keyDown(Editor.getDOMNode(), {key: "Enter"});
+        expect(cellMetaData.onCommit).toHaveBeenCalled();
+      });
+  })
 
 
 
