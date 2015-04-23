4ae46d5fd8dfe173c5675940d008c8d982e79693
malonecj <malonecj@gmail.com>
2015-04-23 12:17:59 +0100

    adding more columns to each example

--- examples/scripts/example03-fixed-cols.js
+++ examples/scripts/example03-fixed-cols.js
@@ -3,64 +3,75 @@ var QuickStartDescription = require('../components/QuickStartDescription')
 var ReactPlayground       = require('../assets/js/ReactPlayground');
 
 var FixedExample = `
+
+//helper to generate a random date
+function randomDate(start, end) {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
+};
+
 var _rows = [];
 for (var i = 1; i < 1000; i++) {
   _rows.push({
     id: i,
-    title: 'Title ' + i,
-    count: i * 1000,
-    firstName : 'Johnny' + i
+    task: 'Task ' + i,
+    complete: Math.min(100, Math.round(Math.random() * 110)),
+    priority : ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
+    issueType : ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
+    startDate: randomDate(new Date(2015, 3, 1), new Date()),
+    completeDate: randomDate(new Date(), new Date(2016, 0, 1))
   });
-}
+};
 
+//function to retrieve a row for a given index
 var rowGetter = function(i){
   return _rows[i];
 };
 
 
+//Columns definition
 var columns = [
 {
   key: 'id',
   name: 'ID',
-  locked : true,
-  width : 200
+  locked : true
 },
 {
-  key: 'title',
+  key: 'task',
   name: 'Title',
-  width : 200
+  width: 200
 },
 {
-  key: 'count',
-  name: 'Count',
-  width : 200
+  key: 'priority',
+  name: 'Priority',
+  width: 200
 },
 {
-  key: 'firstName',
-  name: 'First Name',
-  width : 200
+  key: 'issueType',
+  name: 'Issue Type',
+  width: 200
 },
 {
-  key: 'lastName',
-  name: 'Last Name',
-  width : 200
+  key: 'complete',
+  name: '% Complete',
+  width: 200
 },
 {
-  key: 'email',
-  name: 'Email',
-  width : 200
+  key: 'startDate',
+  name: 'Start Date',
+  width: 200
 },
 {
-  key: 'street',
-  name: 'Street',
-  width : 100
+  key: 'completeDate',
+  name: 'Expected Complete',
+  width: 200
 },
 {
-  key: 'zipCode',
-  name: 'ZipCode',
-  width : 100
-},
-];
+  key: 'completeDate',
+  name: 'Expected Complete',
+  width: 200
+}
+]
+
 
 React.render(<ReactDataGrid
   columns={columns}
