c4b1c5e14f98b3435097abf0a233045e5ddc5c50
malonecj <malonecj@gmail.com>
2015-06-30 16:50:18 +0100

    remove ref to bower in docs

--- examples/scripts/documentation01-gettingstarted.js
+++ examples/scripts/documentation01-gettingstarted.js
@@ -21,9 +21,7 @@ module.exports = React.createClass({
         </div>
         <br/>
         <h4>Using Distribution Scripts</h4>
-        <p>If you prefer not to use a module system, you can reference the distribution scripts directly in your html pages. First you need to download the scripts. This can be done in 3 ways, either download directly from github source, using npm as above or using bower
-          <br/>
-          <kbd>bower install react-data-grid --save</kbd>
+        <p>If you prefer not to use a module system, you can reference the distribution scripts directly in your html pages. First you need to download the scripts. This can be done in 3 ways, either download directly from github source, using npm as above 
         </p>
         <div className="code-block js">
           <pre>&lt;script src="//fb.me/react-0.12.0.js"&gt;&lt;/script&gt;<br/>&lt;script type="text/javascript" src="react-data-grid/dist/react-data-grid.js"&gt;&lt;/script&gt;</pre>
