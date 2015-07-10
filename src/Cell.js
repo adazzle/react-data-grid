679275d4dc97fad9a544f6e6a508a9e171433b6c
Steve Baker <Steven.Baker@adazzle.com>
2015-07-10 17:56:24 +0100

    make simple formatter faster

--- src/Cell.js
+++ src/Cell.js
@@ -291,14 +291,17 @@ var Cell = React.createClass({
 });
 
 var SimpleCellFormatter = React.createClass({
-
   propTypes : {
     value :  React.PropTypes.oneOfType([React.PropTypes.string,React.PropTypes.number, React.PropTypes.object, React.PropTypes.bool]).isRequired
   },
 
   render(): ?ReactElement{
     return <span>{this.props.value}</span>
+  },
+  shouldComponentUpdate(nextProps: any, nextState: any): boolean {
+      return nextProps.value === this.props.value;
   }
+
 })
 
 module.exports = Cell;
