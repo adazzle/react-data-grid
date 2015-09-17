a30b44b9b2a80948676efb13c46587aa58cce9e0
malonecj@gmail.com <malonecj@gmail.com>
2015-09-17 01:52:39 +0100

    decrypt file in publish scripts

--- ci/publish/publishMaster.ps1
+++ ci/publish/publishMaster.ps1
@@ -1,3 +1,5 @@
+nuget install secure-file -ExcludeVersion
+secure-file\tools\secure-file -decrypt .npmrc.enc -secret %npmrc_token%
 Write-Host "Merging to master. Attempting to bump version"
 git config --global credential.helper store
 git config --global push.default simple
