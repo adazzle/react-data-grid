c7c62a694caa4d223c9610d43cca1bc27ee789c6
Michael Boreham <michael.boreham@aba-systems.com.au>
2015-09-29 16:49:35 +1000

    fixed header height assumptions

--- src/Header.js
+++ src/Header.js
@@ -62,7 +62,7 @@ var Header = React.createClass({
     this.props.headerRows.forEach((function(row, index){
       var headerRowStyle = {
         position: 'absolute',
-        top: this.props.height * index,
+        top: this.getCombinedHeaderHeights(index),
         left: 0,
         width: this.props.totalWidth,
         overflow : 'hidden'
@@ -151,10 +151,22 @@ var Header = React.createClass({
     this.refs.row.setScrollLeft(scrollLeft);
   },
 
+  getCombinedHeaderHeights(until: ?number): number {
+    var stop_at = this.props.headerRows.length;
+    if (typeof until != 'undefined')
+      stop_at = until;
+
+    var height = 0;
+    for (var index = 0; index < until || this.props.headerRows.length; index++) {
+      height += this.props.headerRows[index].height || this.props.height;
+    }
+    return height;
+  },
+
   getStyle(): {position: string; height: number} {
     return {
       position: 'relative',
-      height: this.props.height * this.props.headerRows.length,
+      height: this.getCombinedHeaderHeights(),
       overflow : 'hidden'
     };
   },
