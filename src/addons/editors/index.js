720fed3166ca6ed9b4b6e6baadec3ac58a68a46b
Michael Stock <mikeastock@gmail.com>
2015-10-06 09:52:55 -0700

    Export DateRangeEditor
    Add DateRangeEditor to editors index so it can be imported and used

--- src/addons/editors/index.js
+++ src/addons/editors/index.js
@@ -3,7 +3,8 @@ var Editors = {
   AutoComplete: require("./AutoCompleteEditor"),
   DropDownEditor: require("./DropDownEditor"),
   SimpleTextEditor: require("./SimpleTextEditor"),
-  CheckboxEditor: require("./CheckboxEditor")
+  CheckboxEditor: require("./CheckboxEditor"),
+  DateRangeEditor: require("./DateRangeEditor")
 };
 
 module.exports = Editors;
