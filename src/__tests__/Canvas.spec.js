92c1fc90fc3e0c4c11f53de213c21422019039eb
Steve Baker <Steven.Baker@adazzle.com>
2015-12-03 17:06:54 +0000

    test for canvas

---
+++ src/__tests__/Canvas.spec.js
@@ -0,0 +1,42 @@
+'use strict';
+var React        = require('react');
+var rewire       = require('rewire');
+var Canvas         = rewire('../Canvas');
+var TestUtils    = require('react/lib/ReactTestUtils');
+var rewireModule = require("../../test/rewireModule");
+var StubComponent = require("../../test/StubComponent");
+
+describe('Canvas Tests', () => {
+  var testElement;
+  // Configure local variable replacements for the module.
+  rewireModule(Canvas, {
+
+  });
+
+
+
+  var testProps = {
+    rowHeight: 25,
+    height: 200,
+    displayStart: 1,
+    displayEnd: 10,
+    rowsCount: 1,
+    columns: []
+  }
+
+  beforeEach(() => {
+    testElement = TestUtils.renderIntoDocument(<Canvas {...testProps}/>);
+  });
+
+  it('should create a new instance of Cell', () => {
+    expect(testElement).toBeDefined();
+  });
+
+  it('Should not call setScroll on render', () => {
+    spyOn(Canvas, 'setScrollLeft');
+    testElement = TestUtils.renderIntoDocument(<Canvas {...testProps}/>);
+    expect(Canvas.setScrollLeft).not.toHaveBeenCalled();
+  });
+
+
+});
