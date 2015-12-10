8ec20a7bbd8bb3a63aed78186e93950cc6c1c289
Ivan Palamarchuk <i.delef@gmail.com>
2015-12-10 15:36:09 +0300

    change render options (fix)

--- src/addons/editors/DropDownEditor.js
+++ src/addons/editors/DropDownEditor.js
@@ -32,8 +32,12 @@ class DropDownEditor extends EditorBase {
 
   renderOptions(): Array<ReactElement>{
     var options = [];
-    this.props.options.forEach(function(name){
-      options.push(<option key={name} value={name}  >{name}</option>);
+    this.props.options.forEach(function(name, value){
+      if (typeof value !== 'undefined' && value !== null) {
+        options.push(<option key={name} value={value}  >{name}</option>);
+      } else {
+        options.push(<option key={name} value={name}  >{name}</option>);
+      }
     }, this);
     return options;
   }
