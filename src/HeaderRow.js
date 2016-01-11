460709eb299b34aa0f9dcaec361f2eee81d5ca76
jpdriver <JP@JPs-MacBook-Pro.local>
2016-01-11 08:44:48 +0000

    switch shallowEqual to use shallowCloneObject
    shallowEqual is still available as a separate package. since we have
    shallowCloneObject already i figured we could just use that?

--- src/HeaderRow.js
+++ src/HeaderRow.js
@@ -6,7 +6,7 @@
 
 var React             = require('react');
 var PropTypes         = React.PropTypes;
-var shallowEqual      = require('react/lib//shallowEqual');
+var shallowCloneObject = require('./shallowCloneObject');
 var HeaderCell        = require('./HeaderCell');
 var getScrollbarSize  = require('./getScrollbarSize');
 var ExcelColumn  = require('./addons/grids/ExcelColumn');
@@ -111,7 +111,7 @@ var HeaderRow = React.createClass({
       nextProps.width !== this.props.width
       || nextProps.height !== this.props.height
       || nextProps.columns !== this.props.columns
-      || !shallowEqual(nextProps.style, this.props.style)
+      || shallowCloneObject(nextProps.style) !== shallowCloneObject(this.props.style)
       || this.props.sortColumn != nextProps.sortColumn
       || this.props.sortDirection != nextProps.sortDirection
     );
