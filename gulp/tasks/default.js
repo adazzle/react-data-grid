5da97f5b4e968be8a9effa271d3ddf1de02f49a7
Diogo <diogofncunha@gmail.com>
2016-01-21 17:09:46 +0000

    Added gulp task to build before testing locally
    Added gulp task to build before testing locally, also changed the old
    test task so that the default task doesn't build 2 times.

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 
-gulp.task('default', ['clean', 'test', 'eslint'], function(){
+gulp.task('default', ['clean', 'buildTest', 'eslint'], function() {
   gulp.start('watch');
 });
