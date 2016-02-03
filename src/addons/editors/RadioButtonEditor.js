7c50321b8f975840cbe6dc418fb7d21ce657d23e
momaci <muhamet.maci@adazzle.com>
2016-02-03 15:01:26 +0000

    add radio button editor

--- src/addons/editors/RadioButtonEditor.js
+++ src/addons/editors/RadioButtonEditor.js
@@ -6,7 +6,7 @@
 
 var React = require('react');
 
-var CheckboxEditor = React.createClass({
+var RadioButtonEditor = React.createClass({
 
   PropTypes : {
     value : React.PropTypes.bool.isRequired,
@@ -19,7 +19,7 @@ var CheckboxEditor = React.createClass({
 
   render(): ? ReactElement {
     var checked = this.props.value != null ? this.props.value : false;
-    return (<input className="react-grid-CheckBox" type="checkbox" checked={checked} onClick={this.handleChange} />);
+    return (<input className="react-grid-radio-button radio" type="radio" checked={checked} onClick={this.handleChange} />);
   },
 
   handleChange(e: Event){
@@ -27,4 +27,4 @@ var CheckboxEditor = React.createClass({
   }
 });
 
-module.exports = CheckboxEditor;
+module.exports = RadioButtonEditor;
