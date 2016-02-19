595c61015ef1abda8d096945d7ba6bb2e681f12b
jpdriver <jp.driver@adazzle.com>
2016-02-19 13:41:47 +0000

    add ReactDOM to AutoCompleteEditor

--- src/addons/editors/AutoCompleteEditor.js
+++ src/addons/editors/AutoCompleteEditor.js
@@ -1,4 +1,5 @@
 const React                   = require('react');
+const ReactDOM                = require('react-dom');
 const ReactAutocomplete       = require('ron-react-autocomplete');
 const ExcelColumn             = require('../grids/ExcelColumn');
 
