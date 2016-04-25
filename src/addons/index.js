3ff3a7c69c335a97899ab3647d921fac2f2c2414
willgates <willem.oomen@gmail.com>
2016-04-25 15:37:03 +0100

    Add react-contextmenu components to addons

--- src/addons/index.js
+++ src/addons/index.js
@@ -1,6 +1,8 @@
-/* @flow */
+import {ContextMenu, ContextMenuLayer, MenuItem, monitor, SubMenu, connect} from 'react-contextmenu';
+
 module.exports = require('./grids/ReactDataGrid');
 module.exports.Editors = require('./editors');
 module.exports.Formatters = require('./formatters');
 module.exports.Toolbar = require('./toolbars/Toolbar');
 module.exports.Row = require('../Row');
+module.exports.Menu = {ContextMenu, ContextMenuLayer, MenuItem, monitor, SubMenu, connect};
