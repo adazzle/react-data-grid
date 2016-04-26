ba9fae5341f5a29f8a37237d3bc65112dd885887
willgates <willem.oomen@gmail.com>
2016-04-26 10:36:57 +0100

    Add new ExampleList component and have all examples in separate file

---
+++ examples/examplesData.js
@@ -0,0 +1,21 @@
+const ExamplesData = [
+  {hashLocation: 'basic', name: 'Basic Example'},
+  {hashLocation: 'resizable', name: 'Resizable Columns'},
+  {hashLocation: 'fixed', name: 'Frozen Columns'},
+  {hashLocation: 'single-row-select', name: 'Single Row Selection'},
+  {hashLocation: 'multi-row-select', name: 'Multiple Row Selection'},
+  {hashLocation: 'editable', name: 'Editable Grid'},
+  {hashLocation: 'formatters', name: 'Custom Formatters'},
+  {hashLocation: 'cell-drag', name: 'Cell drag down/Fill Column'},
+  {hashLocation: 'editors', name: 'Rich Cell Editors'},
+  {hashLocation: 'million-rows', name: 'One Million Rows'},
+  {hashLocation: 'sortable', name: 'Sortable Grid'},
+  {hashLocation: 'filterable', name: 'Filterable Grid'},
+  {hashLocation: 'filterable-sortable', name: 'Filterable Sortable Grid'},
+  {hashLocation: 'immutable-data', name: 'Immutable Data Grid'},
+  {hashLocation: 'custom-row-renderer', name: 'Custom Row Render'},
+  {hashLocation: 'empty-rows', name: 'Empty Rows'},
+  {hashLocation: 'all-the-features', name: 'All-The-Features Grid'},
+  {hashLocation: 'all-features-immutable', name: 'All-The-Features with Immutable Data'}
+];
+export default ExamplesData;
