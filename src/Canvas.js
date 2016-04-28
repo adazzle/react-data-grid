cd178e584f17697fcc13d035f7c7b5ac1f0a4019
Anis Nouira <darkoleptik@gmail.com>
2016-04-29 01:20:14 +0200

    this.getDOMNode() replaced by findDOMNode(this)

--- src/Canvas.js
+++ src/Canvas.js
@@ -106,7 +106,7 @@ const Canvas = React.createClass({
   },
 
   onScroll(e: any) {
-    if (this.getDOMNode() !== e.target) {
+    if (ReactDOM.findDOMNode(this) !== e.target) {
       return;
     }
     this.appendScrollShim();
