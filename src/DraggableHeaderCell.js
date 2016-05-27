18cdff965e3f722696b97512de84d85f82da6415
malonecj@gmail.com <malonecj@gmail.com>
2016-05-27 15:40:07 +0100

    Styles and images

--- src/DraggableHeaderCell.js
+++ src/DraggableHeaderCell.js
@@ -2,9 +2,18 @@ import {DragItemTypes} from './Constants';
 import { DragSource } from 'react-dnd';
 import HeaderCell from './HeaderCell';
 import React, {PropTypes, Component} from 'react';
-import ReactDOM from 'react-dom';
 
 class DraggableHeaderCell extends Component {
+
+  componentDidMount() {
+    let connectDragPreview = this.props.connectDragPreview;
+    let img = new Image();
+    img.src = './assets/images/drag_column_full.png';
+    img.onload = function() {
+      connectDragPreview(img);
+    };
+  }
+
   setScrollLeft(scrollLeft) {
     let node = ReactDOM.findDOMNode(this);
     node.style.webkitTransform = `translate3d(${scrollLeft}px, 0px, 0px)`;
@@ -12,15 +21,18 @@ class DraggableHeaderCell extends Component {
   }
 
   render() {
-    const { connectDragSource, connectDragPreview} = this.props;
-    connectDragPreview(<span>dsfdsfds</span>);
+    const { connectDragSource, isDragging} = this.props;
+    if (isDragging) {
+      return null;
+    }
     return connectDragSource(<div style={{cursor: 'move'}}><HeaderCell {...this.props}/></div>);
   }
 }
 
 DraggableHeaderCell.propTypes = {
-  connectDragSource: PropTypes.func,
-  connectDragPreview: PropTypes.func
+  connectDragSource: PropTypes.func.isRequired,
+  connectDragPreview: PropTypes.func.isRequired,
+  isDragging: PropTypes.bool.isRequired
 };
 
 function collect(connect, monitor) {
