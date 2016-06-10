d54c9f74851db686bcdd5e9e4be77ad88c9d8685
malonecj@gmail.com <malonecj@gmail.com>
2016-06-10 11:02:45 +0100

    Move ReactDataGrid component to root of src directory

--- src/addons/index.js
+++ src/addons/index.js
@@ -1,4 +1,3 @@
-module.exports = require('./grids/ReactDataGrid');
 module.exports.Editors = require('./editors');
 module.exports.Formatters = require('./formatters');
 module.exports.Toolbar = require('./toolbars/Toolbar');
