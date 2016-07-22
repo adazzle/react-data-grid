9bcfc19172dce4b07a629a5209351372e30d266a
Diego Murakami <diego.murakami@hotmail.com>
2016-07-22 15:31:13 +0100

    Add Empty lines filtering for AutoCompleteFilter

--- src/addons/cells/headerCells/filters/AutoCompleteFilterableHeaderCell.js
+++ src/addons/cells/headerCells/filters/AutoCompleteFilterableHeaderCell.js
@@ -33,7 +33,7 @@ class AutoCompleteFilterableHeaderCell extends React.Component {
       include = false;
     } else if (!isEmptyArray(columnFilter.filterTerm)) {
       for (let prop of columnFilter.filterTerm) {
-        if (row[columnKey].trim().toLowerCase().indexOf(prop.value.trim().toLowerCase()) === -1) {
+        if (row[columnKey].trim().toLowerCase().indexOf(prop.value.trim().toLowerCase()) === -1 || row[columnKey] !== prop.value) {
           include = false;
         } else {
           include = true;
