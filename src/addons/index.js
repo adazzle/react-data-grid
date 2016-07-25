f6c22c8f160a2a314450d581e152269ccb2b146d
Diego Murakami <diego.murakami@hotmail.com>
2016-07-25 10:01:25 +0100

    Fix addons class name

--- src/addons/index.js
+++ src/addons/index.js
@@ -5,7 +5,7 @@ const ToolsPanel = require('./toolbars');
 const Data = require('./data/');
 const Menu = require('./menu');
 import Draggable from './draggable';
-const CustomFilters = require('./cells/headerCells/filters');
+const Filters = require('./cells/headerCells/filters');
 
-window.ReactDataGridPlugins = {Editors, Formatters, Toolbar, Menu, Data, ToolsPanel, Draggable, CustomFilters};
-export {Editors, Formatters, Toolbar, Menu, Data, ToolsPanel, Draggable, CustomFilters};
+window.ReactDataGridPlugins = {Editors, Formatters, Toolbar, Menu, Data, ToolsPanel, Draggable, Filters};
+export {Editors, Formatters, Toolbar, Menu, Data, ToolsPanel, Draggable, Filters};
