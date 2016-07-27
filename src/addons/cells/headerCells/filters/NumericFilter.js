73eed5a078ffbb593177c4efcfedc084134aa428
Diego Murakami <diego.murakami@hotmail.com>
2016-07-27 03:00:53 +0100

    Fix numeric filter and tests to use new rules

--- src/addons/cells/headerCells/filters/NumericFilter.js
+++ src/addons/cells/headerCells/filters/NumericFilter.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ExcelColumn from '../../../../PropTypeShapes/ExcelColumn';
+const RuleType = {
+  Number: 1,
+  Range: 2,
+  GreaterThen: 3,
+  LessThen: 4
+};
 
 class NumericFilter extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.getRows = this.getRows.bind(this);
-    this.getNumericValues = this.getNumericValues.bind(this);
-    this.state = this.getRows();
+    this.getRules = this.getRules.bind(this);
   }
 
   componentDidMount() {
@@ -17,10 +21,6 @@ class NumericFilter extends React.Component {
 
   componentDidUpdate() {
     this.attachTooltip();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState(this.getRows(nextProps));
   }
 
   attachTooltip() {
@@ -32,92 +32,69 @@ class NumericFilter extends React.Component {
   }
 
   filterValues(row, columnFilter, columnKey) {
+    if (columnFilter.filterTerm == null) {
+      return true;
+    }
+    let result = false;
     // implement default filter logic
     let value = parseInt(row[columnKey], 10);
-    if (columnFilter.filterTerm.indexOf(value) === -1) {
-      return false;
+    for (let rule of columnFilter.filterTerm) {
+      switch (rule.type) {
+      case RuleType.Number:
+        if (rule.value === value) {
+          result = true;
+        }
+        break;
+      case RuleType.GreaterThen:
+        if (rule.value <= value) {
+          result = true;
+        }
+        break;
+      case RuleType.LessThen:
+        if (rule.value >= value) {
+          result = true;
+        }
+        break;
+      case RuleType.Range:
+        if (rule.begin <= value && rule.end >= value ) {
+          result = true;
+        }
+        break;
+      default:
+        // do nothing
+        break;
+      }
     }
-    return true;
+    return result;
   }
 
-  getRows(nextProps) {
-    let props = nextProps || this.props;
-    let minValue = Number.MIN_VALUE;
-    let maxValue = Number.MAX_VALUE;
-    let allRowsValues = this.props.getValidFilterValues(props.column.key);
-    allRowsValues = allRowsValues.sort(function(a, b) { return a - b; });
-    for (let i = 0; i < allRowsValues.length; i++) {
-      let value = parseInt(allRowsValues[i], 10);
-      if (minValue > value) {
-        minValue = parseInt(value, 10);
-      }
-      if (maxValue < value) {
-        maxValue = parseInt(value, 10);
-      }
-      allRowsValues[i] = value;
+  getRules(value) {
+    let rules = [];
+    if (value === '') {
+      return rules;
     }
-    return { minValue, maxValue, allRowsValues };
-  }
-
-  getNumericValues(value) {
-    let returnList = [];
-    let greaterThenBegin = -1;
-    let lessThenBegin = -1;
-    let intersection = false;
     // check comma
     let list = value.split(',');
     if (list.length > 0) {
-      // check first greater and less values
-      for (let obj of list) {
-        if (obj.indexOf('>') > -1) {
-          greaterThenBegin = parseInt(obj.split('>')[1], 10);
-        }
-        if (obj.indexOf('<') > -1) {
-          lessThenBegin = parseInt(obj.split('<')[1], 10);
-        }
-      }
-      // check if we have an intersection
-      if (greaterThenBegin > 0 && lessThenBegin > 0 && greaterThenBegin < lessThenBegin) {
-        intersection = true;
-      }
       // handle each value with comma
       for (let obj of list) {
         if (obj.indexOf('-') > 0) { // handle dash
           let begin = parseInt(obj.split('-')[0], 10);
           let end = parseInt(obj.split('-')[1], 10);
-          for (let objValue of this.state.allRowsValues) {
-            if (begin <= objValue && objValue <= end) {
-              returnList.push(objValue);
-            }
-          }
+          rules.push({ type: RuleType.Range, begin: begin, end: end });
         } else if (obj.indexOf('>') > -1) { // handle greater then
           let begin = parseInt(obj.split('>')[1], 10);
-          let _maxValue = (greaterThenBegin > this.state.maxValue ? greaterThenBegin : this.state.maxValue);
-          for (let objValue of this.state.allRowsValues) {
-            if (begin <= objValue && objValue <= (intersection ? lessThenBegin : _maxValue)) {
-              returnList.push(objValue);
-            }
-          }
+          rules.push({ type: RuleType.GreaterThen, value: begin });
         } else if (obj.indexOf('<') > -1) { // handle less then
           let end = parseInt(obj.split('<')[1], 10);
-          let _minValue = (lessThenBegin < this.state.minValue ? lessThenBegin : this.state.minValue);
-          for (let objValue of this.state.allRowsValues) {
-            if ((intersection ? greaterThenBegin : _minValue) <= objValue && objValue <= end) {
-              returnList.push(objValue);
-            }
-          }
+          rules.push({ type: RuleType.LessThen, value: end });
         } else { // handle normal values
-          returnList.push(parseInt(obj, 10));
+          let numericValue = parseInt(obj, 10);
+          rules.push({ type: RuleType.Number, value: numericValue });
         }
       }
     }
-
-    // remove duplicates
-    returnList = returnList.filter((val, index, array) => { return array.indexOf(val) === index; });
-    // sort
-    returnList = returnList.sort(function(a, b) { return a - b; });
-
-    return returnList;
+    return rules;
   }
 
   handleKeyPress(e) { // Validate the input
@@ -130,7 +107,7 @@ class NumericFilter extends React.Component {
 
   handleChange(e) {
     let value = e.target.value;
-    let filters = this.getNumericValues(value);
+    let filters = this.getRules(value);
     this.props.onChange({filterTerm: (filters.length > 0 ? filters : null), column: this.props.column, rawValue: value, filterValues: this.filterValues });
   }
 
@@ -162,8 +139,6 @@ class NumericFilter extends React.Component {
 
 NumericFilter.propTypes = {
   onChange: React.PropTypes.func.isRequired,
-  column: React.PropTypes.shape(ExcelColumn),
-  getValidFilterValues: React.PropTypes.func
+  column: React.PropTypes.shape(ExcelColumn)
 };
-
 module.exports = NumericFilter;
