e4b39b745b1e8e9eaccdd2ce45235422fecc2a7f
diogofcunha <diogofncunha@gmail.com>
2016-08-05 11:29:12 +0100

    Added reference to rowGroupRenderer in grouping example

--- examples/scripts/example21-grouping.js
+++ examples/scripts/example21-grouping.js
@@ -207,6 +207,8 @@ module.exports = React.createClass({
         <p>This example demonstrates how to group rows by column name. Drag a column header to group rows by that column.</p>
         <p>To expand and close a row group, you can use either the mouse or keyboard</p>
         <p>Press <strong>Enter</strong> or <strong>Left Arrow</strong> or <strong>Right Arrow</strong> to toggle whether a row is expanded or not</p>
+        <p>This feature also supports a custom Renderer, by using a renderer you can render some fancy custom html in the row gorup.</p>
+        <p>To use a renderer just inject your component with <code>rowGroupRenderer</code> prop in the grid.</p>
         <ReactPlayground codeText={GroupingExample} />
       </div>
     )
