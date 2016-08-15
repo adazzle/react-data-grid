d721b803d8d72020f32679b909712acb1b0dcee0
malonecj <malonecj@gmail.com>
2016-08-15 11:36:18 +0100

    Compile less to css and output to dist dirctory

--- gulp/tasks/examples.js
+++ gulp/tasks/examples.js
@@ -9,69 +9,62 @@ var path = require("path");
 
 console.log(path.resolve(__dirname, '../../src'))
 webpackConfig = {
-	entry: {
-		'index': './examples/index.js',
-		'shared': './examples/shared.js',
-		'examples': './examples/examples.js',
-		'documentation': './examples/documentation.js'
-	},
-	output: {
-		path: path.join(__dirname, "../../examples/build"),
-		filename: "[name].js",
-		libraryTarget: "umd"
-	},
-	resolve: {
-		alias: {
-			react: path.resolve('./node_modules/react'),
-			'react-dom': path.resolve('./node_modules/react-dom'),
-			'react-data-grid': path.join(__dirname, "/../../dist/react-data-grid")
-		}
-	},
-	externals: {
-		"react": 'React',
+  entry: {
+    'index': './examples/index.js',
+    'shared': './examples/shared.js',
+    'examples': './examples/examples.js',
+    'documentation': './examples/documentation.js'
+  },
+  output: {
+    path: path.join(__dirname, "../../examples/build"),
+    filename: "[name].js",
+    libraryTarget: "umd"
+  },
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+      'react-data-grid': path.join(__dirname, "/../../dist/react-data-grid")
+    }
+  },
+  externals: {
+    "react": 'React',
     "react/addons": 'React',
     "react-dom": 'ReactDOM',
-		'faker': 'faker',
-		"moment": "moment"
-	},
-	module: {
-		loaders: [
-		    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-		  ]
-	},
-	plugins: []
+    'faker': 'faker',
+    "moment": "moment"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  plugins: []
 }
 
 
 gulp.task('script-deps', function () {
 
-	return gulp.src([
-		'node_modules/es5-shim/es5-shim.js'
-	])
-		.pipe(concat('libs.js'))
-		.pipe(gulp.dest('./examples/build'))
+  return gulp.src([
+    'node_modules/es5-shim/es5-shim.js'
+  ])
+    .pipe(concat('libs.js'))
+    .pipe(gulp.dest('./examples/build'))
 });
 
 
 gulp.task("copy-dist", ['dist'], function () {
-	//copy dist folder to examples
-	return gulp.src([
-		'dist/**',
-		])
-		.pipe(gulp.dest('./examples/build'))
+  //copy dist folder to examples
+  return gulp.src([
+    'dist/**',
+  ])
+    .pipe(gulp.dest('./examples/build'))
 });
 
 gulp.task('minify-examples', ['copy-dist']);
 
-// task
-gulp.task('styles', function () {
-	return gulp.src('./themes/react-data-grid.css')
-		.pipe(gulp.dest('./examples/build'));
-});
+gulp.task("examples", ['script-deps', 'minify-examples'], function (callback) {
 
-
-gulp.task("examples", ['script-deps', 'minify-examples', 'styles'], function (callback) {
-
-		// run webpack
-		bundle(Object.create(webpackConfig), callback);
+  // run webpack
+  bundle(Object.create(webpackConfig), callback);
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var del = require('del');
-gulp.task('watch', ['setWatch', 'browserSync'], function() {
+gulp.task('watch', ['setWatch', 'styles', 'browserSync'], function() {
 	gulp.watch('themes/**', ['styles']);
 	gulp.watch('src/**', ['examples']);
 
