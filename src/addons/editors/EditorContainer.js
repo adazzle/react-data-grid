6012bdfc26d616f2721ce1f4177a6ecf4db51ae2
Enzo Vicidomini <vincenzo.vicidomini@adazzle.com>
2016-09-20 11:54:02 +0100

    import fix

--- src/addons/editors/EditorContainer.js
+++ src/addons/editors/EditorContainer.js
@@ -1,4 +1,5 @@
 const React                   = require('react');
+const ReactDOM                = require('react-dom');
 const joinClasses              = require('classnames');
 const keyboardHandlerMixin    = require('../../KeyboardHandlerMixin');
 const SimpleTextEditor        = require('./SimpleTextEditor');
