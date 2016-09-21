1c35a918fa0dd34b80bf3deb7cd752858e37e0ea
Enzo Vicidomini <vincenzo.vicidomini@adazzle.com>
2016-09-21 14:50:14 +0100

    fixed commit condition

--- src/addons/editors/EditorContainer.js
+++ src/addons/editors/EditorContainer.js
@@ -227,7 +227,7 @@ const EditorContainer = React.createClass({
     e.stopPropagation();
     // commit if cliked anywhere outside editor
     // prevent commit if any element inside editor is clicked or if the active cell is clicked
-    if (!e.currentTarget.contains(e.relatedTarget) || !(e.relatedTarget.classList.contains('editing') && e.relatedTarget.classList.contains('react-grid-Cell'))) {
+    if (!e.currentTarget.contains(e.relatedTarget) && !(e.relatedTarget.classList.contains('editing') && e.relatedTarget.classList.contains('react-grid-Cell'))) {
       this.commit(e);
     }
   },
