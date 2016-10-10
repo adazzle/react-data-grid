3e812e7a11e50bc027ae8e7aaeee34bbcc495cf6
willgates <willem.oomen@gmail.com>
2016-10-10 17:47:04 +0100

    Return 0 as last option

--- src/addons/data/RowSorter.js
+++ src/addons/data/RowSorter.js
@@ -1,13 +1,12 @@
 const sortRows = (rows, sortColumn, sortDirection) => {
   let sortDirectionSign = sortDirection === 'ASC' ? 1 : -1;
   let comparer = (a, b) => {
-    if (a[sortColumn] === b[sortColumn]) {
-      return 0;
-    }
     if (a[sortColumn] > b[sortColumn]) {
       return sortDirectionSign;
+    } else if (a[sortColumn] < b[sortColumn]) {
+      return -sortDirectionSign;
     }
-    return -sortDirectionSign;
+    return 0;
   };
   if (sortDirection === 'NONE') {
     return rows;
