0c9b77f23a65a97ca7b3d179ba58aa7acf2d8712
willgates <willgates@users.noreply.github.com>
2016-10-11 11:41:35 +0100

    Fix slow row sorting (#489)
    * Fix slow sorting
    
    * Return 0 as last option
    
    * Test row sorting
    
    * Fix fdescribe
    
    * Extract comparer as separate function
    
    * Test RowSorter comparer

--- src/addons/data/RowSorter.js
+++ src/addons/data/RowSorter.js
@@ -1,15 +1,22 @@
+const comparer = (a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  }
+  return 0;
+};
+
 const sortRows = (rows, sortColumn, sortDirection) => {
-  let comparer = (a, b) => {
-    if (sortDirection === 'ASC') {
-      return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
-    } else if (sortDirection === 'DESC') {
-      return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
-    }
+  let sortDirectionSign = sortDirection === 'ASC' ? 1 : -1;
+  let rowComparer = (a, b) => {
+    return sortDirectionSign * comparer(a[sortColumn], b[sortColumn]);
   };
   if (sortDirection === 'NONE') {
     return rows;
   }
-  return rows.sort(comparer);
+  return rows.sort(rowComparer);
 };
 
 module.exports = sortRows;
+module.exports.comparer = comparer;
