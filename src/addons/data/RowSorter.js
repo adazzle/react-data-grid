662f103214e4f07c3a8d23e7a7b1676645ed4ea0
willgates <willem.oomen@gmail.com>
2016-10-11 11:27:07 +0100

    Extract comparer as separate function

--- src/addons/data/RowSorter.js
+++ src/addons/data/RowSorter.js
@@ -1,17 +1,22 @@
+const comparer = (a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a < b) {
+    return -1;
+  }
+  return 0;
+};
+
 const sortRows = (rows, sortColumn, sortDirection) => {
   let sortDirectionSign = sortDirection === 'ASC' ? 1 : -1;
-  let comparer = (a, b) => {
-    if (a[sortColumn] > b[sortColumn]) {
-      return sortDirectionSign;
-    } else if (a[sortColumn] < b[sortColumn]) {
-      return -sortDirectionSign;
-    }
-    return 0;
+  let rowComparer = (a, b) => {
+    return sortDirectionSign * comparer(a[sortColumn], b[sortColumn]);
   };
   if (sortDirection === 'NONE') {
     return rows;
   }
-  return rows.sort(comparer);
+  return rows.sort(rowComparer);
 };
 
 module.exports = sortRows;
+module.exports.comparer = comparer;
