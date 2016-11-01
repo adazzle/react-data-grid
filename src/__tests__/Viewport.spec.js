55e216bb6a610ef437eca3539cd42cd9dd579f5e
malonecj <malonecj@gmail.com>
2016-11-01 14:56:47 +0000

    Viewport scroll test

--- src/__tests__/Viewport.spec.js
+++ src/__tests__/Viewport.spec.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import Viewport from '../Viewport';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import helpers from './GridPropHelpers';
 
 let viewportProps = {
   rowOffsetHeight: 0,
   totalWidth: 400,
-  columnMetrics: { columns: [] },
+  columnMetrics: {
+    columns: helpers.columns,
+    minColumnWidth: 80,
+    totalWidth: true,
+    width: 2600
+  },
   rowGetter: () => { },
   rowsCount: 50,
   rowHeight: 35,
-  onScroll: () => {},
+  onScroll: () => { },
   minHeight: 500,
   cellMetaData: {
     selected: {},
     dragged: {},
-    onCellClick: () => {},
-    onCellDoubleClick: () => {},
-    onCommit: () => {},
-    onCommitCancel: () => {},
+    onCellClick: () => { },
+    onCellDoubleClick: () => { },
+    onCommit: () => { },
+    onCommitCancel: () => { },
     copied: {},
-    handleDragEnterRow: () => {},
-    handleTerminateDrag: () => {}
+    handleDragEnterRow: () => { },
+    handleTerminateDrag: () => { }
   },
   rowKey: 'Id'
 };
@@ -30,5 +36,26 @@ describe('<Viewport />', () => {
     const wrapper = shallow(<Viewport {...viewportProps} />);
     let Canvas = wrapper.find('Canvas');
     expect(Canvas).toBeDefined();
+  });
+
+  it('should updated scroll state onScroll', () => {
+    let scrollLeft = 0;
+    let scrollTop = 200;
+    const wrapper = shallow(<Viewport {...viewportProps} />);
+    let Canvas = wrapper.find('Canvas');
+    Canvas.props().onScroll({ scrollTop, scrollLeft});
+    expect(wrapper.state()).toEqual({
+      colDisplayEnd: 1,
+      colDisplayStart: 0,
+      colVisibleEnd: 1,
+      colVisibleStart: 0,
+      displayEnd: 33,
+      displayStart: 0,
+      height: viewportProps.minHeight,
+      scrollLeft: scrollLeft,
+      scrollTop: scrollTop,
+      visibleEnd: 20,
+      visibleStart: 3
+    });
   });
 });
