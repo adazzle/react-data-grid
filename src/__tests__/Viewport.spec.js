61979592c34eba86e2daad2c9854621c5292f3ce
malonecj <malonecj@gmail.com>
2016-11-01 15:46:29 +0000

    remove unused mount from test

--- src/__tests__/Viewport.spec.js
+++ src/__tests__/Viewport.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Viewport from '../Viewport';
-import { shallow, mount } from 'enzyme';
+import { shallow} from 'enzyme';
 import helpers from './GridPropHelpers';
 
 let viewportProps = {
