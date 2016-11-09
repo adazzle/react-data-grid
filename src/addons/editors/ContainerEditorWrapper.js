08ca938185f3da2d6af55d1ed56073ccc1a7ed1c
JamesPortelli <james.aportelli@gmail.com>
2016-11-09 16:42:17 +0000

    convert to hoc.

--- src/addons/editors/ContainerEditorWrapper.js
+++ src/addons/editors/ContainerEditorWrapper.js
@@ -1,40 +1,22 @@
 import React, { Component } from 'react';
 
-// Wrapper componenet used when having an editor which is a redux container.
+// Wrapper HOC used when having an editor which is a redux container.
 // Required since react-data-grid requires access to getInputNode, getValue,
 // howvever when doing this.getEditor() in react-data-grid we get a react
 // componenet wrapped by the redux connect function and thus wont have access
 // to the required methods.
-export class ContainerEditorWrapper extends Component {
-  constructor(props, context) {
-    super(props, context);
+export default (ContainerEditor) => {
+  return class ContainerEditorWrapper extends Component {
+    getInputNode() {
+      return this.editorRef.getInputNode();
+    }
 
-    this.setEditorRef = this.setEditorRef.bind(this);
-  }
+    getValue() {
+      return this.editorRef.getValue();
+    }
 
-  getInputNode() {
-    return this.editorRef.getInputNode();
-  }
-
-  getValue() {
-    return this.editorRef.getValue();
-  }
-
-  setEditorRef(ref) {
-    this.editorRef = ref;
-  }
-
-  render() {
-    let EditorComponent = this.props.editorComponent;
-    return (<EditorComponent refCallback={this.setEditorRef} {...this.props} />);
-  }
-}
-
-ContainerEditorWrapper.propTypes = {
-  value: React.PropTypes.string.isRequired,
-  column: React.PropTypes.object.isRequired,
-  rowData: React.PropTypes.isRequired,
-  editorComponent: React.PropTypes.object.isRequired
+    render() {
+      return (<ContainerEditor refCallback={(ref) => { this.editorRef = ref; }} {...this.props} />);
+    }
+  };
 };
-
-export default ContainerEditorWrapper;
