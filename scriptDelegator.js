4815672eb2881a4c0ce284ce9181d74c181ed846
diogofcunha <diogofncunha@gmail.com>
2017-01-25 12:04:44 +0000

    Updates

--- scriptDelegator.js
+++ scriptDelegator.js
@@ -1,9 +1,7 @@
-const path = require('path');
-
 const isDevEnv = () => window.location.hostname === 'localhost';
 
 const delegateScript = fileName => {
-  let src = isDevEnv() ? `http://localhost:8080/${fileName}` : path.join(__dirname, `/dist/${fileName}`);
+  let src = isDevEnv() ? `http://localhost:8080/${fileName}` : __dirname + `/dist/${fileName}`;
 
   let script = document.createElement('script');
   script.setAttribute('src', src);
