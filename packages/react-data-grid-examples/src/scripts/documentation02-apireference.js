f48f224225517e183718c39e480a8334beadc29c
willgates <willem.oomen@gmail.com>
2017-02-15 14:38:38 +0000

    Show onGridRowsUpdated in API references

--- packages/react-data-grid-examples/src/scripts/documentation02-apireference.js
+++ packages/react-data-grid-examples/src/scripts/documentation02-apireference.js
@@ -1,6 +1,4 @@
-var RowUpdateEventString = "type RowUpdateEvent = {\n\trowIdx: number;\n\tupdated: object;\n\tcellKey: string;\n\tkeyCode: string\n};";
-var CellDragEventString = "type CellDragEvent = {\n\trowIdx: number;\n\tfromRow: number,\n\ttoRow: number,\n\tvalue: any\n};";
-var CellCopyPasteEventString = "type CellCopyPasteEvent = {\n\trowIdx: number;\n\tvalue : any;\n\tfromRow: number;\n\ttoRow: number;\n\tcellKey: string\n};";
+var GridRowsUpdateEvent = 'type GridRowsUpdateEvent = {\n\tcellKey: string,\n\tfromRow: number,\n\ttoRow: number,\n\trowIds: array,\n\tupdated: object,\n\taction: string\n}';
 
 module.exports = React.createClass({
   render: function () {
@@ -8,6 +6,19 @@ module.exports = React.createClass({
       <div>
         <h1 id="js-api-refernce">API Reference</h1>
         <h2>Grid Events</h2>
+        <h3>onGridRowsUpdated</h3>
+        <div className="code-block function">
+          <pre>
+            <span className="nx">onGridRowsUpdated</span><span className="p">(</span><span className="nx">e</span> <span className="nx">:GridRowsUpdateEvent</span><span className="p">)</span>
+          </pre>
+        </div>
+        <div className="code-block flow">
+          <pre>
+            /* @flow */
+            <br/>
+            <code>{GridRowsUpdateEvent}</code>
+          </pre>
+        </div>
       </div>
     );
   }
