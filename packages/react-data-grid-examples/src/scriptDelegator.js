d853ac716aba5c859f81bec1f6015b63d2ca4fc2
willgates <willem.oomen@gmail.com>
2017-02-15 11:10:53 +0000

    Fix examples for IE9

--- packages/react-data-grid-examples/src/scriptDelegator.js
+++ packages/react-data-grid-examples/src/scriptDelegator.js
@@ -1,11 +1,11 @@
-const isDevEnv = function() {
+var isDevEnv = function() {
   return window.location.hostname === 'localhost';
 };
 
-const delegateScript = function(fileName) {
-  let src = (isDevEnv() ? 'http://localhost:8080/' : 'dist/') + fileName;
+var delegateScript = function(fileName) {
+  var src = (isDevEnv() ? 'http://localhost:8080/' : 'dist/') + fileName;
 
-  let script = document.createElement('script');
+  var script = document.createElement('script');
   script.setAttribute('src', src);
   document.getElementsByTagName('body')[0].appendChild(script);
 };
