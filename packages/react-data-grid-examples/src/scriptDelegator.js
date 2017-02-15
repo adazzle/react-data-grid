215af5e5615f06f0b99bf485fb2fa2f5bc24fe2d
willgates <willem.oomen@gmail.com>
2017-02-15 11:07:00 +0000

    Fix examples for IE11

--- packages/react-data-grid-examples/src/scriptDelegator.js
+++ packages/react-data-grid-examples/src/scriptDelegator.js
@@ -1,7 +1,9 @@
-const isDevEnv = () => window.location.hostname === 'localhost';
+const isDevEnv = function() {
+  return window.location.hostname === 'localhost';
+};
 
-const delegateScript = fileName => {
-  let src = isDevEnv() ? `http://localhost:8080/${fileName}` :  `dist/${fileName}`;
+const delegateScript = function(fileName) {
+  let src = (isDevEnv() ? 'http://localhost:8080/' : 'dist/') + fileName;
 
   let script = document.createElement('script');
   script.setAttribute('src', src);
