4f2d24cc2258c07d06a4d9c3939d8937d968a4a5
diogofcunha <diogofncunha@gmail.com>
2017-02-16 11:33:04 +0000

    Removed firefox

--- config/karma.js
+++ config/karma.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
       return BROWSERS.split(',');
     }
     if(RELEASE){
-      browsers = ['Chrome','Firefox','IE']
+      browsers = ['Chrome', 'IE']
     }else if(DEBUG){
       browsers = ['Chrome'];
     }
