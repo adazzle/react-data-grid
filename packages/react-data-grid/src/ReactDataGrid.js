09b86fa0ceb615a578854ba0e4123ab1a5d4ef9b
ViciEnzo <vincenzo.vicidomini@adazzle.com>
2017-02-24 12:37:29 +0000

    fixed typo

--- packages/react-data-grid/src/ReactDataGrid.js
+++ packages/react-data-grid/src/ReactDataGrid.js
@@ -293,7 +293,7 @@ const ReactDataGrid = React.createClass({
       rowIds.push(this.props.rowGetter(i)[this.props.rowKey]);
     }
 
-    let fromRowId = this.props.rowGetter(from)[this.props.rowKey];
+    let fromRowId = this.props.rowGetter(fromRow)[this.props.rowKey];
     let toRowId = this.props.rowGetter(toRow)[this.props.rowKey];
 
     this.props.onGridRowsUpdated({cellKey, fromRow, toRow, fromRowId, toRowId, rowIds, updated, action});
