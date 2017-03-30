33421286be054cf7d006a69e54ab98dd0efaa896
Michiel Alexander Boekhoff <michiel@markselectrical.co.uk>
2017-03-30 12:57:17 +0100

    Added require('react-dom') to fix tests.

--- packages/react-data-grid/src/Header.js
+++ packages/react-data-grid/src/Header.js
@@ -1,5 +1,5 @@
 const React               = require('react');
-// const ReactDOM            = require('react-dom');
+const ReactDOM            = require('react-dom');
 const joinClasses         = require('classnames');
 const shallowCloneObject  = require('./shallowCloneObject');
 const ColumnMetrics       = require('./ColumnMetrics');
