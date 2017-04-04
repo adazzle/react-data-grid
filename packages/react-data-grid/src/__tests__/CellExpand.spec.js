489f447413af79214301217e7e7e307524bec77a
Sonny Chan <schan@stratag.com>
2017-04-04 11:51:42 -0500

    adding tests for CellExpand, ChildRowDeleteButton, and RowComparer

---
+++ packages/react-data-grid/src/__tests__/CellExpand.spec.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import CellExpand from '../CellExpand';
+import { mount } from 'enzyme';
+
+describe('CellExpand', () => {
+  let testElement;
+  let getFakeProps = (expanded) => {
+    let expandableOptions = {expanded};
+    let onCellExpand = jasmine.createSpy();
+    return {
+      expandableOptions,
+      onCellExpand
+    };
+  };
+
+  const renderComponent = (props) => {
+    const wrapper = mount(<CellExpand {...props} />);
+    return wrapper;
+  };
+
+  it('should import CellExpand', () => {
+    expect(CellExpand).toBeDefined();
+  });
+
+  it('should create an instance of CellExpand', () => {
+    let fakeProps = getFakeProps(false);
+    testElement = renderComponent(fakeProps);
+    expect(testElement.find(CellExpand).length).toBe(1);
+  });
+
+  it('should render correctly when expanded is true', () => {
+    let fakeProps = getFakeProps(true);
+    testElement = renderComponent(fakeProps);
+    expect(testElement.state('expanded')).toBeTruthy();
+    expect(testElement.find('span.rdg-cell-expand').text()).toBe(String.fromCharCode('9650'));
+  });
+
+  it('should render correctly when expanded is false', () => {
+    let fakeProps = getFakeProps(false);
+    testElement = renderComponent(fakeProps);
+    expect(testElement.state('expanded')).toBeFalsy();
+    expect(testElement.find('span.rdg-cell-expand').text()).toBe(String.fromCharCode('9660'));
+  });
+
+  it('should call onCellExpand when clicked', () => {
+    let fakeProps = getFakeProps(false);
+    testElement = renderComponent(fakeProps);
+    testElement.simulate('click');
+    expect(fakeProps.onCellExpand).toHaveBeenCalled();
+    expect(fakeProps.onCellExpand.calls.count()).toEqual(1);
+  });
+
+  it('should correctly set state  when clicked', () => {
+    let fakeProps = getFakeProps(false);
+    testElement = renderComponent(fakeProps);
+    testElement.simulate('click');
+    expect(testElement.state('expanded')).toBeTruthy();
+    testElement.simulate('click');
+    expect(testElement.state('expanded')).toBeFalsy();
+  });
+});
