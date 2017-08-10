b7705ec655106df9bccc1633db0ed454a681b8e8
Vladimir Honcharenko <vladimir.honch@gmail.com>
2017-08-10 18:43:47 +0300

    Update ColumnUtils.js

--- packages/react-data-grid/src/ColumnUtils.js
+++ packages/react-data-grid/src/ColumnUtils.js
@@ -28,7 +28,8 @@ module.exports = {
   // Logic extented to allow for functions to be passed down in column.editable
   // this allows us to deicde whether we can be edting from a cell level
   canEdit(col, rowData, enableCellSelect) {
-    if (col && col.editable != null && typeof(col.editable) === 'function') {
+    if (!col) return false;
+    if (col.editable != null && typeof(col.editable) === 'function') {
       return enableCellSelect === true && col.editable(rowData);
     }
     return enableCellSelect === true && (!!col.editor || !!col.editable);
