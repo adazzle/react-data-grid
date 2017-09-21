b5d52b6eeee31331764c39e09374a691a0539d47
Chad Huntley <chad.huntley@toyota.com>
2017-09-21 11:37:24 -0700

    Allow for optional SimpleCellFormatter.value prop

--- packages/react-data-grid/src/formatters/SimpleCellFormatter.js
+++ packages/react-data-grid/src/formatters/SimpleCellFormatter.js
@@ -2,7 +2,7 @@ const React = require('react');
 
 const SimpleCellFormatter = React.createClass({
   propTypes: {
-    value: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number, React.PropTypes.object, React.PropTypes.bool]).isRequired
+    value: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number, React.PropTypes.object, React.PropTypes.bool])
   },
 
   shouldComponentUpdate(nextProps: any): boolean {
