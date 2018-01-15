209f0f7bc7f7696dcee8ab5c78277607d8fbfee2
Lucjan Suski <lucjansuski@gmail.com>
2018-01-15 14:51:36 +0100

    Pass sort properties to custom header renderer
    In order to be able to display current sort order, we need sorting information in header renderer.

--- packages/react-data-grid/src/HeaderRow.js
+++ packages/react-data-grid/src/HeaderRow.js
@@ -83,13 +83,26 @@ const HeaderRow = createReactClass({
   },
 
   getSortableHeaderCell(column) {
-    let sortDirection = (this.props.sortColumn === column.key) ? this.props.sortDirection : SortableHeaderCell.DEFINE_SORT.NONE;
-    return <SortableHeaderCell columnKey={column.key} onSort={this.props.onSort} sortDirection={sortDirection}/>;
+    let SortableRenderer = SortableHeaderCell;
+    if (column.headerRenderer !== undefined) {
+      SortableRenderer = column.headerRenderer;
+    }
+    const sortDirection = (this.props.sortColumn === column.key) ? this.props.sortDirection : SortableHeaderCell.DEFINE_SORT.NONE;
+    const props = { columnKey: column.key, onSort: this.props.onSort, sortDirection: sortDirection };
+
+    if (React.isValidElement(SortableRenderer)) {
+      // a string means it's an HTML element and props we want to pass are not valid, return it as is
+      if (typeof SortableRenderer.type === 'string') {
+        return SortableRenderer;
+      }
+      return React.cloneElement(SortableRenderer, props);
+    }
+    return React.createElement(SortableRenderer, props);
   },
 
   getHeaderRenderer(column) {
     let renderer;
-    if (column.headerRenderer && !this.props.filterable) {
+    if (column.headerRenderer && !this.props.filterable && !column.sortable) {
       renderer = column.headerRenderer;
     } else {
       let headerCellType = this.getHeaderCellType(column);
