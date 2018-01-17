fd82eecd9995e131692f94775caa6678de5ee786
Becky Reamy <rlreamy@med.umich.edu>
2018-01-17 11:31:22 -0500

    Make sure code is following coding standards

--- packages/react-data-grid/src/cells/headerCells/SortableHeaderCell.js
+++ packages/react-data-grid/src/cells/headerCells/SortableHeaderCell.js
@@ -23,26 +23,26 @@ class SortableHeaderCell extends React.Component {
     case null:
     case undefined:
     case DEFINE_SORT.NONE:
-          if (this.props.descendingFirst) {
-    	        direction = DEFINE_SORT.DESC;
-          } else {
-	            direction = DEFINE_SORT.ASC;
-	      }
-          break;
+      if (this.props.descendingFirst) {
+        direction = DEFINE_SORT.DESC;
+	  } else {
+		direction = DEFINE_SORT.ASC;
+	  }
+      break;
     case DEFINE_SORT.ASC:
-    	  if (this.props.descendingFirst) {
-    	  	  direction = DEFINE_SORT.NONE;
-    	  } else {
-    		  direction = DEFINE_SORT.DESC;
-    	  }
-          break;
+	  if (this.props.descendingFirst) {
+		direction = DEFINE_SORT.NONE;
+      } else {
+		direction = DEFINE_SORT.DESC;
+	  }
+	  break;
     case DEFINE_SORT.DESC:
-    	  if (this.props.descendingFirst) {
-    		  direction = DEFINE_SORT.ASC;
-    	  } else {
-      	  	  direction = DEFINE_SORT.NONE;
-      	  }
-          break;
+	  if (this.props.descendingFirst) {
+		direction = DEFINE_SORT.ASC;
+      } else {
+      	direction = DEFINE_SORT.NONE;
+      }
+      break;
     }
     this.props.onSort(
       this.props.columnKey,
