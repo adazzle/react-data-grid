22cfcf62d37093f4d1406f63a93eefb02ff3ded4
Aman Mahajan <amahajan@stratag.com>
2018-01-22 12:40:00 -0600

    Add unit tests for getRenderedColumnCount

--- packages/react-data-grid/src/utils/__tests__/viewportUtils.spec.js
+++ packages/react-data-grid/src/utils/__tests__/viewportUtils.spec.js
@@ -1,5 +1,5 @@
 import columnUtils from '../../ColumnUtils';
-import { getGridState } from '../viewportUtils';
+import { getGridState, getRenderedColumnCount } from '../viewportUtils';
 
 describe('viewportUtils', () => {
   describe('getGridState', () => {
@@ -26,22 +26,53 @@ describe('viewportUtils', () => {
       expect(state.height).toBe(95);
     });
 
-    it('should correctly set the displayed rows count when rowsCount is greater than the rendered rows count', () => {
+    it('should correctly set visible rows count when rowsCount is greater than the rendered rows count', () => {
       const { state } = getState();
       expect(state.displayEnd).toBe(16);
     });
 
-    it('should correctly set the displayed rows count when rowsCount is less than the rendered rows count', () => {
+    it('should correctly set visible rows count when rowsCount is less than the rendered rows count', () => {
       const { state } = getState({ rowsCount: 10 });
       expect(state.displayEnd).toBe(10);
     });
 
-    it('should correctly set the column count', () => {
+    it('should correctly set visible column count', () => {
       const fakeGetSize = spyOn(columnUtils, 'getSize').and.returnValue(15);
       const { state, props } = getState();
 
       expect(fakeGetSize).toHaveBeenCalledWith(props.columnMetrics.columns);
       expect(state.colVisibleEnd).toBe(15);
+    });
+  });
+
+  describe('getRenderedColumnCount', () => {
+    const fakeGetDOMNodeOffsetWidth = jasmine.createSpy('getDOMNodeOffsetWidth').and.returnValue(50);
+    const getColumnCount = (width) => {
+      return getRenderedColumnCount({
+        columnMetrics: {
+          columns: [
+            { key: 'col1', width: 5 },
+            { key: 'col2', width: 12 }
+          ],
+          totalWidth: 0
+        }
+      }, fakeGetDOMNodeOffsetWidth, 0, width);
+    };
+
+    beforeEach(() => {
+      fakeGetDOMNodeOffsetWidth.calls.reset();
+    });
+
+    it('correctly set rendered columns count if width is 0', () => {
+      const count = getColumnCount(0);
+      expect(fakeGetDOMNodeOffsetWidth).toHaveBeenCalled();
+      expect(count).toBe(2);
+    });
+
+    it('correctly set rendered columns count if width is greater than 0', () => {
+      const count = getColumnCount(4);
+      expect(fakeGetDOMNodeOffsetWidth).not.toHaveBeenCalled();
+      expect(count).toBe(1);
     });
   });
 });
--- packages/react-data-grid/src/utils/viewportUtils.js
+++ packages/react-data-grid/src/utils/viewportUtils.js
@@ -90,5 +90,6 @@ function getNextScrollState(props, getDOMNodeOffsetWidth, scrollTop, scrollLeft,
 
 export {
   getGridState,
-  getNextScrollState
+  getNextScrollState,
+  getRenderedColumnCount
 };
