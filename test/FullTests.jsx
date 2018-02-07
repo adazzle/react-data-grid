5422d439899038a329d1566ce0bfa2eb96232573
Aman Mahajan <amahajan@stratag.com>
2018-02-06 22:43:27 -0600

    Upgrade enzyme to 3.x

--- test/FullTests.jsx
+++ test/FullTests.jsx
@@ -1,2 +1,7 @@
-var testsContext = require.context("../packages", true, /^.*\.(spec|integration-spec)$/);
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const testsContext = require.context("../packages", true, /^.*\.(spec|integration-spec)$/);
 testsContext.keys().forEach(testsContext);
