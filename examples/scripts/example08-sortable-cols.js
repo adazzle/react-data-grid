b96d79f803ad23cf8e45be0a86b956e7ce42bb60
Conor Malone <malonecj@users.noreply.github.com>
2018-02-12 10:35:23 +0000

    Add custom header to examples

--- examples/scripts/example08-sortable-cols.js
+++ examples/scripts/example08-sortable-cols.js
@@ -1,128 +1,120 @@
-var ReactGrid             = require('../build/react-data-grid');
-var QuickStartDescription = require('../components/QuickStartDescription')
-var ReactPlayground       = require('../assets/js/ReactPlayground');
+const ReactDataGrid = require('react-data-grid');
+const exampleWrapper = require('../components/exampleWrapper');
+const React = require('react');
 
-var EditableExample = `
-//helper to generate a random date
-function randomDate(start, end) {
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
-}
-
-//helper to create a fixed number of rows
-function createRows(numberOfRows){
-  var _rows = [];
-  for (var i = 1; i < numberOfRows; i++) {
-    _rows.push({
-      id: i,
-      task: 'Task ' + i,
-      complete: Math.min(100, Math.round(Math.random() * 110)),
-      priority : ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
-      issueType : ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
-      startDate: randomDate(new Date(2015, 3, 1), new Date()),
-      completeDate: randomDate(new Date(), new Date(2016, 0, 1))
-    });
-  }
-  return _rows;
-}
-
-//function to retrieve a row for a given index
-var rowGetter = function(i){
-  return _rows[i];
+const CustomHeaderRenderer = (props) => {
+  return (<span style={{color: 'blue'}}>Custom Header</span>);
 };
 
-//Columns definition
-var columns = [
-{
-  key: 'id',
-  name: 'ID',
-  width: 80
-},
-{
-  key: 'task',
-  name: 'Title',
-  sortable : true
-},
-{
-  key: 'priority',
-  name: 'Priority',
-  sortable : true
-},
-{
-  key: 'issueType',
-  name: 'Issue Type',
-  sortable : true
-},
-{
-  key: 'complete',
-  name: '% Complete',
-  sortable : true
-},
-{
-  key: 'startDate',
-  name: 'Start Date',
-  sortable : true
-},
-{
-  key: 'completeDate',
-  name: 'Expected Complete',
-  sortable : true
-}
-]
+class Example extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this._columns = [
+      {
+        key: 'id',
+        name: 'ID',
+        locked: true
+      },
+      {
+        key: 'task',
+        name: 'Title',
+        width: 200,
+        sortable: true
+      },
+      {
+        key: 'priority',
+        name: 'Priority',
+        width: 200,
+        sortable: true,
+        headerRenderer: <CustomHeaderRenderer/>
+      },
+      {
+        key: 'issueType',
+        name: 'Issue Type',
+        width: 200,
+        sortable: true
+      },
+      {
+        key: 'complete',
+        name: '% Complete',
+        width: 200,
+        sortable: true
+      },
+      {
+        key: 'startDate',
+        name: 'Start Date',
+        width: 200,
+        sortable: true
+      },
+      {
+        key: 'completeDate',
+        name: 'Expected Complete',
+        width: 200,
+        sortable: true
+      }
+    ];
 
+    let originalRows = this.createRows(1000);
+    let rows = originalRows.slice(0);
+    this.state = { originalRows, rows };
+  }
 
-var Example = React.createClass({
+  getRandomDate = (start, end) => {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
+  };
 
-  getInitialState : function(){
-    var originalRows = createRows(1000);
-    var rows = originalRows.slice(0);
-    //store the original rows array, and make a copy that can be used for modifying eg.filtering, sorting
-    return {originalRows : originalRows, rows : rows};
-  },
+  createRows = () => {
+    let rows = [];
+    for (let i = 1; i < 1000; i++) {
+      rows.push({
+        id: i,
+        task: 'Task ' + i,
+        complete: Math.min(100, Math.round(Math.random() * 110)),
+        priority: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
+        issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
+        startDate: this.getRandomDate(new Date(2015, 3, 1), new Date()),
+        completeDate: this.getRandomDate(new Date(), new Date(2016, 0, 1))
+      });
+    }
 
-  rowGetter : function(rowIdx){
-    return this.state.rows[rowIdx];
-  },
+    return rows;
+  };
 
-  handleGridSort : function(sortColumn, sortDirection){
-
-    var comparer = function(a, b) {
-      if(sortDirection === 'ASC'){
+  handleGridSort = (sortColumn, sortDirection) => {
+    const comparer = (a, b) => {
+      if (sortDirection === 'ASC') {
         return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
-      }else if(sortDirection === 'DESC'){
+      } else if (sortDirection === 'DESC') {
         return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
       }
-    }
-    var rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
-    this.setState({rows : rows});
-  },
+    };
 
-  render:function(){
-    return(
+    const rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
+
+    this.setState({ rows });
+  };
+
+  rowGetter = (i) => {
+    return this.state.rows[i];
+  };
+
+  render() {
+    return  (
       <ReactDataGrid
         onGridSort={this.handleGridSort}
-        columns={columns}
+        columns={this._columns}
         rowGetter={this.rowGetter}
         rowsCount={this.state.rows.length}
-        minHeight={500}
-        onRowUpdated={this.handleRowUpdated} />
-    )
+        minHeight={500} />);
   }
+}
 
-});
+const exampleDescription = (<p>While ReactDataGrid does not provide the ability to sort directly, it does provide hooks that allow you to provide your own sort function. This is done via the <code>onGridSort</code> prop. To enable sorting for a given column, set <code>column.sortable = true</code> for that column. Now when the header cell is clicked for that column, <code>onGridSort</code> will be triggered passing the column name and the sort direction.</p>);
 
-ReactDOM.render(<Example />, mountNode);
-`;
-
-module.exports = React.createClass({
-
-  render:function(){
-    return(
-      <div>
-      <h3>Sortable Columns Example</h3>
-      <p>While ReactDataGrid doesnt not provide the ability to sort directly, it does provide hooks that allow you to provide your own sort function. This is done via the <code>onGridSort</code> prop. To enable sorting for a given column, set <code>column.sortable = true</code> for that column. Now when the header cell is clicked for that column, <code>onGridSort</code> will be triggered passing the column name and the sort direction.</p>
-      <ReactPlayground codeText={EditableExample} />
-      </div>
-    )
-  }
-
+module.exports = exampleWrapper({
+  WrappedComponent: Example,
+  exampleName: 'Sortable Columns Example',
+  exampleDescription,
+  examplePath: './scripts/example08-sortable-cols.js',
+  examplePlaygroundLink: 'https://jsfiddle.net/k7tfnw1n/8/'
 });
