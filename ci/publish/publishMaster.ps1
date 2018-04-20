8f3503d4bcec9f466b46e391d9ab9e8979f43dce
malonecj <malonecj@gmail.com>
2018-04-20 17:07:16 +0100

    Version bump to  [ci skip]

--- ci/publish/publishMaster.ps1
+++ ci/publish/publishMaster.ps1
@@ -1,18 +1,10 @@
-Write-Host "Merging to master. Attempting to bump version"
-git config --global credential.helper store
-git config --global push.default simple
-Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
-git config --global user.name $env:APPVEYOR_REPO_COMMIT_AUTHOR
-git config --global user.email $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
-git checkout master
-$releaseVersion = node ./ci/publish/getReleaseVersion.js
-
+npm run build
 if($?)
 {
   Write-Host "Publishing $($releaseVersion) to npm"
   $versionBumpMessage = "Version bump to $($releaseVersion) [ci skip]"
 
-  ./node_modules/.bin/lerna publish --repo-version $releaseVersion  --skip-git --yes
+  ./node_modules/.bin/lerna publish --repo-version 4.0.0  --skip-git --yes
   if($?) {
     Write-Host "Regenerating public site and examples"
     node ./ci/publish/publishExamples.js
