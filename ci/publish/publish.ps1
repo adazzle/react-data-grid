985715a05aae5b08443d034075b3214b7777a4c1
malonecj <malonecj@gmail.com>
2018-04-20 17:11:16 +0100

    Version bump to  [ci skip]

--- ci/publish/publish.ps1
+++ ci/publish/publish.ps1
@@ -1,10 +1,11 @@
+npm install
 npm run build
 if($?)
 {
   Write-Host "Publishing $($releaseVersion) to npm"
   $versionBumpMessage = "Version bump to $($releaseVersion) [ci skip]"
 
-  ./node_modules/.bin/lerna publish --repo-version 4.0.0  --skip-git --yes
+  ./node_modules/.bin/lerna publish --repo-version 4.0.1  --skip-git --yes
   if($?) {
     Write-Host "Regenerating public site and examples"
     node ./ci/publish/publishExamples.js
