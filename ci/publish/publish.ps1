208a2399f63d0eac342a07c6985b5e30b134b5a4
malonecj <malonecj@gmail.com>
2018-04-23 10:49:54 +0100

    Version bump to 4.0.3 [ci skip]

--- ci/publish/publish.ps1
+++ ci/publish/publish.ps1
@@ -1,11 +1,12 @@
+$Version = Read-Host -Prompt 'Enter semver version you would like to publish'
 npm install
 npm run build
 if($?)
 {
-  Write-Host "Publishing $($releaseVersion) to npm"
-  $versionBumpMessage = "Version bump to $($releaseVersion) [ci skip]"
+  Write-Host "Publishing $($Version) to npm"
+  $versionBumpMessage = "Version bump to $($Version) [ci skip]"
 
-  ./node_modules/.bin/lerna publish --repo-version 4.0.1  --skip-git --yes
+  ./node_modules/.bin/lerna publish --repo-version $Version  --skip-git --yes
   if($?) {
     Write-Host "Regenerating public site and examples"
     node ./ci/publish/publishExamples.js
