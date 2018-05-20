30acd7461320db3b3216f79c18f08a5a7e21be7d
Berislav <berislav.hunjadi@gmail.com>
2018-05-20 12:36:18 +0200

    Updated multiple column sort example

--- packages/react-data-grid-examples/src/scripts/example30-multiple-column-sort.js
+++ packages/react-data-grid-examples/src/scripts/example30-multiple-column-sort.js
@@ -2,61 +2,65 @@ const ReactDataGrid = require('react-data-grid');
 const exampleWrapper = require('../components/exampleWrapper');
 const React = require('react');
 
-const Example = React.createClass({
-  getInitialState() {
-    this._columns = [
-      {
-        key: 'id',
-        name: 'ID',
-        locked: true
-      },
-      {
-        key: 'task',
-        name: 'Title',
-        width: 200,
-        sortable: true
-      },
-      {
-        key: 'priority',
-        name: 'Priority',
-        width: 200,
-        sortable: true
-      },
-      {
-        key: 'issueType',
-        name: 'Issue Type',
-        width: 200,
-        sortable: true
-      },
-      {
-        key: 'complete',
-        name: '% Complete',
-        width: 200,
-        sortable: true
-      },
-      {
-        key: 'startDate',
-        name: 'Start Date',
-        width: 200,
-        sortable: true
-      },
-      {
-        key: 'completeDate',
-        name: 'Expected Complete',
-        width: 200,
-        sortable: true
-      }
-    ];
+const columns = [
+  {
+    key: 'id',
+    name: 'ID',
+    locked: true
+  },
+  {
+    key: 'task',
+    name: 'Title',
+    width: 200,
+    sortable: true
+  },
+  {
+    key: 'priority',
+    name: 'Priority',
+    width: 200,
+    sortable: true
+  },
+  {
+    key: 'issueType',
+    name: 'Issue Type',
+    width: 200,
+    sortable: true
+  },
+  {
+    key: 'complete',
+    name: '% Complete',
+    width: 200,
+    sortable: true
+  },
+  {
+    key: 'startDate',
+    name: 'Start Date',
+    width: 200,
+    sortable: true
+  },
+  {
+    key: 'completeDate',
+    name: 'Expected Complete',
+    width: 200,
+    sortable: true
+  }
+];
+
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log('constructor');
 
     let originalRows = this.createRows(1000);
     let rows = originalRows.slice(0);
     // Store the original rows array, and make a copy that can be used for modifying eg.filtering, sorting
-    return { originalRows, rows };
-  },
+    this.state = { originalRows, rows };
+  }
 
   getRandomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
-  },
+  }
 
   createRows() {
     let rows = [];
@@ -73,9 +77,9 @@ const Example = React.createClass({
     }
 
     return rows;
-  },
+  }
 
-  handleSort(sort) {
+  handleSort = (sort) => {
     const valueComparer = (v1, v2, direction) => {
       if (v1 === v2) return 0;
       if (direction === 'ASC') {
@@ -98,27 +102,27 @@ const Example = React.createClass({
 
     const rows = sort.length === 0 ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
     this.setState({ rows });
-  },
+  }
 
-  rowGetter(i) {
+  getRowAt = (i) => {
     return this.state.rows[i];
-  },
+  }
 
   render() {
     return  (
       <ReactDataGrid
         onGridMultipleColumnsSort={this.handleSort}
         multipleColumnsSort={true}
-        columns={this._columns}
+        columns={columns}
         sort={[{
           column: 'task',
           direction: 'ASC'}
         ]}
-        rowGetter={this.rowGetter}
+        rowGetter={this.getRowAt}
         rowsCount={this.state.rows.length}
         minHeight={500} />);
   }
-});
+}
 
 const exampleDescription = (<p>To enable multiple column sort, set <code>{'multipleColumnsSort={true}'}</code> on ReactDataGrid component. To enable sorting for a given column, set <code>column.sortable = true</code> for that column. Unlike single sort, when the header cell is clicked for a column, <code>onGridMultipleColumnsSort</code> will be triggered passing the array of <code>{'{columnKey, sortDirection}'}</code> pairs. Also, there is an option to provide initial sort array for the grid via <code>sort</code> property which can be passed to ReactDataGrid</p>);
 
