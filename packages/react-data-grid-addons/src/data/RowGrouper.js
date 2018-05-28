6475e939968eeefbaa7d4565a638560337a82b00
Mohit Kashyap <mohit.kashyap.001@gmail.com>
2018-05-28 15:28:02 +0530

    revert changes

--- packages/react-data-grid-addons/src/data/RowGrouper.js
+++ packages/react-data-grid-addons/src/data/RowGrouper.js
@@ -28,12 +28,8 @@ class RowGrouper {
 
     for (let i = 0; i < keys.length; i++) {
       let key = keys[i];
-      let count = 0;
       let isExpanded = this.isRowExpanded(columnName, key);
-      if (groupedRows) {
-        count = groupedRows[key] ? groupedRows[key].length : 0;
-      }
-      let rowGroupHeader = { name: key, groupedRowCount: count, __metaData: { isGroup: true, treeDepth: columnIndex, isExpanded: isExpanded, columnGroupName: columnName } };
+      let rowGroupHeader = {name: key, __metaData: {isGroup: true, treeDepth: columnIndex, isExpanded: isExpanded, columnGroupName: columnName}};
 
       dataviewRows = this.resolver.addHeaderRow(rowGroupHeader, dataviewRows);
 
