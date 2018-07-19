a0767ba6cda7d7160ce3d2110b02e300f1ccfd38
Conor Malone <malonecj@gmail.com>
2018-07-19 18:05:33 +0100

    Set height of grid

--- packages/react-data-grid-examples/src/scripts/example30-efficient-windowing.js
+++ packages/react-data-grid-examples/src/scripts/example30-efficient-windowing.js
@@ -6,8 +6,21 @@ const COL_WIDTH = 200;
 const ROW_HEIGHT = 35;
 const HEADER_HEIGHT = 35;
 
+const createColumns = (numberCols) => [...Array(numberCols).keys()].map(i => {
+  return {
+    key: `col${i}`,
+    name: `col${i}`,
+    width: COL_WIDTH
+  }
+});
+
+const createRows = (numberRows) => [...Array(numberRows).keys()].map(i => {
+  return [...Array(100).keys()].reduce((row, j) => ({...row, ...{[`col${j}`]: `row ${i} col ${j}`} }), {});
+});
+
+
+
 const RenderWindow = ({ isScrolling, colOverscanEndIdx, colOverscanStartIdx, rowOverscanEndIdx, rowOverscanStartIdx, rowVisibleStartIdx, rowVisibleEndIdx, colVisibleStartIdx, colVisibleEndIdx  }) => {
-  console.log(rowVisibleEndIdx);
   const topOffset = (rowOverscanStartIdx - rowVisibleStartIdx) * ROW_HEIGHT;
   const marginTop = HEADER_HEIGHT + topOffset;
   const marginLeft = (colOverscanStartIdx - colVisibleStartIdx) * COL_WIDTH;
@@ -18,29 +31,47 @@ const RenderWindow = ({ isScrolling, colOverscanEndIdx, colOverscanStartIdx, row
     </div> : null);
 }
 
+const NumberInput = ({name, value, onChange}) => {
+  return (
+  <div className="col-lg-2">
+  <div className="input-group">
+  <span className="input-group-addon" id="basic-addon1">{name}</span>
+  <input type="text" className="form-control" defaultValue={value} onChange={onChange}  />
+</div></div>);
+}
+
+const GridHeightInput = ({value, onChange}) => {
+  return (<NumberInput name="Height" value={value} onChange={onChange}/>);
+}
+
+const GridWidthInput = ({value, onChange}) => {
+  return (<NumberInput name="Width" value={value} onChange={onChange}/>);
+}
+
+const NumberOfColumns = ({value, onChange}) => {
+  return (<NumberInput name="Number Columns" value={value} onChange={onChange}/>);
+}
+
+const NumberOfRows = ({value, onChange}) => {
+  return (<NumberInput name="Number Rows" value={value} onChange={onChange}/>);
+}
+
 class Example extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.createRows();
-    this._columns = [...Array(100).keys()].map(i => {
-      return {
-        key: `col${i}`,
-        name: `col${i}`,
-        width: COL_WIDTH
-      }
-    });
-
-    this.state = null;
+    this.state = {columns: createColumns(100), rows: createRows(1000), width: 600, height: 600};
   }
 
-  createRows = () => {
-    this._rows = [...Array(1000).keys()].map(i => {
-      return [...Array(100).keys()].reduce((row, j) => ({...row, ...{[`col${j}`]: `row ${i} col ${j}`} }), {});
-    });
-  };
+  setColumns = (numberCols) => {
+    this.setState({columns: createColumns(numberCols)});
+  }
+
+  setHeight = (e) => {
+    this.setState({height: e.target.value})
+  }
 
   rowGetter = (i) => {
-    return this._rows[i];
+    return this.state.rows[i];
   };
 
   clearScrollTimer = () => {
@@ -73,13 +104,19 @@ class Example extends React.Component {
   render() {
     return (
       <div style={{ marginLeft: '25%', marginTop: '5%' }}>
+        <div className="row" >
+          <NumberOfColumns onChange={this.setNumberColumns} value={this.state.columns.length}/>
+          <NumberOfRows onChange={this.setNumberRows} value={this.state.rows.length}/>
+          <GridWidthInput value={this.state.width}/>
+          <GridHeightInput  value={this.state.height} onChange={this.setHeight}/>
+        </div>
         <RenderWindow {...this.state} />
         <ReactDataGrid
-          columns={this._columns}
+          columns={this.state.columns}
           rowGetter={this.rowGetter}
-          rowsCount={this._rows.length}
-          minWidth={600}
-          minHeight={400}
+          rowsCount={this.state.rows.length}
+          minWidth={this.state.width}
+          minHeight={this.state.height}
           onScroll={this.onScroll}
         /></div>);
   }
