59d0002506d034b887d468229d7a8b90951b4e35
Conor Malone <malonecj@gmail.com>
2018-11-16 15:18:01 +0000

    add siteconfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,14 +14,14 @@ const users = [
 const siteConfig = {
   title: 'React Data Grid', // Title for your website.
   tagline: 'Excel-like data grid component built with React',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'http://adazzle.github.io/react-data-grid/', // Your website URL
+  baseUrl: '/react-data-grid/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'react-data-grid',
   organizationName: 'adazzle',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
