240b3d10bff91aed6fd9132774bb958ff34502e9
Aman Mahajan <amahajan@stratag.com>
2018-11-26 15:09:28 -0600

    Fix Row export

--- packages/react-data-grid/src/index.js
+++ packages/react-data-grid/src/index.js
@@ -1,12 +1,15 @@
 import Grid from './ReactDataGrid';
 import RowComparer from 'common/utils/RowComparer';
 import Cell from './Cell';
+import Row from './Row';
+import EmptyChildRow from './EmptyChildRow';
+
 module.exports = Grid;
-module.exports.Row = require('./Row');
+module.exports.Row = Row;
 module.exports.Cell = Cell;
 module.exports.HeaderCell = require('./HeaderCell');
 module.exports.RowComparer = RowComparer;
-module.exports.EmptyChildRow = require('./EmptyChildRow');
+module.exports.EmptyChildRow = EmptyChildRow;
 module.exports.editors = require('common/editors');
 module.exports.formatters = require('./formatters');
 module.exports.shapes = require('common/prop-shapes');
