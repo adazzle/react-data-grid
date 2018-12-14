6b4444f8dfcabbe91171c92938a71b5da2f8523b
Ross Patterson <rossjp@gmail.com>
2018-12-14 15:31:07 -0500

    added tests for CheckboxEditor

--- packages/common/editors/__tests__/CheckboxEditor.spec.js
+++ packages/common/editors/__tests__/CheckboxEditor.spec.js
@@ -1,26 +1,44 @@
 const React          = require('react');
 const ReactDOM = require('react-dom');
 const TestUtils      = require('react-dom/test-utils');
-const CheckboxEditor = require('../CheckboxEditor');
 const { mount } = require('enzyme');
+const sinon = require('sinon');
+const CheckboxEditor = require('../CheckboxEditor');
 
 describe('CheckboxEditor', () => {
   let component;
   let componentWrapper;
+  let consoleErrorStub;
   const testColumn = {
     key: 'columnKey',
     onCellChange: function() {}
   };
 
   describe('Basic tests', () => {
+    afterEach(() => {
+      consoleErrorStub.restore();
+    });
+
     beforeEach(() => {
       spyOn(testColumn, 'onCellChange');
+      consoleErrorStub = sinon.stub(console, 'error');
       componentWrapper = mount(<CheckboxEditor
         value={true}
         rowIdx={1}
         column={testColumn}/>);
       component = componentWrapper.instance();
     });
+
+    it('should not send warning message to console.error', () => {
+      expect(consoleErrorStub.callCount).toBe(0);
+    });
+
+    it('should have a readOnly flag set', () => {
+      const Input = TestUtils.findRenderedDOMComponentWithTag(component, 'input');
+      const checkboxNode = ReactDOM.findDOMNode(Input);
+      expect(checkboxNode.readOnly).toBe(true);
+    });
+
 
     it('should create a new CheckboxEditor instance', () => {
       expect(component).toBeDefined();
