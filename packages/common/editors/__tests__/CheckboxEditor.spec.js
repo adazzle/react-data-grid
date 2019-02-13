812a9edb91a1bbf34f809166714f67b2ef9ad1cf
Ross Patterson <rossjp@gmail.com>
2019-02-13 10:29:54 -0500

    remove test proving console error is not logged

--- packages/common/editors/__tests__/CheckboxEditor.spec.js
+++ packages/common/editors/__tests__/CheckboxEditor.spec.js
@@ -2,35 +2,25 @@ const React          = require('react');
 const ReactDOM = require('react-dom');
 const TestUtils      = require('react-dom/test-utils');
 const { mount } = require('enzyme');
-const sinon = require('sinon');
 const CheckboxEditor = require('../CheckboxEditor');
 
 describe('CheckboxEditor', () => {
   let component;
   let componentWrapper;
-  let consoleErrorStub;
   const testColumn = {
     key: 'columnKey',
     onCellChange: function() {}
   };
 
   describe('Basic tests', () => {
-    afterEach(() => {
-      consoleErrorStub.restore();
-    });
 
     beforeEach(() => {
       spyOn(testColumn, 'onCellChange');
-      consoleErrorStub = sinon.stub(console, 'error');
       componentWrapper = mount(<CheckboxEditor
         value={true}
         rowIdx={1}
         column={testColumn}/>);
       component = componentWrapper.instance();
-    });
-
-    it('should not send warning message to console.error', () => {
-      expect(consoleErrorStub.callCount).toBe(0);
     });
 
     it('should have a readOnly flag set', () => {
