30593e5e2528a7ed7e7f4355c37ca7efc8ba5dd3
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-03-15 12:31:57 +0000

    Remove mkirp

--- docs/utils/docsGen.js
+++ docs/utils/docsGen.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const mkdirp = require('mkdirp');
 const del = require('del');
 const generateMarkdown = require('./generateMarkdown');
 const generalUtils = require('./generalUtils');
@@ -22,7 +21,6 @@ const conf = {
 function buildDocs(api) {
   try {
     const allDocPaths = [];
-    mkdirp(conf.markdownDir);
     for (const filepath in api) {
       const name = generalUtils.getComponentName(filepath);
       const markdown = generateMarkdown(name, api[filepath]);
@@ -38,23 +36,11 @@ function buildDocs(api) {
   }
 }
 
-function copyDocsToExamples() {
-  mkdirp(conf.exampleDocsDir);
-  fs.copy(conf.markdownDir, conf.exampleDocsDir, function(err) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Documents copied to examples!');
-    }
-  });
-}
-
 async function main() {
   // API
   await new Promise(function(resolve) {
     const res = {};
     generalUtils.traverseDir('packages/react-data-grid/src', res, function() {
-      mkdirp.sync(path.dirname(conf.apiDocsFilePath));
       fs.writeFile(conf.apiDocsFilePath, JSON.stringify(res, null, 2), resolve);
     }, conf.publicAPI, conf.ignoreDir);
   });
