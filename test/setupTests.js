62c12d84e243ba1bddb3330b6e49e2c8b70d9dec
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-03-19 16:26:36 +0000

    Fix missing window.Immutable, remove jasmine-core and unused files

--- test/setupTests.js
+++ test/setupTests.js
@@ -1,5 +1,7 @@
 import '@babel/polyfill';
 import enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import Immutable from 'immutable';
 
 enzyme.configure({ adapter: new Adapter() });
+window.Immutable = Immutable;
