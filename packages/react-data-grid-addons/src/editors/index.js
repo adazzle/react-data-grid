24f63de2a90833846530699fb1c3778dac42bf9c
Aman Mahajan <amahajan@stratag.com>
2019-04-01 14:01:04 -0500

    Deprecate ContainerEditorWrapper

--- packages/react-data-grid-addons/src/editors/index.js
+++ packages/react-data-grid-addons/src/editors/index.js
@@ -3,5 +3,4 @@ import { editors } from 'react-data-grid';
 const { SimpleTextEditor, CheckboxEditor } = editors;
 
 export { SimpleTextEditor, CheckboxEditor };
-export { default as ContainerEditorWrapper } from './ContainerEditorWrapper';
 export { default as DropDownEditor } from './DropDownEditor';
