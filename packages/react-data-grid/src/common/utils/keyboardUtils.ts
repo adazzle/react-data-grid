e5a61ad3e8d987783c76d9bda7cb24f5a4df789e
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-04-05 18:01:23 +0100

    tsify keyboardUtils

--- packages/react-data-grid/src/common/utils/keyboardUtils.ts
+++ packages/react-data-grid/src/common/utils/keyboardUtils.ts
@@ -1,15 +1,12 @@
-export function isKeyPrintable(keycode) {
-  const valid =
-    (keycode > 47 && keycode < 58) // number keys
+export function isKeyPrintable(keycode: number): boolean {
+  return (keycode > 47 && keycode < 58) // number keys
     || keycode === 32 || keycode === 13 // spacebar & return key(s) (if you want to allow carriage returns)
     || (keycode > 64 && keycode < 91) // letter keys
     || (keycode > 95 && keycode < 112) // numpad keys
     || (keycode > 185 && keycode < 193) // ;=,-./` (in order)
     || (keycode > 218 && keycode < 223); // [\]' (in order)
-
-  return valid;
 }
 
-export function isCtrlKeyHeldDown(e) {
+export function isCtrlKeyHeldDown(e: KeyboardEvent): boolean {
   return (e.ctrlKey === true || e.metaKey === true) && e.key !== 'Control';
 }
