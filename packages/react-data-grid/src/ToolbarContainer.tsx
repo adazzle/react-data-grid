0508f96f220014c46b038c39596750f1b4e9017a
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-04-11 17:28:34 +0100

    Make the toolbar prop optional

--- packages/react-data-grid/src/ToolbarContainer.tsx
+++ packages/react-data-grid/src/ToolbarContainer.tsx
@@ -10,10 +10,14 @@ interface ToolbarProps {
 
 export interface ToolbarContainerProps extends ToolbarProps {
   /** Component used to render toolbar above the grid */
-  toolbar: React.ReactElement<ToolbarProps> | React.ComponentType<ToolbarProps>;
+  toolbar?: React.ReactElement<ToolbarProps> | React.ComponentType<ToolbarProps>;
 }
 
 export default function ToolbarContainer({ toolbar, columns, rowsCount, onToggleFilter }: ToolbarContainerProps) {
+  if (!toolbar) {
+    return null;
+  }
+
   const toolBarProps = { columns, onToggleFilter, rowsCount };
 
   if (isElement(toolbar)) {
