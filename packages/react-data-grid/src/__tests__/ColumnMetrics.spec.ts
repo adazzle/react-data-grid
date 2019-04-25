35e6e51f8e47550665677430cbb895b7d78c4602
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-04-25 20:05:41 +0100

    tsify ColumnMetrics

--- packages/react-data-grid/src/__tests__/ColumnMetrics.spec.ts
+++ packages/react-data-grid/src/__tests__/ColumnMetrics.spec.ts
@@ -1,14 +1,13 @@
-import Immutable from 'immutable';
-
 import getScrollbarSize from '../getScrollbarSize';
 import * as ColumnMetrics from '../ColumnMetrics';
+import { Column } from '../common/types';
 
-const getAvailableWidthPerColumn = (totalWidth, consumedWidth, numberOfcolumns) => {
+function getAvailableWidthPerColumn(totalWidth: number, consumedWidth: number, numberOfcolumns: number): number {
   let availableWidth = totalWidth - getScrollbarSize() - consumedWidth;
   availableWidth = availableWidth % numberOfcolumns === 0 ? availableWidth : availableWidth - 1;
 
   return availableWidth / numberOfcolumns;
-};
+}
 
 describe('Column Metrics Tests', () => {
   describe('Creating metrics', () => {
@@ -27,7 +26,7 @@ describe('Column Metrics Tests', () => {
       }];
 
       it('should set the unset column widths based on the total width', () => {
-        const columns = getInitialColumns();
+        const columns = getInitialColumns() as Column[];
         const metrics = ColumnMetrics.recalculate({ columns, totalWidth, minColumnWidth: 50 });
         const expectedCalculatedWidth = getAvailableWidthPerColumn(totalWidth, columns[0].width, 2);
 
@@ -37,7 +36,7 @@ describe('Column Metrics Tests', () => {
       });
 
       it('should set the column left based on the column widths', () => {
-        const columns = getInitialColumns();
+        const columns = getInitialColumns() as Column[];
         const metrics = ColumnMetrics.recalculate({ columns, totalWidth, minColumnWidth: 50 });
         const expectedLeftValue = columns[0].width + getAvailableWidthPerColumn(totalWidth, columns[0].width, 2);
 
@@ -50,36 +49,12 @@ describe('Column Metrics Tests', () => {
         const firstFrozenColumn = { key: 'frozenColumn1', name: 'frozenColumn1', frozen: true };
         const secondFrozenColumn = { key: 'frozenColumn2', name: 'frozenColumn2', frozen: true };
         const thirdFrozenColumn = { key: 'frozenColumn3', name: 'frozenColumn3', frozen: true };
-        const columns = [...getInitialColumns(), secondFrozenColumn, thirdFrozenColumn];
-        columns.splice(2, 0, firstFrozenColumn);
+        const columns = [...getInitialColumns(), secondFrozenColumn, thirdFrozenColumn] as Column[];
+        columns.splice(2, 0, firstFrozenColumn as Column);
         const metrics = ColumnMetrics.recalculate({ columns, totalWidth, minColumnWidth: 50 });
         expect(metrics.columns[0]).toMatchObject(firstFrozenColumn);
         expect(metrics.columns[1]).toMatchObject(secondFrozenColumn);
         expect(metrics.columns[2]).toMatchObject(thirdFrozenColumn);
-      });
-
-      describe('When column data is immutable js object', () => {
-        const immutableColumns = new Immutable.List(getInitialColumns());
-
-        it('should set the unset column widths based on the total width', () => {
-          const columns = getInitialColumns();
-          const metrics = ColumnMetrics.recalculate({ columns: immutableColumns, totalWidth: 300, minColumnWidth: 50 });
-          const expectedCalculatedWidth = getAvailableWidthPerColumn(totalWidth, columns[0].width, 2);
-
-          expect(metrics.columns.get(0).width).toEqual(60);
-          expect(metrics.columns.get(1).width).toEqual(expectedCalculatedWidth);
-          expect(metrics.columns.get(2).width).toEqual(expectedCalculatedWidth);
-        });
-
-        it('should set the column left based on the column widths', () => {
-          const columns = getInitialColumns();
-          const metrics = ColumnMetrics.recalculate({ columns: immutableColumns, totalWidth: 300, minColumnWidth: 50 });
-          const expectedLeftValue = columns[0].width + getAvailableWidthPerColumn(totalWidth, columns[0].width, 2);
-
-          expect(metrics.columns.get(0).left).toEqual(0);
-          expect(metrics.columns.get(1).left).toEqual(columns[0].width);
-          expect(metrics.columns.get(2).left).toEqual(expectedLeftValue);
-        });
       });
     });
   });
