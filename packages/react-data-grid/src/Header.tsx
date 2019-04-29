d7fcca9a8c1e33fc5e871a831b49d6acd6028fb3
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-04-29 19:01:04 +0100

    tsify Header (#1615)
    * tsify Header
    
    * Address pr comments
    
    * Extra fix
    
    * Fix totalWidth type

---
+++ packages/react-data-grid/src/Header.tsx
@@ -0,0 +1,184 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classNames from 'classnames';
+
+import { resizeColumn } from './ColumnMetrics';
+import { getColumn } from './ColumnUtils';
+import HeaderRow, { HeaderRowProps } from './HeaderRow';
+import getScrollbarSize from './getScrollbarSize';
+import { HeaderRowType } from './common/enums';
+import { Column, ColumnMetrics, CellMetaData } from './common/types';
+
+interface HeaderRowData extends Pick<HeaderRowProps, 'filterable' | 'onFilterChange'> {
+  height?: number;
+  rowType: HeaderRowType;
+}
+
+type SharedHeaderRowProps = Pick<HeaderRowProps,
+'draggableHeaderCell'
+| 'getValidFilterValues'
+| 'sortDirection'
+| 'sortColumn'
+| 'onHeaderDrop'
+| 'onSort'
+| 'onScroll'
+>;
+
+export interface Props extends SharedHeaderRowProps {
+  columnMetrics: ColumnMetrics;
+  totalWidth: number | string;
+  height: number;
+  headerRows: HeaderRowData[];
+  onColumnResize(pos: number, width: number): void;
+  cellMetaData: CellMetaData;
+}
+
+interface State {
+  resizing: { column: Column; columnMetrics: ColumnMetrics } | null;
+}
+
+export default class Header extends React.Component<Props, State> {
+  readonly state: Readonly<State> = { resizing: null };
+
+  private readonly row = React.createRef<HeaderRow>();
+  private readonly filterRow = React.createRef<HeaderRow>();
+
+  componentWillReceiveProps(): void {
+    this.setState({ resizing: null });
+  }
+
+  onColumnResize = (column: Column, width: number): void => {
+    const pos = this.getColumnPosition(column);
+
+    if (pos === null) return;
+
+    const prevColumnMetrics = this.state.resizing ? this.state.resizing.columnMetrics : this.props.columnMetrics;
+    const columnMetrics = resizeColumn({ ...prevColumnMetrics }, pos, width);
+
+    // we don't want to influence scrollLeft while resizing
+    if (columnMetrics.totalWidth < prevColumnMetrics.totalWidth) {
+      columnMetrics.totalWidth = prevColumnMetrics.totalWidth;
+    }
+
+    this.setState({
+      resizing: {
+        column: getColumn(columnMetrics.columns, pos),
+        columnMetrics
+      }
+    });
+  };
+
+  onColumnResizeEnd = (column: Column, width: number): void => {
+    const pos = this.getColumnPosition(column);
+    if (pos === null) return;
+    this.props.onColumnResize(pos, width || column.width);
+  };
+
+  getHeaderRows() {
+    const columnMetrics = this.getColumnMetrics();
+
+    return this.props.headerRows.map((row, index) => {
+      // To allow header filters to be visible
+      const isFilterRow = row.rowType === HeaderRowType.FILTER;
+      const rowHeight = isFilterRow ? '500px' : 'auto';
+      const scrollbarSize = getScrollbarSize() > 0 ? getScrollbarSize() : 0;
+      const updatedWidth = typeof this.props.totalWidth === 'number'
+        ? this.props.totalWidth - scrollbarSize
+        : this.props.totalWidth;
+      const headerRowStyle: React.CSSProperties = {
+        position: 'absolute',
+        top: this.getCombinedHeaderHeights(index),
+        left: 0,
+        width: updatedWidth,
+        overflowX: 'hidden',
+        minHeight: rowHeight
+      };
+
+      return (
+        <HeaderRow
+          key={row.rowType}
+          ref={isFilterRow ? this.filterRow : this.row}
+          rowType={row.rowType}
+          style={headerRowStyle}
+          onColumnResize={this.onColumnResize}
+          onColumnResizeEnd={this.onColumnResizeEnd}
+          width={columnMetrics.width}
+          height={row.height || this.props.height}
+          columns={columnMetrics.columns}
+          draggableHeaderCell={this.props.draggableHeaderCell}
+          filterable={row.filterable}
+          onFilterChange={row.onFilterChange}
+          onHeaderDrop={this.props.onHeaderDrop}
+          sortColumn={this.props.sortColumn}
+          sortDirection={this.props.sortDirection}
+          onSort={this.props.onSort}
+          onScroll={this.props.onScroll}
+          getValidFilterValues={this.props.getValidFilterValues}
+        />
+      );
+    });
+  }
+
+  getColumnMetrics(): ColumnMetrics {
+    if (this.state.resizing) {
+      return this.state.resizing.columnMetrics;
+    }
+    return this.props.columnMetrics;
+  }
+
+  getColumnPosition(column: Column): number | null {
+    const { columns } = this.getColumnMetrics();
+    const idx = (columns as Column[]).findIndex(c => c.key === column.key);
+    return idx === -1 ? null : idx;
+  }
+
+  getCombinedHeaderHeights(until?: number): number {
+    const stopAt = typeof until === 'number'
+      ? until
+      : this.props.headerRows.length;
+
+    let height = 0;
+    for (let index = 0; index < stopAt; index++) {
+      height += this.props.headerRows[index].height || this.props.height;
+    }
+    return height;
+  }
+
+  setScrollLeft(scrollLeft: number): void {
+    const node = ReactDOM.findDOMNode(this.row.current) as Element;
+    node.scrollLeft = scrollLeft;
+    this.row.current!.setScrollLeft(scrollLeft);
+    if (this.filterRow.current) {
+      const nodeFilters = ReactDOM.findDOMNode(this.filterRow.current) as Element;
+      nodeFilters.scrollLeft = scrollLeft;
+      this.filterRow.current.setScrollLeft(scrollLeft);
+    }
+  }
+
+  // Set the cell selection to -1 x -1 when clicking on the header
+  onHeaderClick = (): void => {
+    this.props.cellMetaData.onCellClick({ rowIdx: -1, idx: -1 });
+  };
+
+  render() {
+    const className = classNames('react-grid-Header', {
+      'react-grid-Header--resizing': !!this.state.resizing
+    });
+    const { onScroll } = this.props;
+    const style: React.CSSProperties = {
+      position: 'relative',
+      height: this.getCombinedHeaderHeights()
+    };
+
+    return (
+      <div
+        onScroll={onScroll}
+        style={style}
+        className={className}
+        onClick={this.onHeaderClick}
+      >
+        {this.getHeaderRows()}
+      </div>
+    );
+  }
+}
--- packages/react-data-grid/src/__tests__/Header.spec.tsx
+++ packages/react-data-grid/src/__tests__/Header.spec.tsx
@@ -1,56 +1,66 @@
 import React from 'react';
-import Immutable from 'immutable';
 import { shallow } from 'enzyme';
 
-import Header from '../Header';
+import Header, { Props } from '../Header';
 import HeaderRow from '../HeaderRow';
 import helpers, { fakeCellMetaData } from '../helpers/test/GridPropHelpers';
 import * as GetScrollbarSize from '../getScrollbarSize';
+import { HeaderRowType, DEFINE_SORT } from '../common/enums';
+
 const SCROLL_BAR_SIZE = 17;
 
 describe('Header Unit Tests', () => {
-  let testProps;
-  beforeEach(() => {
-    jest.spyOn(GetScrollbarSize, 'default').mockReturnValue(SCROLL_BAR_SIZE);
-    testProps = {
+  function getProps(): Props {
+    return {
       columnMetrics: {
         columns: helpers.columns,
         minColumnWidth: 80,
-        totalWidth: true,
+        totalColumnWidth: 2600,
+        totalWidth: 2600,
         width: 2600
       },
       cellMetaData: fakeCellMetaData,
       totalWidth: 1000,
       height: 50,
-      headerRows: [{ height: 50, ref: 'row' }],
+      headerRows: [{
+        height: 50,
+        rowType: HeaderRowType.HEADER,
+        onFilterChange() {}
+      }],
       onColumnResize: jest.fn(),
-      onSort: () => null
+      onSort: () => null,
+      onHeaderDrop() {},
+      draggableHeaderCell: () => null
     };
+  }
+
+  beforeEach(() => {
+    jest.spyOn(GetScrollbarSize, 'default').mockReturnValue(SCROLL_BAR_SIZE);
   });
 
   function shouldRenderDefaultHeaderRow() {
-    const wrapper = shallow(<Header {...testProps} />);
+    const wrapper = shallow(<Header {...getProps()} />);
     expect(wrapper.find(HeaderRow).length).toBe(1);
   }
 
   function shouldSetResizeState() {
-    const wrapper = shallow(<Header {...testProps} />);
+    const wrapper = shallow<Header>(<Header {...getProps()} />);
     const resizeColIdx = 2;
     const newWidth = 350;
     const headerRow = wrapper.find(HeaderRow);
     headerRow.props().onColumnResize(helpers.columns[resizeColIdx], newWidth);
-    expect(wrapper.state().resizing.column.width).toEqual(newWidth);
-    expect(wrapper.state().resizing.column.key).toEqual(helpers.columns[resizeColIdx].key);
+    expect(wrapper.state().resizing!.column.width).toEqual(newWidth);
+    expect(wrapper.state().resizing!.column.key).toEqual(helpers.columns[resizeColIdx].key);
   }
 
   function shouldTriggerOnColumnResize() {
     const resizeColIdx = 1;
+    const testProps = getProps();
     const wrapper = shallow(<Header {...testProps} />);
     const headerRow = wrapper.find(HeaderRow);
     headerRow.props().onColumnResizeEnd(helpers.columns[resizeColIdx], 200);
     expect(testProps.onColumnResize).toHaveBeenCalled();
-    expect(testProps.onColumnResize.mock.calls[0][0]).toEqual(resizeColIdx);
-    expect(testProps.onColumnResize.mock.calls[0][1]).toEqual(200);
+    expect(testProps.onColumnResize).toHaveBeenCalledWith(resizeColIdx, 200);
   }
 
   it('should render a default header row', () => {
@@ -58,12 +68,12 @@ describe('Header Unit Tests', () => {
   });
 
   it('should initialize the state correctly', () => {
-    const wrapper = shallow(<Header {...testProps} />);
+    const wrapper = shallow<Header>(<Header {...getProps()} />);
     expect(wrapper.state().resizing).toEqual(null);
   });
 
   it('should render a default header row', () => {
-    const wrapper = shallow(<Header {...testProps} />);
+    const wrapper = shallow(<Header {...getProps()} />);
     expect(wrapper.find(HeaderRow).length).toBe(1);
   });
 
@@ -75,57 +85,55 @@ describe('Header Unit Tests', () => {
     shouldTriggerOnColumnResize();
   });
 
-  describe('When columns are immutable', () => {
-    beforeEach(() => {
-      testProps.columnMetrics.columns = new Immutable.List(helpers.columns);
-    });
-
-    it('should render a default header row', () => {
-      shouldRenderDefaultHeaderRow();
-    });
-
-    it('header row drag start should set resize column state ', () => {
-      shouldSetResizeState();
-    });
-
-    it('header row drag end should trigger onColumnResize callback', () => {
-      shouldTriggerOnColumnResize();
-    });
-  });
-
   describe('Rendering Header component', () => {
-    const renderComponent = (props) => {
-      return shallow(<Header {...props} />);
-    };
-    const testRequiredProps = {
+    function renderComponent(props: Props) {
+      return shallow<Header>(<Header {...props} />);
+    }
+    const testRequiredProps: Props = {
       columnMetrics: {
         columns: helpers.columns,
         minColumnWidth: 81,
-        totalWidth: true,
+        totalColumnWidth: 2600,
+        totalWidth: 2600,
         width: 2601
       },
       height: 51,
-      headerRows: [{ height: 51, ref: 'row' }],
-      onSort: jest.fn()
+      totalWidth: 2600,
+      headerRows: [{
+        height: 51,
+        rowType: HeaderRowType.HEADER,
+        onFilterChange() {}
+      }],
+      onSort: jest.fn(),
+      onHeaderDrop() {},
+      cellMetaData: fakeCellMetaData,
+      draggableHeaderCell: () => null,
+      onColumnResize() {}
     };
-    const testAllProps = {
+    const testAllProps: Props = {
       columnMetrics: {
         columns: helpers.columns,
         minColumnWidth: 80,
-        totalWidth: true,
+        totalColumnWidth: 2600,
+        totalWidth: 2600,
         width: 2600
       },
       totalWidth: 1000,
       height: 50,
-      headerRows: [{ height: 50, ref: 'row' }],
+      headerRows: [{
+        height: 50,
+        rowType: HeaderRowType.HEADER,
+        onFilterChange() {}
+      }],
       sortColumn: 'sortColumnValue',
-      sortDirection: 'DESC',
+      sortDirection: DEFINE_SORT.DESC,
       onSort: jest.fn(),
       onColumnResize: jest.fn(),
       onScroll: jest.fn(),
       draggableHeaderCell: jest.fn(),
       getValidFilterValues: jest.fn(),
-      cellMetaData: fakeCellMetaData
+      cellMetaData: fakeCellMetaData,
+      onHeaderDrop() {}
     };
     it('passes classname property', () => {
       const wrapper = renderComponent(testAllProps);
@@ -140,13 +148,8 @@ describe('Header Unit Tests', () => {
 
     it('should account for scrollbar size in header', () => {
       const wrapper = renderComponent(testAllProps);
-      const headerRow = wrapper.find('.react-grid-Header').props().children[0];
-      expect(headerRow.props.style.width).toBe(testAllProps.totalWidth - SCROLL_BAR_SIZE);
-    });
-    it('passes height property', () => {
-      const wrapper = renderComponent(testAllProps);
-      const headerDiv = wrapper.find('div');
-      expect(headerDiv.props().height).toBe(50);
+      const headerRow = wrapper.find('.react-grid-Header').childAt(0);
+      expect(headerRow.props().style.width).toBe(testAllProps.totalWidth as number - SCROLL_BAR_SIZE);
     });
     it('passes onScroll property, if available from props', () => {
       const wrapper = renderComponent(testAllProps);
@@ -158,27 +161,13 @@ describe('Header Unit Tests', () => {
       const headerDiv = wrapper.find('div');
       expect(headerDiv.props().onScroll).toBeUndefined();
     });
-    it('does not pass unknown properties to the div', () => {
-      const wrapper = renderComponent(testAllProps);
-      const headerDiv = wrapper.find('div');
-      expect(headerDiv.props().columnMetrics).toBeUndefined();
-      expect(headerDiv.props().totalWidth).toBeUndefined();
-      expect(headerDiv.props().headerRows).toBeUndefined();
-      expect(headerDiv.props().sortColumn).toBeUndefined();
-      expect(headerDiv.props().sortDirection).toBeUndefined();
-      expect(headerDiv.props().onSort).toBeUndefined();
-      expect(headerDiv.props().onColumnResize).toBeUndefined();
-      expect(headerDiv.props().draggableHeaderCell).toBeUndefined();
-      expect(headerDiv.props().getValidFilterValues).toBeUndefined();
-    });
 
     it('execute onCellClick event on cellMetaData and rowIdx & idx = -1', () => {
       jest.spyOn(testAllProps.cellMetaData, 'onCellClick').mockImplementation(() => {});
       const wrapper = renderComponent(testAllProps);
       const headerDiv = wrapper.find('div');
       headerDiv.simulate('click');
-      expect(testAllProps.cellMetaData.onCellClick).toHaveBeenCalled();
-      expect(testAllProps.cellMetaData.onCellClick.mock.calls[0][0]).toEqual({ rowIdx: -1, idx: -1 });
+      expect(testAllProps.cellMetaData.onCellClick).toHaveBeenCalledWith({ rowIdx: -1, idx: -1 });
     });
   });
 });
