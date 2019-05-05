4015a5d53a7913cbd8eb383c03c593242dfa7d89
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-05-05 02:51:53 +0100

    Cleanup

--- tests/ReactDataGrid.test.ts
+++ tests/ReactDataGrid.test.ts
@@ -1,8 +1,6 @@
 import { setup } from './utils';
 
-describe('<ReactDataGrid />', () => {
-  it('should render', () => {
-    const { container } = setup();
-    expect(container.querySelector('.react-grid-Container')).not.toBeNull();
-  });
+test('rendering <ReactDataGrid /> should work', () => {
+  const { container } = setup();
+  expect(container.querySelector('.react-grid-Container')).not.toBeNull();
 });
