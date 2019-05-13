7184622aa436d5549cf37e51e8d16d577ce6cea0
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-05-13 13:29:40 +0100

    Upstream the rdg props

--- packages/react-data-grid/src/ReactDataGrid.tsx
+++ packages/react-data-grid/src/ReactDataGrid.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Grid, { GridProps } from './Grid';
+import Grid from './Grid';
 import ToolbarContainer from './ToolbarContainer';
 import CheckboxEditor, { CheckboxEditorProps } from './common/editors/CheckboxEditor';
 import { SelectAll } from './formatters';
@@ -8,73 +8,36 @@ import * as rowUtils from './RowUtils';
 import { getSize } from './ColumnUtils';
 import KeyCodes from './KeyCodes';
 import { sameColumn, sameColumns, recalculate, resizeColumn } from './ColumnMetrics';
-import { CellNavigationMode, EventTypes, UpdateActions, HeaderRowType, DEFINE_SORT } from './common/enums';
+import { ScrollState } from './Viewport';
+import { RowsContainerProps } from './RowsContainer';
 import { EventBus } from './masks';
-import { Position, Column, CalculatedColumn, CellMetaData, InteractionMasksMetaData, ColumnMetrics, RowData, SelectedRange, RowSelection, HeaderRowData, AddFilterEvent, ColumnList, CommitEvent, GridRowsUpdatedEvent, RowSelectionParams } from './common/types';
+import { CellNavigationMode, EventTypes, UpdateActions, HeaderRowType, DEFINE_SORT } from './common/enums';
+import {
+  AddFilterEvent,
+  CalculatedColumn,
+  CellActionButton,
+  CellCopyPasteEvent,
+  CellMetaData,
+  CheckCellIsEditableEvent,
+  Column,
+  ColumnList,
+  ColumnMetrics,
+  CommitEvent,
+  GridRowsUpdatedEvent,
+  HeaderRowData,
+  InteractionMasksMetaData,
+  Position,
+  RowData,
+  RowExpandToggleEvent,
+  RowGetter,
+  RowSelection,
+  RowSelectionParams,
+  SelectedRange,
+  SubRowDetails,
+  SubRowOptions
+} from './common/types';
 
-type SharedGridProps = Pick<GridProps,
-/** The primary key property of each row */
-'rowKey'
-/** The height of each row in pixels */
-| 'rowHeight'
-| 'rowRenderer'
-| 'rowGroupRenderer'
-/** A function called for each rendered row that should return a plain key/value pair object */
-| 'rowGetter'
-/** The number of rows to be rendered */
-| 'rowsCount'
-/** The minimum height of the grid in pixels */
-| 'minHeight'
-/** When set, grid will scroll to this row index */
-| 'scrollToRowIndex'
-/** Component used to render a context menu. react-data-grid-addons provides a default context menu which may be used*/
-| 'contextMenu'
-/** Used to toggle whether cells can be selected or not */
-| 'enableCellSelect'
-/** Toggles whether cells should be autofocused */
-| 'enableCellAutoFocus'
-| 'cellNavigationMode'
-/** The node where the editor portal should mount. */
-| 'editorPortalTarget'
-/** The key of the column which is currently being sorted */
-| 'sortColumn'
-/** The direction to sort the sortColumn*/
-| 'sortDirection'
-/** Called when the grid is scrolled */
-| 'onScroll'
-/** Component used to render a draggable header cell */
-| 'draggableHeaderCell'
-| 'getValidFilterValues'
-| 'RowsContainer'
-| 'emptyRowsView'
-| 'onHeaderDrop'
-| 'getSubRowDetails'
->;
-
-type SharedCellMetaData = Pick<CellMetaData,
-/** Function called on each cell render to render a list of actions for each cell */
-'getCellActions'
-/** Called whenever a sub row is deleted from the grid */
-| 'onDeleteSubRow'
-/** Called whenever a sub row is added to the grid */
-| 'onAddSubRow'
-/** Function called whenever a cell has been expanded */
-| 'onCellExpand'
-| 'onRowExpandToggle'
->;
-
-type SharedInteractionMasksMetaData = Pick<InteractionMasksMetaData,
-/** Deprecated: Function called when grid is updated via a copy/paste. Use onGridRowsUpdated instead*/
-'onCellCopyPaste'
-/** Function called whenever a cell is selected */
-| 'onCellSelected'
-/** Function called whenever a cell is deselected */
-| 'onCellDeSelected'
-/** called before cell is set active, returns a boolean to determine whether cell is editable */
-| 'onCheckCellIsEditable'
->;
-
-interface Props extends SharedGridProps, SharedCellMetaData, SharedInteractionMasksMetaData {
+interface Props {
   /** An array of objects representing each column on the grid. Can also be an ImmutableJS object */
   columns: ColumnList;
   /** The minimum width of the grid in pixels */
@@ -100,14 +63,14 @@ interface Props extends SharedGridProps, SharedCellMetaData, SharedInteractionMa
   onRowClick?(rowIdx: number, rowData: RowData, column: CalculatedColumn): void;
   /** Function called whenever row is double clicked */
   onRowDoubleClick?(rowIdx: number, rowData: RowData, column: CalculatedColumn): void;
-  onAddFilter?(e: AddFilterEvent): void;
+  onAddFilter?(event: AddFilterEvent): void;
   onClearFilters?(): void;
   /** Function called whenever grid is sorted*/
-  onGridSort?: GridProps['onSort'];
+  onGridSort?(columnKey: string, direction: DEFINE_SORT): void;
   /** Function called whenever keyboard key is released */
-  onGridKeyUp?(e: React.KeyboardEvent<HTMLDivElement>): void;
+  onGridKeyUp?(event: React.KeyboardEvent<HTMLDivElement>): void;
   /** Function called whenever keyboard key is pressed down */
-  onGridKeyDown?(e: React.KeyboardEvent<HTMLDivElement>): void;
+  onGridKeyDown?(event: React.KeyboardEvent<HTMLDivElement>): void;
   onRowSelect?(rowData: RowData[]): void;
   columnEquality(c1: Column, c2: Column): boolean;
   rowSelection?: {
@@ -126,7 +89,7 @@ interface Props extends SharedGridProps, SharedCellMetaData, SharedInteractionMa
   /**
    * Callback called whenever row data is updated
    * When editing is enabled, this callback will be called for the following scenarios
-   * 1. Using the supplied editor of the column. The default editor is the [SimpleTextEditor](https://github.com/adazzle/react-data-grid/blob/master/packages/common/editors/SimpleTextEditor.js).
+   * 1. Using the supplied editor of the column. The default editor is the SimpleTextEditor.
    * 2. Copy/pasting the value from one cell to another <kbd>CTRL</kbd>+<kbd>C</kbd>, <kbd>CTRL</kbd>+<kbd>V</kbd>
    * 3. Update multiple cells by dragging the fill handle of a cell up or down to a destination cell.
    * 4. Update all cells under a given cell by double clicking the cell's fill handle.
@@ -134,6 +97,64 @@ interface Props extends SharedGridProps, SharedCellMetaData, SharedInteractionMa
   onGridRowsUpdated?<V>(event: GridRowsUpdatedEvent<V>): void;
   /** Called when a column is resized */
   onColumnResize?(idx: number, width: number): void;
+
+  /** Grid Props */
+  /** The primary key property of each row */
+  rowKey: string;
+  /** The height of each row in pixels */
+  rowHeight: number;
+  rowRenderer?: React.ReactElement | React.ComponentType;
+  rowGroupRenderer?: React.ComponentType;
+  /** A function called for each rendered row that should return a plain key/value pair object */
+  rowGetter: RowGetter;
+  /** The number of rows to be rendered */
+  rowsCount: number;
+  /** The minimum height of the grid in pixels */
+  minHeight: number;
+  /** When set, grid will scroll to this row index */
+  scrollToRowIndex?: number;
+  /** Component used to render a context menu. react-data-grid-addons provides a default context menu which may be used*/
+  contextMenu?: React.ReactElement;
+  /** Used to toggle whether cells can be selected or not */
+  enableCellSelect: boolean;
+  /** Toggles whether cells should be autofocused */
+  enableCellAutoFocus: boolean;
+  cellNavigationMode: CellNavigationMode;
+  /** The node where the editor portal should mount. */
+  editorPortalTarget: Element;
+  /** The key of the column which is currently being sorted */
+  sortColumn?: string;
+  /** The direction to sort the sortColumn*/
+  sortDirection?: DEFINE_SORT;
+  /** Called when the grid is scrolled */
+  onScroll?(scrollState: ScrollState): void;
+  /** Component used to render a draggable header cell */
+  draggableHeaderCell?: React.ComponentType<{ column: CalculatedColumn; onHeaderDrop(): void }>;
+  getValidFilterValues?(): void;
+  RowsContainer?: React.ComponentType<RowsContainerProps>;
+  emptyRowsView?: React.ComponentType<{}>;
+  onHeaderDrop?(): void;
+  getSubRowDetails?(): SubRowDetails;
+
+  /** CellMetaData */
+  getCellActions?(column: CalculatedColumn, rowData: RowData): CellActionButton[] | undefined;
+  /** Called whenever a sub row is deleted from the grid */
+  onDeleteSubRow?(options: SubRowOptions): void;
+  /** Called whenever a sub row is added to the grid */
+  onAddSubRow?(): void;
+  /** Function called whenever a cell has been expanded */
+  onCellExpand?(options: SubRowOptions): void;
+  onRowExpandToggle?(event: RowExpandToggleEvent): void;
+
+  /** InteractionMasksMetaData */
+  /** Deprecated: Function called when grid is updated via a copy/paste. Use onGridRowsUpdated instead*/
+  onCellCopyPaste?(event: CellCopyPasteEvent): void;
+  /** Function called whenever a cell is selected */
+  onCellSelected?(position: Position): void;
+  /** Function called whenever a cell is deselected */
+  onCellDeSelected?(position: Position): void;
+  /** called before cell is set active, returns a boolean to determine whether cell is editable */
+  onCheckCellIsEditable?(event: CheckCellIsEditableEvent): boolean;
 }
 
 type DefaultProps = Pick<Props,
@@ -150,7 +171,6 @@ type DefaultProps = Pick<Props,
 | 'editorPortalTarget'
 >;
 
-
 interface State {
   columnMetrics: ColumnMetrics;
   lastRowIdxUiSelected: number;
@@ -164,17 +184,12 @@ function isRowSelected(keys: unknown, indexes: unknown, isSelectedKey: unknown, 
   return rowUtils.isRowSelected(keys as { rowKey?: string; values?: string[] } | null, indexes as number[] | null, isSelectedKey as string | null, rowData, rowIdx);
 }
 
-/** Main API Component to render a data grid of rows and columns
+/**
+ * Main API Component to render a data grid of rows and columns
  *
- * Example code
- * -----
+ * @example
  *
- * ```javascript
- * <ReactDataGrid
- *   columns={columns}
- *   rowGetter={i => rows[i]}
- *   rowsCount={3} />
- * ```
+ * <ReactDataGrid columns={columns} rowGetter={i => rows[i]} rowsCount={3} />
 */
 export default class ReactDataGrid extends React.Component<Props, State> {
   static displayName = 'ReactDataGrid';
