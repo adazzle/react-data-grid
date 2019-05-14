0734df4dfbbda766eb40f68e2906ffd6147dc972
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-05-14 11:23:52 +0100

    Small ChildRowDeleteButton style cleanup

--- packages/react-data-grid/src/ChildRowDeleteButton.tsx
+++ packages/react-data-grid/src/ChildRowDeleteButton.tsx
@@ -8,15 +8,13 @@ export interface ChildRowDeleteButtonProps {
 }
 
 export default function ChildRowDeleteButton({ treeDepth, cellHeight, onDeleteSubRow, isDeleteSubRowEnabled }: ChildRowDeleteButtonProps) {
-  const height = 12;
-  const width = 12;
   const left = treeDepth * 15;
   const top = (cellHeight - 12) / 2;
   return (
     <div>
       <div className="rdg-child-row-action-cross" />
       {isDeleteSubRowEnabled && (
-        <div style={{ left, top, width, height }} className="rdg-child-row-btn" onClick={onDeleteSubRow}>
+        <div style={{ left, top }} className="rdg-child-row-btn" onClick={onDeleteSubRow}>
           <div className="glyphicon glyphicon-remove-sign" />
         </div>
       )}
