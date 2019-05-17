442bc29b1325695ffdd3b5ee3b91855ab2c45585
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-05-17 15:32:06 +0100

    tsify AdvancedToolbar

---
+++ packages/react-data-grid-addons/src/toolbars/AdvancedToolbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AdvancedToolbar({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="react-grid-Toolbar">
+      {children}
+      <div className="tools" />
+    </div>
+  );
+}
