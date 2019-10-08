272218e7899ed368eb93f6b8e817fbefaec6d3f3
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-10-08 20:56:01 +0100

    Proper page css

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route, Link } from 'react-router-dom';
+import { HashRouter, Route, NavLink } from 'react-router-dom';
 
 import Basic from './demos/example01-basic';
 import ResizableCols from './demos/example02-resizable-cols';
@@ -40,38 +40,38 @@ ReactDOM.render((
   <StrictMode>
     <HashRouter>
       <ul className="nav">
-        <li><Link to="/examples/basic">Basic Example</Link></li>
-        <li><Link to="/examples/resizable-cols">Resizable Cols Example</Link></li>
-        <li><Link to="/examples/frozen-cols">Frozen Cols Example</Link></li>
-        <li><Link to="/examples/editable">Editable Example</Link></li>
-        <li><Link to="/examples/custom-formatters">Custom Formatters Example</Link></li>
-        <li><Link to="/examples/built-in-editors">Built In Editors Example</Link></li>
-        <li><Link to="/examples/sortable-cols">Sortable Cols Example</Link></li>
-        <li><Link to="/examples/filterable-grid">Filterable Grid Example</Link></li>
-        <li><Link to="/examples/immutable-data">Immutable Data Example</Link></li>
-        <li><Link to="/examples/customRowRenderer">Custom Row Renderer Example</Link></li>
-        <li><Link to="/examples/all-features">All Features Example</Link></li>
-        <li><Link to="/examples/all-features-immutable">All Features Immutable Example</Link></li>
-        <li><Link to="/examples/empty-rows">Empty Rows Example</Link></li>
-        <li><Link to="/examples/cell-drag-down">Cell Drag Down Example</Link></li>
-        <li><Link to="/examples/filterable-sortable-grid">Filterable Sortable Grid Example</Link></li>
-        <li><Link to="/examples/row-select">Row Select Example</Link></li>
-        <li><Link to="/examples/grid-events">Grid Events Example</Link></li>
-        <li><Link to="/examples/context-menu">Context Menu Example</Link></li>
-        <li><Link to="/examples/column-events">Column Events Example</Link></li>
-        <li><Link to="/examples/cell-navigation">Cell Navigation Example</Link></li>
-        <li><Link to="/examples/cell-selection-events">Cell Selection Events</Link></li>
-        <li><Link to="/examples/grouping">Grouping Example</Link></li>
-        <li><Link to="/examples/custom-filters">Custom Filters Example</Link></li>
-        <li><Link to="/examples/immutable-data-grouping">Immutable Data Grouping Example</Link></li>
-        <li><Link to="/examples/draggable-header">Draggable Header Example</Link></li>
-        <li><Link to="/examples/tree-view">Tree View Example</Link></li>
-        <li><Link to="/examples/tree-view-no-add-delete">Tree View No Add Delete Example</Link></li>
-        <li><Link to="/examples/cell-actions">Cell Actions Example</Link></li>
-        <li><Link to="/examples/scroll-to-row-index">Scroll To Row Index Example</Link></li>
-        <li><Link to="/examples/descendingFirstSortable">Descending First Sortable Example</Link></li>
-        <li><Link to="/examples/selection-range-events">Selection Range Events Example</Link></li>
-        <li><Link to="/examples/isScrolling">IsScrolling Example</Link></li>
+        <li><NavLink to="/examples/basic">Basic Example</NavLink></li>
+        <li><NavLink to="/examples/resizable-cols">Resizable Cols Example</NavLink></li>
+        <li><NavLink to="/examples/frozen-cols">Frozen Cols Example</NavLink></li>
+        <li><NavLink to="/examples/editable">Editable Example</NavLink></li>
+        <li><NavLink to="/examples/custom-formatters">Custom Formatters Example</NavLink></li>
+        <li><NavLink to="/examples/built-in-editors">Built In Editors Example</NavLink></li>
+        <li><NavLink to="/examples/sortable-cols">Sortable Cols Example</NavLink></li>
+        <li><NavLink to="/examples/filterable-grid">Filterable Grid Example</NavLink></li>
+        <li><NavLink to="/examples/immutable-data">Immutable Data Example</NavLink></li>
+        <li><NavLink to="/examples/customRowRenderer">Custom Row Renderer Example</NavLink></li>
+        <li><NavLink to="/examples/all-features">All Features Example</NavLink></li>
+        <li><NavLink to="/examples/all-features-immutable">All Features Immutable Example</NavLink></li>
+        <li><NavLink to="/examples/empty-rows">Empty Rows Example</NavLink></li>
+        <li><NavLink to="/examples/cell-drag-down">Cell Drag Down Example</NavLink></li>
+        <li><NavLink to="/examples/filterable-sortable-grid">Filterable Sortable Grid Example</NavLink></li>
+        <li><NavLink to="/examples/row-select">Row Select Example</NavLink></li>
+        <li><NavLink to="/examples/grid-events">Grid Events Example</NavLink></li>
+        <li><NavLink to="/examples/context-menu">Context Menu Example</NavLink></li>
+        <li><NavLink to="/examples/column-events">Column Events Example</NavLink></li>
+        <li><NavLink to="/examples/cell-navigation">Cell Navigation Example</NavLink></li>
+        <li><NavLink to="/examples/cell-selection-events">Cell Selection Events</NavLink></li>
+        <li><NavLink to="/examples/grouping">Grouping Example</NavLink></li>
+        <li><NavLink to="/examples/custom-filters">Custom Filters Example</NavLink></li>
+        <li><NavLink to="/examples/immutable-data-grouping">Immutable Data Grouping Example</NavLink></li>
+        <li><NavLink to="/examples/draggable-header">Draggable Header Example</NavLink></li>
+        <li><NavLink to="/examples/tree-view">Tree View Example</NavLink></li>
+        <li><NavLink to="/examples/tree-view-no-add-delete">Tree View No Add Delete Example</NavLink></li>
+        <li><NavLink to="/examples/cell-actions">Cell Actions Example</NavLink></li>
+        <li><NavLink to="/examples/scroll-to-row-index">Scroll To Row Index Example</NavLink></li>
+        <li><NavLink to="/examples/descendingFirstSortable">Descending First Sortable Example</NavLink></li>
+        <li><NavLink to="/examples/selection-range-events">Selection Range Events Example</NavLink></li>
+        <li><NavLink to="/examples/isScrolling">IsScrolling Example</NavLink></li>
       </ul>
       <Route path="/examples/basic" component={Basic} />
       <Route path="/examples/resizable-cols" component={ResizableCols} />
