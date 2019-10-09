4aae932e9bc14a13d391d66427c4e29583eaa850
Aman Mahajan <amahajan@stratag.com>
2019-10-09 05:34:32 -0400

    Delete rowUtils (#1778)

--- packages/react-data-grid/src/utils/rowUtils.ts
+++
@@ -1,9 +0,0 @@
-import { RowData } from '../common/types';
-
-export function get<R>(row: R, property: keyof R) {
-  if (typeof (row as RowData).get === 'function') {
-    return (row as RowData).get!(property) as R[typeof property];
-  }
-
-  return row[property];
-}
