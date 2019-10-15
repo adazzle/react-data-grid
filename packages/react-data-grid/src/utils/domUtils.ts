ea579a9c70074a22b62c50b6eb1036329d524cd3
Li <25043398+qili26@users.noreply.github.com>
2019-10-14 21:24:27 -0500

    chores: remove dead code

--- packages/react-data-grid/src/utils/domUtils.ts
+++ packages/react-data-grid/src/utils/domUtils.ts
@@ -30,8 +30,3 @@ export function isPositionStickySupported(): boolean {
   }
   return positionSticky;
 }
-
-const userAgent = navigator.userAgent;
-export const isIE = userAgent.indexOf('MSIE ') > -1 || userAgent.indexOf('Trident/') > -1;
-export const isEdge = !isIE && !!window.StyleMedia;
-export const isIEOrEdge = isIE || isEdge;
