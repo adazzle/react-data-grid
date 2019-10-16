3aa00eb39a29fd58d51e18aa7ef8a8e3e5c7ac79
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-10-16 16:59:10 +0100

    Optimize Row getCells

--- packages/react-data-grid/src/Row.tsx
+++ packages/react-data-grid/src/Row.tsx
@@ -3,7 +3,8 @@ import classNames from 'classnames';
 
 import Cell from './Cell';
 import { isFrozen } from './utils/columnUtils';
-import { IRowRendererProps, CellRendererProps, CalculatedColumn } from './common/types';
+import { isPositionStickySupported } from './utils';
+import { IRowRendererProps } from './common/types';
 
 export default class Row<R> extends React.Component<IRowRendererProps<R>> {
   static displayName = 'Row';
@@ -32,33 +33,44 @@ export default class Row<R> extends React.Component<IRowRendererProps<R>> {
     e.preventDefault();
   };
 
-  getCell(column: CalculatedColumn<R>) {
-    const Renderer = this.props.cellRenderer!;
-    const { idx, cellMetaData, row, scrollLeft, isRowSelected, onRowSelectionChange } = this.props;
-    const { key } = column;
-
-    const cellProps: CellRendererProps<R> = {
-      idx: column.idx,
-      rowIdx: idx,
-      height: this.props.height,
-      column,
-      cellMetaData,
-      value: this.getCellValue(key || String(column.idx) as keyof R) as R[keyof R], // FIXME: fix types
-      rowData: row,
-      expandableOptions: this.getExpandableOptions(key),
-      scrollLeft,
-      isRowSelected,
-      onRowSelectionChange
-    };
-
-    return <Renderer key={`${key as keyof R}-${idx}`} {...cellProps} />; // FIXME: fix key type
-  }
-
   getCells() {
-    const { colOverscanStartIdx, colOverscanEndIdx, columns, lastFrozenColumnIndex } = this.props;
-    const frozenColumns = columns.slice(0, lastFrozenColumnIndex + 1);
-    const nonFrozenColumn = columns.slice(colOverscanStartIdx, colOverscanEndIdx + 1).filter(c => !isFrozen(c));
-    return nonFrozenColumn.concat(frozenColumns).map(c => this.getCell(c));
+    const {
+      cellMetaData,
+      columns,
+      height,
+      idx,
+      isRowSelected,
+      onRowSelectionChange,
+      row,
+      scrollLeft
+    } = this.props;
+    const Renderer = this.props.cellRenderer!;
+    const canSticky = isPositionStickySupported();
+
+    // FIXME: do we need this, considering columnsMetrics should have these columns sorted already?
+    const frozenColumns = columns.slice(0, this.props.lastFrozenColumnIndex + 1);
+    const nonFrozenColumn = columns.slice(this.props.colOverscanStartIdx, this.props.colOverscanEndIdx + 1).filter(c => !isFrozen(c));
+
+    return nonFrozenColumn.concat(frozenColumns).map(column => {
+      const { key } = column;
+
+      return (
+        <Renderer
+          key={`${key as keyof R}-${idx}`} // FIXME: fix key type
+          idx={column.idx}
+          rowIdx={idx}
+          height={height}
+          column={column}
+          cellMetaData={cellMetaData}
+          value={this.getCellValue(key || String(column.idx) as keyof R) as R[keyof R]} // FIXME: fix types
+          rowData={row}
+          expandableOptions={this.getExpandableOptions(key)}
+          scrollLeft={!canSticky && isFrozen(column) ? scrollLeft : undefined}
+          isRowSelected={isRowSelected}
+          onRowSelectionChange={onRowSelectionChange}
+        />
+      );
+    });
   }
 
   getCellValue(key: keyof R) {
