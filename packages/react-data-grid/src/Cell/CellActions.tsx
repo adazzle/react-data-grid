ac40f5ec1fa1d5b4cc9e35186b9df48faa1638bc
Nicolas Stepien <567105+MayhemYDG@users.noreply.github.com>
2019-10-18 15:49:22 +0100

    FC-ize Cell, move CellValue implementation in CellContent (#1797)

--- packages/react-data-grid/src/Cell/CellActions.tsx
+++ packages/react-data-grid/src/Cell/CellActions.tsx
@@ -4,18 +4,16 @@ import { CellMetaData } from '../common/types';
 import CellAction from './CellAction';
 import { CellProps } from '../Cell';
 
-type CellActionsProps<R> = Pick<CellProps<R>, 'column' | 'rowData'> & Pick<CellMetaData<R>, 'getCellActions'>;
+type CellActionsProps<R> = Pick<CellProps<R>, 'column' | 'rowData'> & Pick<Required<CellMetaData<R>>, 'getCellActions'>;
 
 export default function CellActions<R>({ getCellActions, column, rowData }: CellActionsProps<R>) {
-  if (getCellActions) {
-    const cellActionButtons = getCellActions(column, rowData);
-    if (cellActionButtons && cellActionButtons.length > 0) {
-      const actionButtons = cellActionButtons.map((action, index) => {
-        return <CellAction key={index} isFirst={index === 0} {...action} />;
-      });
+  const cellActionButtons = getCellActions(column, rowData);
+  if (cellActionButtons && cellActionButtons.length > 0) {
+    const actionButtons = cellActionButtons.map((action, index) => {
+      return <CellAction key={index} isFirst={index === 0} {...action} />;
+    });
 
-      return <>{actionButtons}</>;
-    }
+    return <>{actionButtons}</>;
   }
   return null;
 }
--- packages/react-data-grid/src/Cell/CellContent.tsx
+++ packages/react-data-grid/src/Cell/CellContent.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { createElement, cloneElement } from 'react';
+import { isElement, isValidElementType } from 'react-is';
 import classNames from 'classnames';
 
-import { CellMetaData } from '../common/types';
+import { SimpleCellFormatter } from '../formatters';
 import ChildRowDeleteButton from '../ChildRowDeleteButton';
+import { CellMetaData } from '../common/types';
 import { CellProps } from '../Cell';
-import CellValue from './CellValue';
 
 export type CellContentProps<R> = Pick<CellProps<R>,
 | 'idx'
@@ -40,6 +41,31 @@ export default function CellContent<R>({
   const treeDepth = expandableOptions ? expandableOptions.treeDepth : 0;
   const style = expandableOptions && isExpandCell ? { marginLeft: expandableOptions.treeDepth * 30 } : undefined;
 
+  function getFormatterDependencies(row: R) {
+    // convention based method to get corresponding Id or Name of any Name or Id property
+    const { getRowMetaData } = column;
+    if (getRowMetaData) {
+      if (process.env.NODE_ENV === 'development') {
+        console.warn('getRowMetaData for formatters is deprecated and will be removed in a future version of ReactDataGrid. Instead access row prop of formatter');
+      }
+      return getRowMetaData(row, column);
+    }
+  }
+
+  function getFormatterProps() {
+    return {
+      /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+      value: value as any, //FIXME: fix value type
+      column,
+      rowIdx,
+      row: rowData,
+      isRowSelected,
+      onRowSelectionChange,
+      dependentValues: getFormatterDependencies(rowData),
+      isSummaryRow
+    };
+  }
+
   function handleDeleteSubRow() {
     if (onDeleteSubRow) {
       onDeleteSubRow({
@@ -63,19 +89,19 @@ export default function CellContent<R>({
     { 'rdg-cell-tooltip': !!tooltip }
   );
 
+  const { formatter } = column;
+
   return (
     <div className={classes}>
       {cellDeleter}
       <div style={style}>
-        <CellValue<R>
-          rowIdx={rowIdx}
-          rowData={rowData}
-          column={column}
-          value={value}
-          isRowSelected={isRowSelected}
-          onRowSelectionChange={onRowSelectionChange}
-          isSummaryRow={isSummaryRow}
-        />
+        {formatter === undefined
+          ? <SimpleCellFormatter value={value as string} />
+          : isValidElementType(formatter)
+            ? createElement(formatter, getFormatterProps())
+            : isElement(formatter)
+              ? cloneElement(formatter, getFormatterProps())
+              : null}
         {cellControls}
       </div>
       {tooltip && <span className="rdg-cell-tooltip-text">{tooltip}</span>}
