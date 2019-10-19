f48a0405c386216850a3ea1cd02a8e3bbe1ce78f
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-10-19 23:18:19 +0100

    Simplify legacyCellContentRenderer

--- packages/react-data-grid/src/Cell/cellContentRenderers.tsx
+++ packages/react-data-grid/src/Cell/cellContentRenderers.tsx
@@ -3,9 +3,7 @@ import CellContent from './CellContent';
 import { CellContentRendererProps } from '../common/types';
 
 export function legacyCellContentRenderer<R>(props: CellContentRendererProps<R>) {
-  return props.isSummaryRow
-    ? <CellContent<R> {...props} isRowSelected={false} />
-    : <CellContent<R> {...props} />;
+  return React.createElement<CellContentRendererProps<R>>(CellContent, props);
 }
 
 export function valueCellContentRenderer<R>(props: CellContentRendererProps<R>) {
