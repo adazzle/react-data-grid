f1f2e4eca6649deff2cbacbf992e785765a70946
Nicolas Stepien <stepien.nicolas@gmail.com>
2019-10-25 12:29:44 +0100

    Fix missing box-sizing css on the root div

--- packages/react-data-grid/src/DataGrid.tsx
+++ packages/react-data-grid/src/DataGrid.tsx
@@ -412,7 +412,7 @@ function DataGrid<R, K extends keyof R>({
   return (
     <div
       className="rdg-root"
-      style={{ width: width === undefined ? undefined : width - 2, lineHeight: `${rowHeight}px` }}
+      style={{ width, lineHeight: `${rowHeight}px` }}
       ref={gridRef}
     >
       {columnMetrics && (
