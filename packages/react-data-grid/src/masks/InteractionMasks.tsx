e7460620e068566e5f3a0540bb2408b85bc166b7
jiosif <jiosif@stratag.com>
2019-11-08 16:24:59 +0000

    fix dragging issue

--- packages/react-data-grid/src/masks/InteractionMasks.tsx
+++ packages/react-data-grid/src/masks/InteractionMasks.tsx
@@ -600,8 +600,8 @@ export default class InteractionMasks<R> extends React.Component<InteractionMask
     const column = columns[draggedPosition.idx];
     const value = getSelectedCellValue({ selectedPosition: draggedPosition, columns, rowGetter });
     const cellKey = column.key;
-    const fromRow = rowIdx < overRowIdx ? rowIdx : overRowIdx;
-    const toRow = rowIdx > overRowIdx ? rowIdx : overRowIdx;
+    const fromRow = rowIdx;
+    const toRow = overRowIdx;
 
     onGridRowsUpdated(cellKey, fromRow, toRow, { [cellKey]: value }, UpdateActions.CELL_DRAG);
 
