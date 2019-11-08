5bd8054e47e5d9112ebd9cbecfae05e31e92603a
Li <25043398+qili26@users.noreply.github.com>
2019-11-08 11:30:27 -0600

    chores: export more hooks

--- packages/react-data-grid/src/common/editors/index.ts
+++ packages/react-data-grid/src/common/editors/index.ts
@@ -1,2 +1,3 @@
 export { default as SimpleTextEditor } from './SimpleTextEditor';
 export { DataGridEditor, useDataGridEditor, useInputNodeSelect, useKeyDownPrevention, useNodeFocus, withDataGridEditor } from './useDataGridEditor';
+export { useDataGridEditorContext } from './NewEditorContainer';
