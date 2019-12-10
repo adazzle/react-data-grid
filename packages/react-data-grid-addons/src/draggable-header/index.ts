223e3de2ffb379be81b63a01160b05327dbab368
Li <25043398+qili26@users.noreply.github.com>
2019-12-10 13:44:22 -0600

    chores: initial drag re-implementation work

--- packages/react-data-grid-addons/src/draggable-header/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as DraggableContainer } from './DraggableContainer';
---
+++ packages/react-data-grid/src/Header/DraggableContainer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import DraggableHeaderCell from './DraggableHeaderCell';
+
+function withDragDropContext<TProps extends {}>(Comp: React.ComponentType<TProps>) {
+  function DragDropContext(props: TProps): JSX.Element {
+    return (
+      <DndProvider backend={HTML5Backend}>
+        <Comp {...props} />
+      </DndProvider>
+    );
+  }
+
+  (DragDropContext as React.FC).displayName = `DragDropContext_${Comp.displayName}`;
+  return DragDropContext;
+}
+
+interface Props {
+  children: React.ReactElement;
+  onHeaderDrop(source: number, target: number): void;
+}
+
+export default withDragDropContext(function DraggableContainer(props: Props): JSX.Element {
+  return React.cloneElement(
+    React.Children.only(props.children), {
+      ...props,
+      draggableHeaderCell: DraggableHeaderCell
+    }
+  );
+});
