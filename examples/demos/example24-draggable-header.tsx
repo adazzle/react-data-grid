4e9ea2744b7e7f1cfbf6e4ba37ad5490c02b288b
Li <25043398+qili26@users.noreply.github.com>
2019-12-20 11:23:27 -0600

    chores: re-implement the draggable header example to functional component

--- examples/demos/example24-draggable-header.tsx
+++ examples/demos/example24-draggable-header.tsx
@@ -1,70 +1,74 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import DataGrid, { Column } from '../../src';
 import Wrapper from './Wrapper';
 
-export default class extends React.Component {
-  createRows = () => {
-    const rows = [];
-    for (let i = 1; i < 1000; i++) {
-      rows.push({
-        id: i,
-        title: `Title ${i}`,
-        count: i * 1000
-      });
-    }
-
-    return rows;
-  };
-
-  rowGetter = (i: number) => {
-    return this.state.rows[i];
-  };
-
-  onHeaderDrop = (source: string, target: string) => {
-    const columns = [...this.state.columns];
-    const columnSourceIndex = columns.findIndex(i => i.key === source);
-    const columnTargetIndex = columns.findIndex(i => i.key === target);
-    const temp = { ...columns[columnSourceIndex] };
-    columns[columnSourceIndex] = { ...columns[columnTargetIndex] };
-    columns[columnTargetIndex] = temp;
-    this.setState({ columns });
-  };
-
-  state = {
-    columns: [
-      {
-        key: 'id',
-        name: 'ID',
-        width: 50,
-        draggable: true
-      },
-      {
-        key: 'title',
-        name: 'Title',
-        draggable: true,
-        resizable: true
-      },
-      {
-        key: 'count',
-        name: 'Count',
-        resizable: true
-      }
-    ],
-    rows: this.createRows()
-  };
-
-  render() {
-    return (
-      <Wrapper title="Drag Columns to Reorder">
-        <DataGrid
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          columns={this.state.columns as Column<any, any>[]}
-          rowGetter={this.rowGetter}
-          rowsCount={this.state.rows.length}
-          minHeight={500}
-          onHeaderDrop={this.onHeaderDrop}
-        />
-      </Wrapper>
-    );
-  }
+interface Row {
+  id: number;
+  title: string;
+  count: number;
 }
+
+type RowFields = keyof Row;
+
+function createRows(): Row[] {
+  const rows: Row[] = [];
+
+  for (let i = 1; i < 1000; i++) {
+    rows.push({
+      id: i,
+      title: `Title ${i}`,
+      count: i * 1000
+    });
+  }
+
+  return rows;
+}
+
+function DraggableHeaderExample() {
+  const [rows] = useState<Row[]>(createRows);
+  const [columns, setColumns] = useState<Column<Row>[]>([
+    {
+      key: 'id',
+      name: 'ID',
+      width: 50,
+      draggable: true
+    },
+    {
+      key: 'title',
+      name: 'Title',
+      draggable: true,
+      resizable: true
+    },
+    {
+      key: 'count',
+      name: 'Count',
+      resizable: true
+    }
+  ]);
+
+  const onHeaderDrop = useCallback((source: string, target: string) => {
+    const newColumns = [...columns];
+    const columnSourceIndex = newColumns.findIndex(i => i.key === source);
+    const columnTargetIndex = newColumns.findIndex(i => i.key === target);
+    const temp = { ...newColumns[columnSourceIndex] };
+    newColumns[columnSourceIndex] = { ...newColumns[columnTargetIndex] };
+    newColumns[columnTargetIndex] = temp;
+    setColumns(newColumns);
+  }, [columns]);
+
+  const rowGetter = useCallback((i: number) => rows[i], [rows]);
+
+  return (
+    <Wrapper title="Drag Columns to Reorder">
+      <DataGrid
+        columns={columns}
+        rowGetter={rowGetter}
+        rowsCount={rows.length}
+        minHeight={500}
+        onHeaderDrop={onHeaderDrop}
+      />
+    </Wrapper>
+  );
+}
+
+export default DraggableHeaderExample;
