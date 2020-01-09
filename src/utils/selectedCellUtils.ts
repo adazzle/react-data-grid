5e97726e5e52007b0ee27ddb991d3c6530081204
Aman Mahajan <amahajan@stratag.com>
2020-01-09 07:49:57 -0600

    Do not copy object
    Co-Authored-By: Nicolas Stepien <567105+nstepien@users.noreply.github.com>

--- src/utils/selectedCellUtils.ts
+++ src/utils/selectedCellUtils.ts
@@ -125,7 +125,7 @@ export function getNextSelectedCellPosition<R>({ cellNavigationMode, columns, ro
     }
   }
 
-  return { ...nextPosition };
+  return nextPosition;
 }
 
 interface canExitGridOpts<R> {
