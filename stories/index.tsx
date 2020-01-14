960a7f47f59736504edfb5e5ef5fd00669ea292e
Nicolas Stepien <stepien.nicolas@gmail.com>
2020-01-14 15:21:42 +0000

    stories

--- stories/index.tsx
+++ stories/index.tsx
@@ -6,6 +6,8 @@ import { storiesOf } from '@storybook/react';
 
 import AllFeatures from './demos/AllFeatures';
 import Basic from './demos/Basic';
+import Editable from './demos/Editable';
+import CustomFormatters from './demos/CustomFormatters';
 import CellActions from './demos/CellActions';
 import FrozenColumns from './demos/FrozenColumns';
 import ResizableColumns from './demos/ResizableColumns';
@@ -14,6 +16,8 @@ import TreeView from './demos/TreeView';
 storiesOf('Demos', module)
   .add('All Features', () => <AllFeatures />)
   .add('Basic', () => <Basic />)
+  .add('Editable', () => <Editable />)
+  .add('Custom Formatters', () => <CustomFormatters />)
   .add('Resizable Columns', () => <ResizableColumns />)
   .add('Frozen Columns', () => <FrozenColumns />)
   .add('Cell Actions', () => <CellActions />)
