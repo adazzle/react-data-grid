d8abaec1b9376ffff11afdad5625e0779b5d0b46
Nicolas Stepien <stepien.nicolas@gmail.com>
2020-01-15 12:34:43 +0000

    tsify empty rows demo

---
+++ stories/demos/NoRows.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import DataGrid, { Column } from '../../src';
+
+function EmptyRowsView() {
+  return <div style={{ textAlign: 'center' }}>Nothing to show <span lang="ja" title="ショボーン">(´・ω・`)</span></div>;
+}
+
+interface Row {
+  id: number;
+  title: string;
+  count: number;
+}
+
+const columns: readonly Column<Row>[] = [
+  { key: 'id', name: 'ID' },
+  { key: 'title', name: 'Title' },
+  { key: 'count', name: 'Count' }
+];
+
+const rows: readonly Row[] = [];
+
+export default function NoRows() {
+  return (
+    <DataGrid
+      columns={columns}
+      rows={rows}
+      width={600}
+      emptyRowsView={EmptyRowsView}
+    />
+  );
+}
