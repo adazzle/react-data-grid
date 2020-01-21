ba3270cbab5ce6af4dd5a4c4ec131deb9eeaf406
Nicolas Stepien <stepien.nicolas@gmail.com>
2020-01-21 13:36:07 +0000

    Close TextEditor on escape/enter key press

--- src/editors/TextEditor.tsx
+++ src/editors/TextEditor.tsx
@@ -4,7 +4,8 @@ import { EditorProps2 } from '../common/types';
 export default function TextEditor<R>({
   column,
   row,
-  onChange
+  onChange,
+  onClose
 }: EditorProps2<R>) {
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -12,12 +13,24 @@ export default function TextEditor<R>({
     inputRef.current!.focus();
   }, []);
 
+  function onKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    switch (event.key) {
+      case 'Escape':
+      case 'Enter':
+        onClose();
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <input
       ref={inputRef}
       className="rdg-text-editor"
       value={row[column.key] as unknown as string} // TODO: fix type?
       onChange={event => onChange({ [column.key]: event.target.value } as {})} // TODO: fix type?
+      onKeyDown={onKeyDown}
     />
   );
 }
