9c38c4bfbc5565c3fa92723f290ec2a4e61d38d2
Nicolas Stepien <567105+nstepien@users.noreply.github.com>
2020-02-12 16:06:02 +0000

    Remove HeaderCell export (#1951)
    Co-authored-by: Aman Mahajan <nova.aman@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { default, DataGridProps, DataGridHandle } from './DataGrid';
 export { default as Cell } from './Cell';
 export { default as Row } from './Row';
-export { default as HeaderCell } from './HeaderCell';
 export * from './Columns';
 export * from './formatters';
 export * from './editors';
