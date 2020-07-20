470ad50ce8911d7b52652d2c7cfed186e1cf3da5
Mahajan <amahajan@freewheel.com>
2020-07-20 10:27:33 -0500

    Inline getViewportColumns and getHorizontalRangeToRender

--- src/hooks/useViewportColumns.ts
+++ src/hooks/useViewportColumns.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 
 import { CalculatedColumn } from '../common/types';
-import { getColumnMetrics, getHorizontalRangeToRender, getViewportColumns } from '../utils';
+import { getColumnMetrics } from '../utils';
 import { DataGridProps } from '../DataGrid';
 
 type SharedDataGridProps<R, K extends keyof R, SR> =
@@ -12,6 +12,14 @@ interface ViewportColumnsArgs<R, K extends keyof R, SR> extends SharedDataGridPr
   viewportWidth: number;
   scrollLeft: number;
   columnWidths: ReadonlyMap<string, number>;
+}
+
+function getTotalFrozenColumnWidth<R, SR>(columns: readonly CalculatedColumn<R, SR>[], lastFrozenColumnIndex: number): number {
+  if (lastFrozenColumnIndex === -1) {
+    return 0;
+  }
+  const lastFrozenColumn = columns[lastFrozenColumnIndex];
+  return lastFrozenColumn.left + lastFrozenColumn.width;
 }
 
 export function useViewportColumns<R, K extends keyof R, SR>({
@@ -32,21 +40,61 @@ export function useViewportColumns<R, K extends keyof R, SR>({
     });
   }, [columnWidths, rawColumns, defaultFormatter, minColumnWidth, viewportWidth]);
 
+  // Get horizontal range to render
   const [colOverscanStartIdx, colOverscanEndIdx] = useMemo((): [number, number] => {
-    return getHorizontalRangeToRender(
-      columns,
-      lastFrozenColumnIndex,
-      viewportWidth,
-      scrollLeft
-    );
+    // get the viewport's left side and right side positions for non-frozen columns
+    const totalFrozenColumnWidth = getTotalFrozenColumnWidth(columns, lastFrozenColumnIndex);
+    const viewportLeft = scrollLeft + totalFrozenColumnWidth;
+    const viewportRight = scrollLeft + viewportWidth;
+    // get first and last non-frozen column indexes
+    const lastColIdx = columns.length - 1;
+    const firstUnfrozenColumnIdx = Math.min(lastFrozenColumnIndex + 1, lastColIdx);
+
+    // skip rendering non-frozen columns if the frozen columns cover the entire viewport
+    if (viewportLeft >= viewportRight) {
+      return [firstUnfrozenColumnIdx, firstUnfrozenColumnIdx];
+    }
+
+    // get the first visible non-frozen column index
+    let colVisibleStartIdx = firstUnfrozenColumnIdx;
+    while (colVisibleStartIdx < lastColIdx) {
+      const { left, width } = columns[colVisibleStartIdx];
+      // if the right side of the columnn is beyond the left side of the available viewport,
+      // then it is the first column that's at least partially visible
+      if (left + width > viewportLeft) {
+        break;
+      }
+      colVisibleStartIdx++;
+    }
+
+    // get the last visible non-frozen column index
+    let colVisibleEndIdx = colVisibleStartIdx;
+    while (colVisibleEndIdx < lastColIdx) {
+      const { left, width } = columns[colVisibleEndIdx];
+      // if the right side of the column is beyond or equal to the right side of the available viewport,
+      // then it the last column that's at least partially visible, as the previous column's right side is not beyond the viewport.
+      if (left + width >= viewportRight) {
+        break;
+      }
+      colVisibleEndIdx++;
+    }
+
+    const colOverscanStartIdx = Math.max(firstUnfrozenColumnIdx, colVisibleStartIdx - 1);
+    const colOverscanEndIdx = Math.min(lastColIdx, colVisibleEndIdx + 1);
+
+    return [colOverscanStartIdx, colOverscanEndIdx];
   }, [scrollLeft, columns, lastFrozenColumnIndex, viewportWidth]);
 
   const viewportColumns = useMemo((): readonly CalculatedColumn<R, SR>[] => {
-    return getViewportColumns(
-      columns,
-      colOverscanStartIdx,
-      colOverscanEndIdx
-    );
+    const viewportColumns: CalculatedColumn<R, SR>[] = [];
+    for (let colIdx = 0; colIdx <= colOverscanEndIdx; colIdx++) {
+      const column = columns[colIdx];
+
+      if (colIdx < colOverscanStartIdx && !column.frozen) continue;
+      viewportColumns.push(column);
+    }
+
+    return viewportColumns;
   }, [colOverscanEndIdx, colOverscanStartIdx, columns]);
 
   return { columns, viewportColumns, totalColumnWidth, lastFrozenColumnIndex };
