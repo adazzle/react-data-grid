8bd664f8cbfaf32db00722c68079530ea9db2032
Nicolas Stepien <stepien.nicolas@gmail.com>
2020-08-25 13:17:16 +0100

    fix tests

--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -21,6 +21,9 @@ export function useGridDimensions(): [React.RefObject<HTMLDivElement>, number, n
   useLayoutEffect(() => {
     const { ResizeObserver } = window as typeof window & { ResizeObserver: ResizeObserver };
 
+    // don't break in jest/jsdom and browsers that don't support ResizeObserver
+    if (ResizeObserver == null) return;
+
     const resizeObserver = new ResizeObserver(entries => {
       const { width, height } = entries[0].contentRect;
       setGridWidth(width);
