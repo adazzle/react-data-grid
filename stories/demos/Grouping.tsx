73a4ce9b9bab484ded31b1a89e0dea01476c579f
Mahajan <amahajan@freewheel.com>
2020-08-25 05:44:51 -0500

    Use SortableSelect

--- stories/demos/Grouping.tsx
+++ stories/demos/Grouping.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useMemo } from 'react';
 import { AutoSizer } from 'react-virtualized';
 import { groupBy as rowGrouper } from 'lodash';
-import Select from 'react-select';
+import Select, { components } from 'react-select';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import faker from 'faker';
 
 import DataGrid, { Column, Row, SelectColumn } from '../../src';
@@ -88,10 +89,23 @@ function createRows(): Row[] {
   return rows.sort((r1, r2) => r2.country.localeCompare(r1.country));
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const SortableMultiValue = SortableElement((props: any) => {
+  const onMouseDown = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const innerProps = { onMouseDown };
+  return <components.MultiValue {...props} innerProps={innerProps} />;
+});
+
+const SortableSelect = SortableContainer(Select);
+
 const options = [
   { value: 'country', label: 'Country' },
   { value: 'year', label: 'Year' },
-  { value: 'sport', label: 'Sport' }
+  { value: 'sport', label: 'Sport' },
+  { value: 'athlete', label: 'athlete' }
 ];
 
 export default function Grouping() {
@@ -102,11 +116,24 @@ export default function Grouping() {
 
   const groupBy = useMemo(() => selectedOptions === null ? undefined : selectedOptions.map(o => o.value), [selectedOptions]);
 
+  function onSortEnd({ oldIndex, newIndex }: { oldIndex: number; newIndex: number }) {
+    const newOptions = [...selectedOptions];
+    newOptions.splice(newIndex < 0 ? newOptions.length + newIndex : newIndex, 0, newOptions.splice(oldIndex, 1)[0]);
+    setSelectedOptions(newOptions);
+  }
+
   return (
     <>
       <label style={{ width: 400, marginBottom: 24 }}>
-        Group by
-        <Select
+        <b>Group by</b>
+        <SortableSelect
+          // react-sortable-hoc props
+          axis="xy"
+          onSortEnd={onSortEnd}
+          distance={4}
+          getHelperDimensions={({ node }) => node.getBoundingClientRect()}
+          // react-select props:
+          isMulti
           value={selectedOptions}
           onChange={options => {
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -114,7 +141,10 @@ export default function Grouping() {
             setExpandedGroupIds(new Set());
           }}
           options={options}
-          isMulti
+          components={{
+            MultiValue: SortableMultiValue
+          }}
+          closeMenuOnSelect={false}
         />
       </label>
       <AutoSizer>
