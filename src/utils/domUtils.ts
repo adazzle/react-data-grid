52bdee7d0a17d9170f37b72606862b1c2a1e5bb2
Mahajan <amahajan@freewheel.com>
2020-08-31 12:21:39 -0500

    Add stopPropagation

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -2,6 +2,10 @@ export function preventDefault(event: React.SyntheticEvent) {
   event.preventDefault();
 }
 
+export function stopPropagation(event: React.SyntheticEvent) {
+  event.stopPropagation();
+}
+
 export function wrapEvent<E extends React.SyntheticEvent>(ourHandler: React.EventHandler<E>, theirHandler: React.EventHandler<E> | undefined) {
   if (theirHandler === undefined) return ourHandler;
 
