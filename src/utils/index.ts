4676d85d7fcb4822233dc3652cb830bf3f460a8d
Nicolas Stepien <stepien.nicolas@gmail.com>
2020-10-28 15:47:35 +0000

    inline wrapRefs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,17 +8,3 @@ export function assertIsValidKeyGetter<R>(keyGetter: unknown): asserts keyGetter
     throw new Error('Please specify the rowKeyGetter prop to use selection');
   }
 }
-
-export function wrapRefs<T>(...refs: readonly React.Ref<T>[]) {
-  return (handle: T | null) => {
-    for (const ref of refs) {
-      if (typeof ref === 'function') {
-        ref(handle);
-      } else if (ref !== null) {
-        // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/31065
-        // @ts-expect-error
-        ref.current = handle;
-      }
-    }
-  };
-}
