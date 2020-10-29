9e6ef0fe3945b234fb764f262ee3fa22386006c2
Nicolas Stepien <stepien.nicolas@gmail.com>
2020-10-29 14:19:14 +0000

    static columns

--- stories/demos/Resizable.tsx
+++ stories/demos/Resizable.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import DataGrid, { Column, FormatterProps } from '../../src';
 
 type Row = undefined;
@@ -8,22 +8,18 @@ function CellFormatter(props: FormatterProps<Row>) {
   return <>{props.column.key}&times;{props.rowIdx}</>;
 }
 
+const columns: Column<Row>[] = [];
+
+for (let i = 0; i < 50; i++) {
+  const key = String(i);
+  columns.push({
+    key,
+    name: key,
+    formatter: CellFormatter
+  });
+}
+
 export function ResizableGrid() {
-  const columns = useMemo((): readonly Column<Row>[] => {
-    const columns: Column<Row>[] = [];
-
-    for (let i = 0; i < 50; i++) {
-      const key = String(i);
-      columns.push({
-        key,
-        name: key,
-        formatter: CellFormatter
-      });
-    }
-
-    return columns;
-  }, []);
-
   return (
     <DataGrid
       columns={columns}
