5e48b79b677354532463ae00666b0294078fa5fc
Nicolas Stepien <stepien.nicolas@gmail.com>
2020-11-05 16:58:00 +0000

    Tweak comment

--- src/utils/columnUtils.ts
+++ src/utils/columnUtils.ts
@@ -194,7 +194,7 @@ export function getColumnScrollPosition<R, SR>(columns: readonly CalculatedColum
  * By default, the following navigation keys are enabled while an editor is open, under specific conditions:
  * - Tab:
  *   - The editor must be an <input>, a <textarea>, or a <select> element.
- *   - The editor element must be the only immmediate child of the editor container.
+ *   - The editor element must be the only immediate child of the editor container/a label.
  */
 export function onEditorNavigation({ key, target }: React.KeyboardEvent<HTMLDivElement>): boolean {
   if (key === 'Tab' && (target instanceof HTMLInputElement || target instanceof HTMLTextAreaElement || target instanceof HTMLSelectElement)) {
