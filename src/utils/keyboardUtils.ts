539c38cdc4bc3f9a52b58d41404470dbe2144bce
Nicolas Stepien <567105+nstepien@users.noreply.github.com>
2020-11-11 16:55:15 +0000

    isDefaultCellInput: use a list of disallowed keys (#2225)

--- src/utils/keyboardUtils.ts
+++ src/utils/keyboardUtils.ts
@@ -1,16 +1,57 @@
-export function isKeyPrintable(keycode: number): boolean {
-  return (keycode > 47 && keycode < 58) // number keys
-    || keycode === 32 || keycode === 13 // spacebar & return key(s) (if you want to allow carriage returns)
-    || (keycode > 64 && keycode < 91) // letter keys
-    || (keycode > 95 && keycode < 112) // numpad keys
-    || (keycode > 185 && keycode < 193) // ;=,-./` (in order)
-    || (keycode > 218 && keycode < 223); // [\]' (in order)
-}
+// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
+const nonInputKeys = new Set([
+  // Special keys
+  'Unidentified',
+  // Modifier keys
+  'Alt',
+  'AltGraph',
+  'CapsLock',
+  'Control',
+  'Fn',
+  'FnLock',
+  'Meta',
+  'NumLock',
+  'ScrollLock',
+  'Shift',
+  // Whitespace keys
+  'Tab',
+  // Navigation keys
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'ArrowUp',
+  'End',
+  'Home',
+  'PageDown',
+  'PageUp',
+  // Editing
+  'Insert',
+  // UI keys
+  'ContextMenu',
+  'Escape',
+  'Pause',
+  'Play',
+  // Device keys
+  'PrintScreen',
+  // Function keys
+  'F1',
+  // 'F2', /!\ specifically allowed, do not edit
+  'F3',
+  'F4',
+  'F5',
+  'F6',
+  'F7',
+  'F8',
+  'F9',
+  'F10',
+  'F11',
+  'F12'
+]);
 
 export function isCtrlKeyHeldDown(e: React.KeyboardEvent): boolean {
   return (e.ctrlKey || e.metaKey) && e.key !== 'Control';
 }
 
-export function isDefaultCellInput(event: React.KeyboardEvent<HTMLDivElement>) {
-  return isKeyPrintable(event.keyCode) || ['Enter', 'F2', 'Backspace', 'Delete'].includes(event.key);
+export function isDefaultCellInput(event: React.KeyboardEvent<HTMLDivElement>): boolean {
+  return !nonInputKeys.has(event.key);
 }
