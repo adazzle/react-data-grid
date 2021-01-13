49ad82b7b678cc71355baaf603a2e85e46d6b141
Nicolas Stepien <stepien.nicolas@gmail.com>
2021-01-13 14:09:05 +0000

    typescript@4.2

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,8 +3,6 @@ import '../style/index.less';
 import '../stories/index.less';
 import { ResizeObserver as Polyfill } from '@juggle/resize-observer';
 
-// @ts-expect-error
 if (typeof ResizeObserver === 'undefined') {
-  // @ts-expect-error
   window.ResizeObserver = Polyfill;
 }
--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -1,25 +1,12 @@
 import { useRef, useState, useLayoutEffect } from 'react';
 
-// https://github.com/microsoft/TypeScript/issues/37861
-interface ResizeObserverEntry {
-  contentRect: {
-    width: number;
-    height: number;
-  };
-}
-
-type ResizeObserver = new (callback: (entries: readonly ResizeObserverEntry[]) => void) => {
-  observe: (target: Element) => void;
-  disconnect: () => void;
-};
-
-export function useGridDimensions(): [React.RefObject<HTMLDivElement>, number, number] {
+export function useGridDimensions() {
   const gridRef = useRef<HTMLDivElement>(null);
   const [gridWidth, setGridWidth] = useState(1);
   const [gridHeight, setGridHeight] = useState(1);
 
   useLayoutEffect(() => {
-    const { ResizeObserver } = window as typeof window & { ResizeObserver: ResizeObserver };
+    const { ResizeObserver } = window;
 
     // don't break in jest/jsdom and browsers that don't support ResizeObserver
     if (ResizeObserver == null) return;
@@ -37,5 +24,5 @@ export function useGridDimensions(): [React.RefObject<HTMLDivElement>, number, n
     };
   }, []);
 
-  return [gridRef, gridWidth, gridHeight];
+  return [gridRef, gridWidth, gridHeight] as const;
 }
