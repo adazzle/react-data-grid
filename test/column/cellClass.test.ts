2bf31b6dc9d1eee8ee028322833686b482116829
Nicolas Stepien <stepien.nicolas@gmail.com>
2021-01-19 15:59:24 +0000

    Fix tests

--- test/column/cellClass.test.ts
+++ test/column/cellClass.test.ts
@@ -1,6 +1,6 @@
 import type { Column } from '../../src';
-import { cellClassname } from '../../src/style';
 import { setup, getCells } from '../utils';
+import { cellClassname } from '../../src/style';
 
 interface Row {
   id: number;
