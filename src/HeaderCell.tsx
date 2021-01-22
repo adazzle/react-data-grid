0169b375c54fbea30118a8092098e1606851e63d
Aman Mahajan <amahajan@freewheel.com>
2021-01-22 07:09:38 -0600

    Fix types

--- src/HeaderCell.tsx
+++ src/HeaderCell.tsx
@@ -32,9 +32,11 @@ function getAriaSort(sortDirection?: SortDirection) {
   }
 }
 
-type SharedHeaderRowProps<R, SR> = Pick<
-  HeaderRowProps<R, SR>,
-  'sortColumn' | 'sortDirection' | 'onSort' | 'allRowsSelected'
+type SharedHeaderRowProps<R, SR> = Pick<HeaderRowProps<R, SR>,
+  | 'sortColumn'
+  | 'sortDirection'
+  | 'onSort'
+  | 'allRowsSelected'
 >;
 
 export interface HeaderCellProps<R, SR> extends SharedHeaderRowProps<R, SR> {
@@ -61,8 +63,7 @@ export default function HeaderCell<R, SR>({
     const { right } = currentTarget.getBoundingClientRect();
     const offset = right - event.clientX;
 
-    if (offset > 11) {
-      // +1px to account for the border size
+    if (offset > 11) { // +1px to account for the border size
       return;
     }
 
@@ -72,8 +73,7 @@ export default function HeaderCell<R, SR>({
         onPointerUp();
         return;
       }
-      const width =
-        event.clientX + offset - currentTarget.getBoundingClientRect().left;
+      const width = event.clientX + offset - currentTarget.getBoundingClientRect().left;
       if (width > 0) {
         onResize(column, width);
       }
@@ -128,9 +128,7 @@ export default function HeaderCell<R, SR>({
     <div
       role="columnheader"
       aria-colindex={column.idx + 1}
-      aria-sort={
-        sortColumn === column.key ? getAriaSort(sortDirection) : undefined
-      }
+      aria-sort={sortColumn === column.key ? getAriaSort(sortDirection) : undefined}
       className={className}
       style={getCellStyle(column)}
       onPointerDown={column.resizable ? onPointerDown : undefined}
