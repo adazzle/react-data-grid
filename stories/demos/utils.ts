eaa8d91fb7fae0a8759547ecf500a74d18d7ef07
Aman Mahajan <amahajan@freewheel.com>
2021-04-09 15:19:58 -0500

    serialiseCellValue

--- stories/demos/utils.ts
+++ stories/demos/utils.ts
@@ -3,6 +3,14 @@ import type { ReactElement } from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
 import type { DataGridProps } from '../../src';
 
+function serialiseCellValue(value: unknown) {
+  if (typeof value === 'string') {
+    const formattedValue = value.replace(/"/g, '""');
+    return formattedValue.includes(',') ? `"${formattedValue}"` : formattedValue;
+  }
+  return value;
+}
+
 export function exportToCsv<R, SR>(gridElement: ReactElement<DataGridProps<R, SR>>, fileName: string) {
   const grid = document.createElement('content');
   grid.innerHTML = renderToStaticMarkup(cloneElement(gridElement, {
@@ -13,20 +21,18 @@ export function exportToCsv<R, SR>(gridElement: ReactElement<DataGridProps<R, SR
   const gridRows = grid.querySelectorAll<HTMLDivElement>('.rdg-header-row, .rdg-row, .rdg-summary-row');
 
   for (const gridRow of gridRows) {
-    const gridCells = Array.from(
-      gridRow.querySelectorAll<HTMLDivElement>('.rdg-cell')
-    );
+    const gridCells = gridRow.querySelectorAll<HTMLDivElement>('.rdg-cell');
     const rows = [];
 
-    for (const gridCell of gridCells.slice(1)) {
-      rows.push(gridCell.innerText);
+    for (const gridCell of gridCells) {
+      rows.push(serialiseCellValue(gridCell.innerText));
     }
 
     csv.push(rows.join(','));
   }
 
 
-  const csvFile = new Blob([csv.join('\n')], { type: 'text/csv' });
+  const csvFile = new Blob([csv.join('\n')], { type: 'text/csv;charset=utf-8;' });
   const downloadLink = document.createElement('a');
   downloadLink.download = fileName;
   downloadLink.href = URL.createObjectURL(csvFile);
