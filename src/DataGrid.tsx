898d6894f2e21f0321c74a5efb255b26c8867cf0
Aman Mahajan <amahajan@freewheel.com>
2021-04-22 14:51:20 -0500

    Cleanup

--- src/DataGrid.tsx
+++ src/DataGrid.tsx
@@ -878,7 +878,6 @@ function DataGrid<R, SR>({
     const rowElements = [];
     let startRowIndex = 0;
     for (let rowIdx = rowOverscanStartIdx; rowIdx <= rowOverscanEndIdx; rowIdx++) {
-      gridRowStart++;
       const row = rows[rowIdx];
       if (isGroupRow(row)) {
         ({ startRowIndex } = row);
@@ -894,7 +893,7 @@ function DataGrid<R, SR>({
             viewportColumns={viewportColumns}
             childRows={row.childRows}
             rowIdx={rowIdx}
-            gridRowStart={gridRowStart}
+            gridRowStart={++gridRowStart}
             level={row.level}
             isExpanded={row.isExpanded}
             selectedCellIdx={selectedPosition.rowIdx === rowIdx ? selectedPosition.idx : undefined}
@@ -928,7 +927,7 @@ function DataGrid<R, SR>({
           isRowSelected={isRowSelected}
           onRowClick={onRowClick}
           rowClass={rowClass}
-          gridRowStart={gridRowStart}
+          gridRowStart={++gridRowStart}
           copiedCellIdx={copiedCell !== null && copiedCell.row === row ? columns.findIndex(c => c.key === copiedCell.columnKey) : undefined}
           draggedOverCellIdx={getDraggedOverCellIdx(rowIdx)}
           setDraggedOverRowIdx={isDragging ? setDraggedOverRowIdx : undefined}
@@ -970,6 +969,8 @@ function DataGrid<R, SR>({
     templateRows += ` repeat(${summaryRowsCount}, ${summaryRowHeight}px)`;
   }
 
+  const totalRowHeight = Math.max(rows.length * rowHeight, clientHeight) + headerRowHeight + summaryRowHeight * summaryRowsCount;
+
   return (
     <div
       role={hasGroups ? 'treegrid' : 'grid'}
@@ -986,7 +987,7 @@ function DataGrid<R, SR>({
         '--filter-row-height': `${headerFiltersHeight}px`,
         '--row-width': `${totalColumnWidth}px`,
         '--row-height': `${rowHeight}px`,
-        '--grid-height': `${Math.max(rows.length * rowHeight, clientHeight) + headerRowHeight + summaryRowHeight * summaryRowsCount}px`,
+        '--grid-height': `${totalRowHeight}px`,
         '--summary-row-height': `${summaryRowHeight}px`,
         '--template-rows': templateRows,
         ...layoutCssVars
