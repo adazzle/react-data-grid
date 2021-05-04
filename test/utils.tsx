737d50ea9174f9105446892d46bd500268b1c3b9
Aman Mahajan <amahajan@freewheel.com>
2021-05-04 08:44:52 -0500

    Fix typo

--- test/utils.tsx
+++ test/utils.tsx
@@ -34,7 +34,7 @@ export function getSelectedCell() {
 export function validateCellPosition(columnIdx: number, rowIdx: number) {
   const cell = getSelectedCell();
   if (cell === null) {
-    throw new Error('Selected cell now found');
+    throw new Error('Selected cell not found');
   }
   expect(cell).toHaveAttribute('aria-colindex', `${columnIdx + 1}`);
   expect(cell.parentNode).toHaveAttribute('aria-rowindex', `${rowIdx + 2}`);
