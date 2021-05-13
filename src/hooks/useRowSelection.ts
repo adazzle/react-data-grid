f7b5a1683595edf5e320ff08cacade1d68f1fe90
Aman Mahajan <amahajan@freewheel.com>
2021-05-13 11:51:26 -0500

    Cleanup useRowSelection: remove extra functions

--- src/hooks/useRowSelection.ts
+++ src/hooks/useRowSelection.ts
@@ -5,27 +5,18 @@ const RowSelectionContext = createContext<boolean | undefined>(undefined);
 
 export const RowSelectionProvider = RowSelectionContext.Provider;
 
-function useSelection() {
-  const context = useContext(RowSelectionContext);
-  if (context === undefined) {
-    throw new Error('useRowSelection must be used within a RowSelectionContext');
-  }
-  return context;
-}
-
 const RowSelectionChangeContext =
   createContext<((selectRowEvent: SelectRowEvent) => void) | undefined>(undefined);
 
 export const RowSelectionChangeProvider = RowSelectionChangeContext.Provider;
 
-function useRowSelectionChange() {
-  const context = useContext(RowSelectionChangeContext);
-  if (context === undefined) {
-    throw new Error('useRowSelectionChange must be used within a RowSelectionChangeContext');
-  }
-  return context;
-}
-
 export function useRowSelection(): readonly [boolean, (selectRowEvent: SelectRowEvent) => void] {
-  return [useSelection(), useRowSelectionChange()];
+  const rowSelectionContext = useContext(RowSelectionContext);
+  const rowSelectionChangeContext = useContext(RowSelectionChangeContext);
+
+  if (rowSelectionContext === undefined || rowSelectionChangeContext === undefined) {
+    throw new Error('useRowSelection must be used within DataGrid cells');
+  }
+
+  return [rowSelectionContext, rowSelectionChangeContext];
 }
