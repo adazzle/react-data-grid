0b4e1e7db50f9003b7e659b31da05088febe5007
Nicolas Stepien <567105+nstepien@users.noreply.github.com>
2021-05-21 20:16:07 +0100

    Add virtualization tests (#2413)
    * Virtualization tests
    
    * fix type issues
    
    * fix need to unmount
    
    * + 3 edge cases
    
    * +1 edge case
    
    * optimize getCellsAtRowIndex
    
    * query 0
    
    * +3 tests

--- test/utils.tsx
+++ test/utils.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react';
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import DataGrid from '../src/';
 import type { DataGridProps } from '../src/';
 
@@ -11,20 +11,38 @@ export function setup<R, SR, K extends React.Key>(props: DataGridProps<R, SR, K>
   );
 }
 
+export function getGrid() {
+  return screen.getByRole('grid');
+}
+
 export function getRows() {
   return screen.getAllByRole('row').slice(1);
 }
 
+export function queryRows() {
+  return screen.queryAllByRole('row').slice(1);
+}
+
 export function getCellsAtRowIndex(rowIdx: number) {
-  return within(getRows()[rowIdx]).getAllByRole('gridcell');
+  return Array.from(
+    document.querySelectorAll<HTMLDivElement>(`[aria-rowindex="${rowIdx + 2}"] > .rdg-cell`)
+  );
 }
 
 export function getCells() {
   return screen.getAllByRole('gridcell');
 }
 
+export function queryCells() {
+  return screen.queryAllByRole('gridcell');
+}
+
 export function getHeaderCells() {
   return screen.getAllByRole('columnheader');
+}
+
+export function queryHeaderCells() {
+  return screen.queryAllByRole('columnheader');
 }
 
 export function getSelectedCell() {
