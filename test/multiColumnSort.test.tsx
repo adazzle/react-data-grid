7c1b0f465884085b563ba77178763f3ca45fd6a5
Justine Mitchell <jmitch430@cable.comcast.com>
2021-05-26 09:14:09 -0500

    test refactor

--- test/multiColumnSort.test.tsx
+++ test/multiColumnSort.test.tsx
@@ -1,154 +1,54 @@
 import userEvent from '@testing-library/user-event';
 import { getCellsAtColumn, getHeaderCells } from './utils';
 import type { Column, SortColumn } from '../src/types';
-import { StrictMode, useMemo, useState } from 'react';
+import { StrictMode, useState } from 'react';
 import DataGrid from '../src';
 import { render, screen } from '@testing-library/react';
 
-interface Row {
-  id: number;
-  area: string;
-  assignee: string;
-  budget: number;
-  transaction: string;
-}
-const newRows: readonly Row[] = [
-  { id: 8, area: 'Creative', assignee: 'Phillip Kiehn', budget: 6877, transaction: 'invoice' },
-  {
-    id: 1,
-    area: 'Accountability',
-    assignee: 'Keith McGlynn',
-    budget: 2508,
-    transaction: 'deposit'
-  },
-  { id: 5, area: 'Brand', assignee: 'Brad Braun', budget: 8014, transaction: 'invoice' },
-  { id: 7, area: 'Brand', assignee: 'Janice Stehr', budget: 10913, transaction: 'payment' },
-  { id: 4, area: 'Accounts', assignee: 'Lynee Sawyer', budget: 3211, transaction: 'invoice' },
-  {
-    id: 2,
-    area: 'Accountability',
-    assignee: 'Michael Abshire',
-    budget: 5252,
-    transaction: 'invoice'
-  },
-  {
-    id: 3,
-    area: 'Accountability',
-    assignee: 'Luther Larkin',
-    budget: 1522,
-    transaction: 'deposit'
-  },
-  { id: 6, area: 'Brand', assignee: 'Tony Reinger', budget: 700, transaction: 'invoice' }
+const columns: readonly Column<SortColumn>[] = [
+  { key: 'columnKey', name: 'columnKey' },
+  { key: 'direction', name: 'direction' }
 ];
 
-const newColumns: readonly Column<Row>[] = [
-  { key: 'id', name: 'id' },
-  { key: 'area', name: 'area' },
-  { key: 'assignee', name: 'assignee' },
-  { key: 'budget', name: 'budget' },
-  { key: 'transaction', name: 'transaction' }
-];
-
-interface Props {
-  newRows: readonly Readonly<Row>[];
-  newColumns: readonly Column<Row>[];
-}
-
-const SortingContainer = ({ newRows, newColumns }: Props) => {
-  const [rows, setRows] = useState(newRows);
-  const [sortColumns, setSortColumns] = useState<SortColumn[]>([]);
-
-  const getComparator = (sortColumn: string) => {
-    switch (sortColumn) {
-      case 'assignee':
-      case 'area':
-      case 'transaction':
-        return function (a: Row, b: Row) {
-          return a[sortColumn].localeCompare(b[sortColumn]);
-        };
-      case 'id':
-      case 'budget':
-        return function (a: Row, b: Row) {
-          return a[sortColumn] - b[sortColumn];
-        };
-      default:
-        throw new Error(`unsupported sortColumn: "${sortColumn}"`);
-    }
-  };
-  const sortedRows: readonly Row[] = useMemo(() => {
-    if (sortColumns.length === 0) return rows;
-
-    const sortedRows: Row[] = [...rows];
-    sortedRows.sort((a: Row, b: Row) => {
-      for (const sort of sortColumns) {
-        const comparator = getComparator(sort.columnKey);
-        const compResult = comparator(a, b);
-        if (compResult !== 0) {
-          return sort.direction === 'ASC' ? compResult : -compResult;
-        }
-      }
-      return 0;
-    });
-    return sortedRows;
-  }, [rows, sortColumns]);
+function TestGrid() {
+  const [sortColumns, setSortColumns] = useState<readonly SortColumn[]>([]);
 
   return (
-    <DataGrid
-      columns={newColumns}
-      rows={sortedRows}
-      defaultColumnOptions={{
-        sortable: true,
-        resizable: true
-      }}
-      onRowsChange={setRows}
-      sortColumns={sortColumns}
-      onSortColumnsChange={setSortColumns}
-      className="fill-grid"
-    />
+    <>
+      <DataGrid
+        columns={columns}
+        rows={sortColumns}
+        defaultColumnOptions={{ sortable: true }}
+        sortColumns={sortColumns}
+        onSortColumnsChange={setSortColumns}
+      />
+      <div data-testid="sortColumnsValue">{JSON.stringify(sortColumns)}</div>
+    </>
   );
-};
+}
 
-function setupNewGrid(props: Props) {
+function setup() {
   return render(
     <StrictMode>
-      <SortingContainer {...props} />
+      <TestGrid />
     </StrictMode>
   );
 }
 
-test('single column sort', async () => {
-  setupNewGrid({ newRows, newColumns });
-  const grid = await screen.findByRole('grid');
-  const assigneeColumn = getCellsAtColumn({ grid, name: 'assignee' });
-
-  //Sort by Assignee - ASC
-  userEvent.click(getHeaderCells()[2].firstElementChild!);
-  expect(assigneeColumn[0]).toHaveTextContent('Brad Braun');
-  expect(assigneeColumn[1]).toHaveTextContent('Janice Stehr');
-  expect(assigneeColumn[2]).toHaveTextContent('Keith McGlynn');
+test('single column sort', () => {
+  setup();
+  userEvent.click(getHeaderCells()[1].firstElementChild!);
+  expect(JSON.parse(screen.getByTestId('sortColumnsValue').textContent)).toStrictEqual([
+    { columnKey: 'direction', direction: 'ASC' }
+  ]);
 });
 
-test('multi column sort', async () => {
-  setupNewGrid({ newRows, newColumns });
-  const grid = await screen.findByRole('grid');
-  const areaColumn = getCellsAtColumn({ grid, name: 'area' });
-  const transactionColumn = getCellsAtColumn({ grid, name: 'transaction' });
-  const budgetColumn = getCellsAtColumn({ grid, name: 'budget' });
-
-  //Sort by Area - ASC, Transaction - ASC, Budget - ASC
-  userEvent.click(getHeaderCells()[1].firstElementChild!);
-  userEvent.click(getHeaderCells()[4].firstElementChild!, { ctrlKey: true });
-  userEvent.click(getHeaderCells()[3].firstElementChild!, { ctrlKey: true });
-
-  expect(areaColumn[0]).toHaveTextContent('Accountability');
-  expect(areaColumn[1]).toHaveTextContent('Accountability');
-  expect(areaColumn[2]).toHaveTextContent('Accountability');
-
-  expect(transactionColumn[0]).toHaveTextContent('deposit');
-  expect(transactionColumn[1]).toHaveTextContent('deposit');
-  expect(transactionColumn[2]).toHaveTextContent('invoice');
-
-  expect(budgetColumn[0]).toHaveTextContent('1522');
-  expect(budgetColumn[1]).toHaveTextContent('2508');
-  expect(budgetColumn[2]).toHaveTextContent('5252');
+test('multi column sort', () => {
+  setup();
+  userEvent.click(getHeaderCells()[0].firstElementChild!);
+  userEvent.click(getHeaderCells()[1].firstElementChild!, { ctrlKey: true });
+  expect(JSON.parse(screen.getByTestId('sortColumnsValue').textContent)).toStrictEqual([
+    { columnKey: 'columnKey', direction: 'ASC' },
+    { columnKey: 'direction', direction: 'ASC' }
+  ]);
 });
