b2ccf16c37c5030cb01e2db10be85d5dcc76e656
Nicolas Stepien <stepien.nicolas@gmail.com>
2021-06-11 13:12:48 +0100

    remove mount logger

--- src/Row.tsx
+++ src/Row.tsx
@@ -1,4 +1,4 @@
-import { memo, forwardRef, useEffect } from 'react';
+import { memo, forwardRef } from 'react';
 import type { RefAttributes, CSSProperties } from 'react';
 import clsx from 'clsx';
 
@@ -46,13 +46,6 @@ function Row<R, SR>(
     rowClass?.(row),
     className
   );
-
-  useEffect(() => {
-    console.log('mount!');
-    return () => {
-      console.log('unmount!');
-    };
-  }, []);
 
   const cells = [];
 
