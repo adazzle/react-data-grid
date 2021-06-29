7eef4df2bd87c9085e2aa5192703d6d5877c69aa
Nicolas Stepien <567105+nstepien@users.noreply.github.com>
2021-06-29 13:10:39 +0100

    SortableHeaderCell: show sort arrow with svg (#2501)
    * SortableHeaderCell: show sort arrow with svg
    
    * Add aria-hidden

--- src/headerCells/SortableHeaderCell.tsx
+++ src/headerCells/SortableHeaderCell.tsx
@@ -1,5 +1,6 @@
 import { css } from '@linaria/core';
 import type { HeaderRendererProps } from '../types';
+
 const headerSortCell = css`
   cursor: pointer;
   display: flex;
@@ -16,10 +17,21 @@ const headerSortName = css`
 
 const headerSortNameClassname = `rdg-header-sort-name ${headerSortName}`;
 
+const arrow = css`
+  fill: currentColor;
+
+  > path {
+    transition: d 0.1s;
+  }
+`;
+
+const arrowClassname = `rdg-sort-arrow ${arrow}`;
+
 type SharedHeaderCellProps<R, SR> = Pick<
   HeaderRendererProps<R, SR>,
   'sortDirection' | 'onSort' | 'priority'
 >;
+
 interface Props<R, SR> extends SharedHeaderCellProps<R, SR> {
   children: React.ReactNode;
 }
@@ -30,18 +42,15 @@ export default function SortableHeaderCell<R, SR>({
   priority,
   children
 }: Props<R, SR>) {
-  let sortText = '';
-  if (sortDirection === 'ASC') {
-    sortText = '\u25B2';
-  } else if (sortDirection === 'DESC') {
-    sortText = '\u25BC';
-  }
-
   return (
     <span className={headerSortCellClassname} onClick={(e) => onSort(e.ctrlKey)}>
       <span className={headerSortNameClassname}>{children}</span>
       <span>
-        {sortText}
+        {sortDirection !== undefined && (
+          <svg viewBox="0 0 12 8" width="12" height="8" className={arrowClassname} aria-hidden>
+            <path d={sortDirection === 'ASC' ? 'M0 8 6 0 12 8' : 'M0 0 6 8 12 0'} />
+          </svg>
+        )}
         {priority}
       </span>
     </span>
