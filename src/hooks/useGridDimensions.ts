bbaa03ad071062ebc28fbd0e19f5855eeb865ca3
Nicolas Stepien <stepien.nicolas@gmail.com>
2021-07-06 13:54:44 +0100

    Only flushSync on the initial render

--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -17,19 +17,19 @@ export function useGridDimensions(): [
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (ResizeObserver == null) return;
 
-    const resizeObserver = new ResizeObserver(() => {
+    function saveDimensions() {
       // Get dimensions without scrollbars.
-      // The dimensions given by the callback entries in Firefox do not substract the scrollbar sizes.
+      // The dimensions given by the ResizeObserver callback entries in Firefox do not substract the scrollbar sizes.
       const { clientWidth, clientHeight } = gridRef.current!;
-      flushSync(() => {
-        // TODO: remove once fixed upstream
-        // we reduce width by 1px here to avoid layout issues in Chrome
-        // https://bugs.chromium.org/p/chromium/issues/detail?id=1206298
-        setGridWidth(clientWidth - (devicePixelRatio % 0.5 === 0 ? 0 : 1));
-        setGridHeight(clientHeight);
-      });
-    });
+      // TODO: remove once fixed upstream
+      // we reduce width by 1px here to avoid layout issues in Chrome
+      // https://bugs.chromium.org/p/chromium/issues/detail?id=1206298
+      setGridWidth(clientWidth - (devicePixelRatio % 0.5 === 0 ? 0 : 1));
+      setGridHeight(clientHeight);
+    }
 
+    flushSync(saveDimensions);
+    const resizeObserver = new ResizeObserver(saveDimensions);
     resizeObserver.observe(gridRef.current!);
 
     return () => {
