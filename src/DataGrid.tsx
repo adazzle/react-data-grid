fcd258cf08e6eb8ced4c0c91cfe93c509accadb9
Aman Mahajan <amahajan@stratag.com>
2021-07-28 13:56:43 -0400

    Move drag related functions to the DragHandle component (#2538)
    * Move drag related functions to the DragHandle component
    
    * Tweak onFill check
    
    * Remove ref and useEffect. Use previous value syntax to set state
    
    * Update src/DragHandle.tsx
    
    Co-authored-by: Nicolas Stepien <567105+nstepien@users.noreply.github.com>
    
    * Fix types
    
    * Remove if checks
    
    Co-authored-by: Nicolas Stepien <567105+nstepien@users.noreply.github.com>

--- src/DataGrid.tsx
+++ src/DataGrid.tsx
@@ -52,7 +52,7 @@ import type {
   RowHeightArgs
 } from './types';
 
-interface SelectCellState extends Position {
+export interface SelectCellState extends Position {
   readonly mode: 'SELECT';
 }
 
@@ -255,14 +255,13 @@ function DataGrid<R, SR, K extends Key>(
   );
   const [copiedCell, setCopiedCell] = useState<{ row: R; columnKey: string } | null>(null);
   const [isDragging, setDragging] = useState(false);
-  const [draggedOverRowIdx, setOverRowIdx] = useState<number | undefined>(undefined);
+  const [draggedOverRowIdx, setDraggedOverRowIdx] = useState<number | undefined>(undefined);
 
   /**
    * refs
    */
   const focusSinkRef = useRef<HTMLDivElement>(null);
   const prevSelectedPosition = useRef(selectedPosition);
-  const latestDraggedOverRowIdx = useRef(draggedOverRowIdx);
   const lastSelectedRowIdx = useRef(-1);
   const isCellFocusable = useRef(false);
 
@@ -345,9 +344,6 @@ function DataGrid<R, SR, K extends Key>(
   const hasGroups = groupBy.length > 0 && typeof rowGrouper === 'function';
   const minColIdx = hasGroups ? -1 : 0;
 
-  // Cell drag is not supported on a treegrid
-  const enableCellDragAndDrop = hasGroups ? false : onFill != null;
-
   /**
    * The identity of the wrapper function is stable so it won't break memoization
    */
@@ -417,11 +413,6 @@ function DataGrid<R, SR, K extends Key>(
     },
     [onColumnResize]
   );
-
-  const setDraggedOverRowIdx = useCallback((rowIdx?: number) => {
-    setOverRowIdx(rowIdx);
-    latestDraggedOverRowIdx.current = rowIdx;
-  }, []);
 
   /**
    * event handlers
@@ -659,80 +650,6 @@ function DataGrid<R, SR, K extends Key>(
         row,
         originalRow: row
       }));
-    }
-  }
-
-  function handleDragEnd() {
-    const overRowIdx = latestDraggedOverRowIdx.current;
-    if (overRowIdx === undefined || !onFill || !onRowsChange) return;
-
-    const { idx, rowIdx } = selectedPosition;
-    const sourceRow = rawRows[rowIdx];
-    const startRowIndex = rowIdx < overRowIdx ? rowIdx + 1 : overRowIdx;
-    const endRowIndex = rowIdx < overRowIdx ? overRowIdx + 1 : rowIdx;
-    const targetRows = rawRows.slice(startRowIndex, endRowIndex);
-    const column = columns[idx];
-    const updatedTargetRows = onFill({ columnKey: column.key, sourceRow, targetRows });
-    const updatedRows = [...rawRows];
-    const indexes: number[] = [];
-
-    for (let i = startRowIndex; i < endRowIndex; i++) {
-      const targetRowIdx = i - startRowIndex;
-      if (updatedRows[i] !== updatedTargetRows[targetRowIdx]) {
-        updatedRows[i] = updatedTargetRows[targetRowIdx];
-        indexes.push(i);
-      }
-    }
-
-    if (indexes.length > 0) {
-      onRowsChange(updatedRows, { indexes, column });
-    }
-    setDraggedOverRowIdx(undefined);
-  }
-
-  function handleMouseDown(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    if (event.buttons !== 1) return;
-    setDragging(true);
-    window.addEventListener('mouseover', onMouseOver);
-    window.addEventListener('mouseup', onMouseUp);
-
-    function onMouseOver(event: MouseEvent) {
-      // Trigger onMouseup in edge cases where we release the mouse button but `mouseup` isn't triggered,
-      // for example when releasing the mouse button outside the iframe the grid is rendered in.
-      // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/buttons
-      if (event.buttons !== 1) onMouseUp();
-    }
-
-    function onMouseUp() {
-      window.removeEventListener('mouseover', onMouseOver);
-      window.removeEventListener('mouseup', onMouseUp);
-      setDragging(false);
-      handleDragEnd();
-    }
-  }
-
-  function handleDoubleClick(event: React.MouseEvent<HTMLDivElement>) {
-    event.stopPropagation();
-    if (!onFill || !onRowsChange) return;
-
-    const { idx, rowIdx } = selectedPosition;
-    const sourceRow = rawRows[rowIdx];
-    const targetRows = rawRows.slice(rowIdx + 1);
-    const column = columns[idx];
-    const updatedTargetRows = onFill({ columnKey: column.key, sourceRow, targetRows });
-    const updatedRows = [...rawRows];
-    const indexes: number[] = [];
-
-    for (let i = rowIdx + 1; i < updatedRows.length; i++) {
-      const targetRowIdx = i - rowIdx - 1;
-      if (updatedRows[i] !== updatedTargetRows[targetRowIdx]) {
-        updatedRows[i] = updatedTargetRows[targetRowIdx];
-        indexes.push(i);
-      }
-    }
-
-    if (indexes.length > 0) {
-      onRowsChange(updatedRows, { indexes, column });
     }
   }
 
@@ -982,8 +899,17 @@ function DataGrid<R, SR, K extends Key>(
       onFocus: handleFocus,
       onKeyDown: handleKeyDown,
       selectedCellDragHandle:
-        enableCellDragAndDrop && isCellEditable(selectedPosition) ? (
-          <DragHandle onMouseDown={handleMouseDown} onDoubleClick={handleDoubleClick} />
+        // Cell drag is not supported on a treegrid
+        !hasGroups && onFill != null ? (
+          <DragHandle
+            rows={rawRows}
+            columns={columns}
+            selectedPosition={selectedPosition}
+            onRowsChange={onRowsChange}
+            onFill={onFill}
+            setDragging={setDragging}
+            setDraggedOverRowIdx={setDraggedOverRowIdx}
+          />
         ) : undefined
     };
   }
--- src/DragHandle.tsx
+++ src/DragHandle.tsx
@@ -1,5 +1,8 @@
 import { css } from '@linaria/core';
 
+import type { CalculatedColumn, FillEvent } from './types';
+import type { DataGridProps, SelectCellState } from './DataGrid';
+
 const cellDragHandle = css`
   cursor: move;
   position: absolute;
@@ -19,6 +22,102 @@ const cellDragHandle = css`
 
 const cellDragHandleClassname = `rdg-cell-drag-handle ${cellDragHandle}`;
 
-export default function DragHandle(props: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cellDragHandleClassname} {...props} />;
+interface Props<R, SR> extends Pick<DataGridProps<R, SR>, 'rows' | 'onRowsChange'> {
+  columns: readonly CalculatedColumn<R, SR>[];
+  selectedPosition: SelectCellState;
+  onFill: (event: FillEvent<R>) => R[];
+  setDragging: (isDragging: boolean) => void;
+  setDraggedOverRowIdx: React.Dispatch<React.SetStateAction<number | undefined>>;
+}
+
+export default function DragHandle<R, SR>({
+  rows,
+  columns,
+  selectedPosition,
+  onRowsChange,
+  onFill,
+  setDragging,
+  setDraggedOverRowIdx
+}: Props<R, SR>) {
+  function handleMouseDown(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    if (event.buttons !== 1) return;
+    setDragging(true);
+    window.addEventListener('mouseover', onMouseOver);
+    window.addEventListener('mouseup', onMouseUp);
+
+    function onMouseOver(event: MouseEvent) {
+      // Trigger onMouseup in edge cases where we release the mouse button but `mouseup` isn't triggered,
+      // for example when releasing the mouse button outside the iframe the grid is rendered in.
+      // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/buttons
+      if (event.buttons !== 1) onMouseUp();
+    }
+
+    function onMouseUp() {
+      window.removeEventListener('mouseover', onMouseOver);
+      window.removeEventListener('mouseup', onMouseUp);
+      setDragging(false);
+      handleDragEnd();
+    }
+  }
+
+  function handleDragEnd() {
+    setDraggedOverRowIdx((overRowIdx) => {
+      if (overRowIdx === undefined) return undefined;
+
+      const { idx, rowIdx } = selectedPosition;
+      const sourceRow = rows[rowIdx];
+      const startRowIndex = rowIdx < overRowIdx ? rowIdx + 1 : overRowIdx;
+      const endRowIndex = rowIdx < overRowIdx ? overRowIdx + 1 : rowIdx;
+      const targetRows = rows.slice(startRowIndex, endRowIndex);
+      const column = columns[idx];
+      const updatedTargetRows = onFill({ columnKey: column.key, sourceRow, targetRows });
+      const updatedRows = [...rows];
+      const indexes: number[] = [];
+
+      for (let i = startRowIndex; i < endRowIndex; i++) {
+        const targetRowIdx = i - startRowIndex;
+        if (updatedRows[i] !== updatedTargetRows[targetRowIdx]) {
+          updatedRows[i] = updatedTargetRows[targetRowIdx];
+          indexes.push(i);
+        }
+      }
+
+      if (indexes.length > 0) {
+        onRowsChange?.(updatedRows, { indexes, column });
+      }
+
+      return undefined;
+    });
+  }
+
+  function handleDoubleClick(event: React.MouseEvent<HTMLDivElement>) {
+    event.stopPropagation();
+    const { idx, rowIdx } = selectedPosition;
+    const sourceRow = rows[rowIdx];
+    const targetRows = rows.slice(rowIdx + 1);
+    const column = columns[idx];
+    const updatedTargetRows = onFill({ columnKey: column.key, sourceRow, targetRows });
+    const updatedRows = [...rows];
+    const indexes: number[] = [];
+
+    for (let i = rowIdx + 1; i < updatedRows.length; i++) {
+      const targetRowIdx = i - rowIdx - 1;
+      if (updatedRows[i] !== updatedTargetRows[targetRowIdx]) {
+        updatedRows[i] = updatedTargetRows[targetRowIdx];
+        indexes.push(i);
+      }
+    }
+
+    if (indexes.length > 0) {
+      onRowsChange?.(updatedRows, { indexes, column });
+    }
+  }
+
+  return (
+    <div
+      className={cellDragHandleClassname}
+      onMouseDown={handleMouseDown}
+      onDoubleClick={handleDoubleClick}
+    />
+  );
 }
