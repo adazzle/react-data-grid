bb01f984f46149fda09b6c4f276330b865d55bd0
Aman Mahajan <amahajan@freewheel.com>
2021-08-02 08:31:44 -0400

    Remove usused column

--- test/dragFill.test.tsx
+++ test/dragFill.test.tsx
@@ -7,23 +7,32 @@ import { render } from '@testing-library/react';
 import { getCellsAtRowIndex } from './utils';
 
 interface Row {
-  col1: number;
-  col2: string;
+  col: string;
 }
 
 const columns: readonly Column<Row>[] = [
   {
-    key: 'col1',
-    name: 'Col1',
-    editable: false
-  },
-  {
-    key: 'col2',
-    name: 'Col2',
-    editable: (row) => row.col1 !== 4,
+    key: 'col',
+    name: 'Col',
+    editable: (row) => row.col !== 'a4',
     editor() {
       return null;
     }
+  }
+];
+
+const initialRows: readonly Row[] = [
+  {
+    col: 'a1'
+  },
+  {
+    col: 'a2'
+  },
+  {
+    col: 'a3'
+  },
+  {
+    col: 'a4'
   }
 ];
 
@@ -36,26 +45,7 @@ function setup(allowDragFill = true) {
 }
 
 function DragFillTest({ allowDragFill = true }: { allowDragFill?: boolean }) {
-  const [rows, setRows] = useState((): readonly Row[] => {
-    return [
-      {
-        col1: 1,
-        col2: 'a1'
-      },
-      {
-        col1: 2,
-        col2: 'a2'
-      },
-      {
-        col1: 3,
-        col2: 'a3'
-      },
-      {
-        col1: 4,
-        col2: 'a4'
-      }
-    ];
-  });
+  const [rows, setRows] = useState(initialRows);
 
   function onFill({ columnKey, sourceRow, targetRow }: FillEvent<Row>): Row {
     return { ...targetRow, [columnKey]: sourceRow[columnKey as keyof Row] };
@@ -73,15 +63,15 @@ function DragFillTest({ allowDragFill = true }: { allowDragFill?: boolean }) {
 
 test('should not allow dragFill if onFill is undefined', () => {
   setup(false);
-  userEvent.click(getCellsAtRowIndex(0)[1]);
+  userEvent.click(getCellsAtRowIndex(0)[0]);
   expect(document.querySelector('.rdg-cell-drag-handle')).not.toBeInTheDocument();
 });
 
 test('should allow dragFill if onFill is specified', () => {
   setup();
-  userEvent.click(getCellsAtRowIndex(0)[1]);
+  userEvent.click(getCellsAtRowIndex(0)[0]);
   userEvent.dblClick(document.querySelector('.rdg-cell-drag-handle')!);
-  expect(getCellsAtRowIndex(1)[1]).toHaveTextContent('a1');
-  expect(getCellsAtRowIndex(2)[1]).toHaveTextContent('a1');
-  expect(getCellsAtRowIndex(3)[1]).toHaveTextContent('a4'); // readonly cell
+  expect(getCellsAtRowIndex(1)[0]).toHaveTextContent('a1');
+  expect(getCellsAtRowIndex(2)[0]).toHaveTextContent('a1');
+  expect(getCellsAtRowIndex(3)[0]).toHaveTextContent('a4'); // readonly cell
 });
