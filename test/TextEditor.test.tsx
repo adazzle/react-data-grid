6e6d1f85c0738f5447fe8c131ac7f5b0031a28ff
Nicolas Stepien <567105+nstepien@users.noreply.github.com>
2021-08-24 13:25:14 +0100

    Add TextEditor tests (#2599)
    * Add TextEditor tests
    
    * fix types

---
+++ test/TextEditor.test.tsx
@@ -0,0 +1,53 @@
+import { StrictMode, useState } from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import DataGrid, { TextEditor } from '../src';
+import type { Column } from '../src';
+import { getCells } from './utils';
+
+interface Row {
+  readonly name: string;
+}
+
+const columns: readonly Column<Row>[] = [{ key: 'name', name: 'Name', editor: TextEditor }];
+const initialRows: readonly Row[] = [{ name: 'Tacitus Kilgore' }];
+
+function Test() {
+  const [rows, setRows] = useState(initialRows);
+
+  return <DataGrid columns={columns} rows={rows} onRowsChange={setRows} />;
+}
+
+test('TextEditor', () => {
+  render(
+    <StrictMode>
+      <Test />
+    </StrictMode>
+  );
+
+  userEvent.dblClick(getCells()[0]);
+  let input: HTMLInputElement | null = screen.getByRole('textbox') as HTMLInputElement;
+  expect(input).toHaveClass('rdg-text-editor');
+  // input value is row[column.key]
+  expect(input).toHaveValue(initialRows[0].name);
+  // input is focused
+  expect(input).toHaveFocus();
+  // input value is fully selected
+  expect(input.selectionStart).toBe(0);
+  expect(input.selectionEnd).toBe(initialRows[0].name.length);
+
+  // pressing escape closes the editor without committing
+  userEvent.keyboard('Test{escape}');
+  expect(input).not.toBeInTheDocument();
+  input = null;
+  expect(getCells()[0]).toHaveTextContent(/^Tacitus Kilgore$/);
+
+  // blurring the input closes and commits the editor
+  userEvent.dblClick(getCells()[0]);
+  input = screen.getByRole('textbox') as HTMLInputElement;
+  userEvent.keyboard('Jim Milton');
+  fireEvent.blur(input);
+  expect(input).not.toBeInTheDocument();
+  expect(getCells()[0]).toHaveTextContent(/^Jim Milton$/);
+});
