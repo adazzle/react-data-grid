b80a4518a876feb104c2fb314afcaef8e2dca99e
Aman Mahajan <amahajan@freewheel.com>
2021-08-27 13:36:48 -0500

    Remove react import

--- stories/demos/HeaderFilters.tsx
+++ stories/demos/HeaderFilters.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useMemo, useState } from 'react';
+import { createContext, useContext, useMemo, useState } from 'react';
 import faker from 'faker';
 import { css } from '@linaria/core';
 
