9b8351b31970309e9931ea5c5909de12c85747fa
Aman Mahajan <amahajan@freewheel.com>
2022-02-04 09:27:21 -0600

    Export DataGridDefaultComponentsProvider

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default, type DataGridProps, type DataGridHandle } from './DataGrid';
+export { DataGridDefaultComponentsProvider } from './DataGridDefaultComponentsProvider';
 export { default as Row } from './Row';
 export * from './Columns';
 export * from './formatters';
