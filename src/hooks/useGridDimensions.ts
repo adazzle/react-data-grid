4860cd603d8f785c4cee16923fa869971c18fab8
Aman Mahajan <amahajan@freewheel.com>
2022-02-23 14:05:36 -0600

    Use contentBoxSize

--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -7,8 +7,8 @@ export function useGridDimensions(): [
   height: number
 ] {
   const gridRef = useRef<HTMLDivElement>(null);
-  const [gridWidth, setGridWidth] = useState(1);
-  const [gridHeight, setGridHeight] = useState(1);
+  const [inlineSize, setInlineSize] = useState(1);
+  const [blockSize, setBlockSize] = useState(1);
 
   useLayoutEffect(() => {
     const { ResizeObserver } = window;
@@ -17,15 +17,13 @@ export function useGridDimensions(): [
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (ResizeObserver == null) return;
 
-    const resizeObserver = new ResizeObserver(() => {
-      // Get dimensions without scrollbars.
-      // The dimensions given by the callback entries in Firefox do not substract the scrollbar sizes.
-      const { clientWidth, clientHeight } = gridRef.current!;
+    const resizeObserver = new ResizeObserver((entries) => {
+      const size = entries[0].contentBoxSize[0];
       // TODO: remove once fixed upstream
       // we reduce width by 1px here to avoid layout issues in Chrome
       // https://bugs.chromium.org/p/chromium/issues/detail?id=1206298
-      setGridWidth(clientWidth - (devicePixelRatio % 1 === 0 ? 0 : 1));
-      setGridHeight(clientHeight);
+      setInlineSize(size.inlineSize - (devicePixelRatio % 1 === 0 ? 0 : 1));
+      setBlockSize(size.blockSize);
     });
 
     resizeObserver.observe(gridRef.current!);
@@ -35,5 +33,5 @@ export function useGridDimensions(): [
     };
   }, []);
 
-  return [gridRef, gridWidth, gridHeight];
+  return [gridRef, inlineSize, blockSize];
 }
