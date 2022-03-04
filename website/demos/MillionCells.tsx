4ffc451081e553154a198d9ead67349859b36ffc
Aman Mahajan <amahajan@freewheel.com>
2022-03-04 15:47:38 -0600

    Revert example

--- website/demos/MillionCells.tsx
+++ website/demos/MillionCells.tsx
@@ -1,6 +1,6 @@
-import { useMemo, useRef, useState } from 'react';
+import { useMemo } from 'react';
 import DataGrid from '../../src';
-import type { Column, FormatterProps, DataGridHandle } from '../../src';
+import type { Column, FormatterProps } from '../../src';
 import type { Props } from './types';
 
 type Row = number;
@@ -15,9 +15,6 @@ function CellFormatter(props: FormatterProps<Row>) {
 }
 
 export default function MillionCells({ direction }: Props) {
-  const [value, setValue] = useState('10');
-  const [value1, setValue1] = useState('10');
-  const gridRef = useRef<DataGridHandle>(null);
   const columns = useMemo((): readonly Column<Row>[] => {
     const columns: Column<Row>[] = [];
 
@@ -36,37 +33,12 @@ export default function MillionCells({ direction }: Props) {
   }, []);
 
   return (
-    <>
-      <div style={{ marginBlockEnd: 5 }}>
-        <span style={{ marginInlineEnd: 5 }}>Row index: </span>
-        <input
-          style={{ inlineSize: 50 }}
-          type="number"
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
-        />
-        <button type="button" onClick={() => gridRef.current!.scrollToColumn(Number(value))}>
-          Scroll to column
-        </button>
-        <span style={{ marginInlineEnd: 5 }}>Row index: </span>
-        <input
-          style={{ inlineSize: 50 }}
-          type="number"
-          value={value1}
-          onChange={(event) => setValue1(event.target.value)}
-        />
-        <button type="button" onClick={() => gridRef.current!.scrollToRow(Number(value1))}>
-          Scroll to row
-        </button>
-      </div>
-      <DataGrid
-        ref={gridRef}
-        columns={columns}
-        rows={rows}
-        rowHeight={22}
-        className="fill-grid"
-        direction={direction}
-      />
-    </>
+    <DataGrid
+      columns={columns}
+      rows={rows}
+      rowHeight={22}
+      className="fill-grid"
+      direction={direction}
+    />
   );
 }
