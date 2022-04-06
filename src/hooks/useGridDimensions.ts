f96118d5388469edae7ef274e2ecb2a7b6ba9897
Aman Mahajan <amahajan@freewheel.com>
2022-04-06 11:19:03 -0500

    Use contentBoxSize

--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -7,8 +7,8 @@ export function useGridDimensions(): [
   height: number
 ] {
   const gridRef = useRef<HTMLDivElement>(null);
-  const [gridWidth, setGridWidth] = useState(1);
-  const [gridHeight, setGridHeight] = useState(1);
+  const [inlineSize, setInlineSize] = useState(1);
+  const [blockSize, setBlockSize] = useState(1);
 
   useLayoutEffect(() => {
     const { ResizeObserver } = window;
@@ -17,20 +17,18 @@ export function useGridDimensions(): [
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (ResizeObserver == null) return;
 
-    function saveDimensions() {
-      // Get dimensions without scrollbars.
-      // The dimensions given by the callback entries in Firefox do not substract the scrollbar sizes.
-      // https://bugzilla.mozilla.org/show_bug.cgi?id=1733042
-      const { clientWidth, clientHeight } = gridRef.current!;
+    const { clientWidth, clientHeight } = gridRef.current!;
+    setInlineSize(clientWidth - (devicePixelRatio % 1 === 0 ? 0 : 1));
+    setBlockSize(clientHeight);
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      const size = entries[0].contentBoxSize[0];
       // TODO: remove once fixed upstream
       // we reduce width by 1px here to avoid layout issues in Chrome
       // https://bugs.chromium.org/p/chromium/issues/detail?id=1206298
-      setGridWidth(clientWidth - (devicePixelRatio % 1 === 0 ? 0 : 1));
-      setGridHeight(clientHeight);
-    }
-
-    saveDimensions();
-    const resizeObserver = new ResizeObserver(saveDimensions);
+      setInlineSize(size.inlineSize - (devicePixelRatio % 1 === 0 ? 0 : 1));
+      setBlockSize(size.blockSize);
+    });
     resizeObserver.observe(gridRef.current!);
 
     return () => {
@@ -38,5 +36,5 @@ export function useGridDimensions(): [
     };
   }, []);
 
-  return [gridRef, gridWidth, gridHeight];
+  return [gridRef, inlineSize, blockSize];
 }
