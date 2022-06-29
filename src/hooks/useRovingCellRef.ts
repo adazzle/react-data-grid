64eaab7558c4a83ea9fa287e06a2aeaefdd22075
Wroud <wroud@yandex.ru>
2022-06-29 12:03:56 +0300

    Fix cell focus stealing

--- src/hooks/useRovingCellRef.ts
+++ src/hooks/useRovingCellRef.ts
@@ -1,32 +1,34 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
+import { useLayoutEffect } from './useLayoutEffect';
 import { scrollIntoView } from '../utils';
 
 // https://www.w3.org/TR/wai-aria-practices-1.1/#kbd_roving_tabindex
 export function useRovingCellRef(isSelected: boolean) {
   // https://www.w3.org/TR/wai-aria-practices-1.1/#gridNav_focus
   const [isChildFocused, setIsChildFocused] = useState(false);
+  const [cell, setRef] = useState<HTMLDivElement | null>(null);
 
   if (isChildFocused && !isSelected) {
     setIsChildFocused(false);
   }
-
-  const ref = useCallback((cell: HTMLDivElement | null) => {
-    if (cell === null) return;
-    scrollIntoView(cell);
-    if (cell.contains(document.activeElement)) return;
-    cell.focus({ preventScroll: true });
-  }, []);
 
   function onFocus(event: React.FocusEvent<HTMLDivElement>) {
     if (event.target !== event.currentTarget) {
       setIsChildFocused(true);
     }
   }
+  useLayoutEffect(() => {
+    if (isSelected && cell) {
+      scrollIntoView(cell);
+      if (cell.contains(document.activeElement)) return;
+      cell.focus({ preventScroll: true });
+    }
+  }, [isSelected, cell]);
 
   const isFocused = isSelected && !isChildFocused;
 
   return {
-    ref: isSelected ? ref : undefined,
+    ref: setRef,
     tabIndex: isFocused ? 0 : -1,
     onFocus: isSelected ? onFocus : undefined
   };
