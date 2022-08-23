48174a32b41cd4fbce7edc16027c3bba416170fc
Nicolas Stepien <stepien.nicolas@gmail.com>
2022-08-23 15:23:16 +0100

    fix rollup build

--- src/HeaderRow.tsx
+++ src/HeaderRow.tsx
@@ -31,14 +31,14 @@ const headerRow = css`
   background-color: var(--rdg-header-background-color);
   font-weight: bold;
 
-  > .${cell} {
+  & > .${cell} {
     /* Should have a higher value than 0 to show up above regular cells */
     z-index: 1;
     position: sticky;
     inset-block-start: 0;
   }
 
-  > .${cellFrozen} {
+  & > .${cellFrozen} {
     z-index: 2;
   }
 `;
