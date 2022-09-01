2161bdfb08aa10f734e21a1ddfac10300b761fa8
Aman Mahajan <amahajan@freewheel.com>
2022-09-01 16:28:11 -0500

    Fix zIndex

--- src/style/core.ts
+++ src/style/core.ts
@@ -104,6 +104,6 @@ export const viewportDraggingClassname = `rdg-viewport-dragging ${viewportDraggi
 export const focusSinkClassname = css`
   grid-column: 1/-1;
   pointer-events: none;
-  /* Should have a higher value than 3 to show up above header row */
-  z-index: 4;
+  /* Should have a higher value than 2 to show up above header row */
+  z-index: 3;
 `;
