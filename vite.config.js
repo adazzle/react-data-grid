e3286fb2a4bc6821839e4572546dc53c60068c0b
Aman Mahajan <amahajan@freewheel.tv>
2023-01-26 21:29:48 -0600

    Revert "@vitejs/plugin-react"
    This reverts commit 74895d616447e1db0ec3f823c5a9dfd1e29a3360.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import linaria from '@linaria/vite';
 import postcssNested from 'postcss-nested';
 
