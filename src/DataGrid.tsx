b0e582f730ce59cee5534aab256139d3dc4af236
Aman Mahajan <amahajan@freewheel.tv>
2023-02-10 09:47:34 -0600

    Fix EditCell

--- src/DataGrid.tsx
+++ src/DataGrid.tsx
@@ -987,9 +987,6 @@ function DataGrid<R, SR, K extends Key>(
         //
         // Otherwise commitEditorChanges may be called before the cell state is changed to
         // SELECT and this results in onRowChange getting called twice.
-        //
-        // Sometimes rows prop is changed before selectCell is called and this results in
-        // rows.indexOf(row) returning -1 so incorrect cell gets selected
         flushSync(() => {
           updateRow(column, selectedPosition.rowIdx, row);
           closeEditor();
