f4ac160e53048eaee895ddc176767834d73c69e0
Aman Mahajan <amahajan@freewheel.tv>
2023-05-03 22:04:36 -0500

    Update vite

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,12 +7,7 @@ const isCI = process.env.CI === 'true';
 
 // https://vitest.dev/config/
 export default defineConfig({
-  // https://github.com/vitest-dev/vitest/issues/3124#issuecomment-1505908243
-  optimizeDeps: {
-    exclude: ['vitest/utils'],
-    include: ['@vitest/utils']
-  },
-  plugins: [react({ fastRefresh: false }), linaria({ preprocessor: 'none' })],
+  plugins: [react(), linaria({ preprocessor: 'none' })],
   css: {
     postcss: {
       plugins: [postcssNested]
