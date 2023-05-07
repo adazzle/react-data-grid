792c4e44c50a666fd4f040a22288fc171f35f5cb
Aman Mahajan <amahajan@stratag.com>
2023-05-06 19:01:52 -0500

    Migrate from Webpack to Vite (#3135)
    * Initial commit
    
    * Add dev command
    
    * Add vitest
    
    * Fix a few tests
    
    * Update commands
    
    * Fix types?
    
    * Fix command
    
    * Add missing package
    
    * Trigger
    
    * Test
    
    * Remove eslint-plugin-jest rules
    
    * Move test check
    
    * threads: false
    
    * Does not work
    
    * Remove vitest
    
    * Use vite to build website
    
    * Enable sourcemap
    
    * Us vite for build website and lib
    
    * Remove webpack and rollup
    
    * format
    
    * Simplify babel config
    
    * Optimize clsx
    
    * Vite 4.1.0-beta.1, add back exports
    
    * vite 4.1.1
    
    * update linaria
    
    * Revert lazy routes
    
    * vite@4.1.2
    
    * Update vite
    
    * Remove html-webpack-plugin
    
    * vite@4.3.0
    
    * update vite
    
    * Update vite
    
    * Remove @linaria/babel-preset
    
    * update deps
    
    * Use `@linaria/rollup` instead
    
    * tweaks
    
    * add `--clearScreen false`
    
    * restore rollup
    
    * fix type issue
    
    ---------
    
    Co-authored-by: Nicolas Stepien <stepien.nicolas@gmail.com>
    Co-authored-by: Nicolas Stepien <567105+nstepien@users.noreply.github.com>

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import linaria from '@linaria/rollup';
+import react from '@vitejs/plugin-react';
+import postcssNested from 'postcss-nested';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'website',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true
+  },
+  resolve: {
+    alias: {
+      lodash: 'lodash-es'
+    }
+  },
+  plugins: [
+    react({
+      babel: {
+        babelrc: false,
+        configFile: false,
+        plugins: [['optimize-clsx', { functionNames: ['getCellClassname'] }]]
+      }
+    }),
+    linaria({ preprocessor: 'none' })
+  ],
+  css: {
+    postcss: {
+      plugins: [postcssNested]
+    }
+  },
+  server: {
+    open: true
+  }
+});
