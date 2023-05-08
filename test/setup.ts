f083ecb8cf74f85c24b9c4d329e4fb15c4299994
Aman Mahajan <amahajan@freewheel.tv>
2023-05-08 11:27:09 -0500

    Fix ssr test

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,13 @@
-import '@testing-library/jest-dom';
 import { act } from 'react-dom/test-utils';
+
+// Allow node-environment tests to properly fail when accessing DOM APIs,
+// as @testing-library/jest-dom may polyfill some DOM APIs like `window.CSS`
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (globalThis.window !== undefined) {
+  // TODO: use `await import()` instead of `require()`
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('@testing-library/jest-dom');
+}
 
 if (typeof window !== 'undefined') {
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
