bdd0a27a2f5598c0631b7f4a32888c6088803ec6
Aman Mahajan <amahajan@freewheel.tv>
2023-05-17 16:41:19 -0500

    early return

--- src/hooks/useFocusRef.ts
+++ src/hooks/useFocusRef.ts
@@ -3,9 +3,8 @@ import { useCallback } from 'react';
 export function useFocusRef<T extends HTMLOrSVGElement>(isSelected: boolean, isFocused: boolean) {
   const ref = useCallback(
     (el: T | null) => {
-      if (isFocused) {
-        el?.focus({ preventScroll: true });
-      }
+      if (el === null || !isFocused) return;
+      el.focus({ preventScroll: true });
     },
     [isFocused]
   );
