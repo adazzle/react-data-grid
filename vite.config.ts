fb09afc2cb2cbaf7c66adb72dc53b6d0cd2533ee
Aman Mahajan <amahajan@freewheel.tv>
2023-06-07 07:04:01 -0500

    Revert "try v8"
    This reverts commit 958bf48d7ea109b9b7da1f19f85e0896e456927b.

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     root: '.',
     globals: true,
     coverage: {
-      provider: 'v8',
+      provider: 'istanbul',
       enabled: isCI,
       include: ['src/**/*.{ts,tsx}', '!src/types.ts'],
       all: true,
