75562006b7b2a61ddac18e5969b732b3fd0333fe
Nicolas Stepien <stepien.nicolas@gmail.com>
2023-06-19 17:00:11 +0100

    fix flaky(?) renderEditCell test

--- test/column/renderEditCell.test.tsx
+++ test/column/renderEditCell.test.tsx
@@ -80,6 +80,9 @@ describe('Editor', () => {
     // await userEvent.click() triggers both mousedown and click, but without delay,
     // which isn't realistic, and isn't enough to trigger outside click detection
     await user.pointer([{ keys: '[MouseLeft>]', target: saveButton }]);
+    await new Promise((resolve) => {
+      requestAnimationFrame(resolve);
+    });
     await user.pointer({ keys: '[/MouseLeft]' });
 
     expect(onSave).toHaveBeenCalledTimes(1);
