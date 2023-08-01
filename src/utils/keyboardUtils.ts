ac0630c67828a6a62a3bc8cd3dd43cc454a3afd1
Nicolas Stepien <567105+nstepien@users.noreply.github.com>
2023-08-01 13:33:25 +0100

    Tweak `onEditorNavigation` and `getCellToScroll` (#3295)

--- src/utils/keyboardUtils.ts
+++ src/utils/keyboardUtils.ts
@@ -69,8 +69,9 @@ export function onEditorNavigation({ key, target }: React.KeyboardEvent<HTMLDivE
       target instanceof HTMLTextAreaElement ||
       target instanceof HTMLSelectElement)
   ) {
-    return target.matches(
-      '.rdg-editor-container > :only-child, .rdg-editor-container > label:only-child > :only-child, .rdg-editor-container > div:only-child > label:only-child > :only-child'
+    return (
+      target.closest('.rdg-editor-container')?.querySelectorAll('input, textarea, select')
+        .length === 1
     );
   }
   return false;
