85f55a4670cdbc268b210bfd169c3505133e9431
Aman Mahajan <amahajan@stratag.com>
2023-12-04 10:48:51 -0600

    Vitest 1 (#3397)
    * Vitest@1.0.0-beta.6
    
    * Update JSDOM, add node types
    
    * Option `coverage.all` is now enabled by default
    
    * update deps

--- vite.config.ts
+++ vite.config.ts
@@ -38,16 +38,19 @@ export default defineConfig({
   test: {
     root: '.',
     environment: 'jsdom',
-    experimentalVmThreads: true,
+    pool: 'vmThreads',
     globals: true,
     coverage: {
       provider: 'v8',
       enabled: isCI,
       include: ['src/**/*.{ts,tsx}', '!src/types.ts'],
-      all: true,
       reporter: ['text', 'json']
     },
-    useAtomics: true,
+    poolOptions: {
+      vmThreads: {
+        useAtomics: true
+      }
+    },
     testTimeout: isCI ? 10000 : 5000,
     setupFiles: ['test/setup.ts'],
     restoreMocks: true,
