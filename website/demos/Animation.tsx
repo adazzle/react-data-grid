8e4361a10a4259cd0301dbf340af743c4a7d0816
Aman Mahajan <amahajan@freewheel.tv>
2024-01-02 10:06:29 -0600

    No need to animate line height

--- website/demos/Animation.tsx
+++ website/demos/Animation.tsx
@@ -14,10 +14,6 @@ const rangeClassname = css`
 
 const transitionClassname = css`
   transition: grid-template-rows 0.5s ease;
-
-  > :is(.rdg-header-row, .rdg-row) {
-    transition: line-height 0.5s ease;
-  }
 `;
 
 interface Row {
