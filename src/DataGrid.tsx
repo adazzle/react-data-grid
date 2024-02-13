db4219f99b876b2cf52ae6071d6569b5064d3467
Aman Mahajan <amahajan@freewheel.tv>
2024-02-13 15:06:41 -0600

    Add `--rdg-total-height` variable

--- src/DataGrid.tsx
+++ src/DataGrid.tsx
@@ -339,7 +339,8 @@ function DataGrid<R, SR, K extends Key>(
    */
   const isTreeGrid = role === 'treegrid';
   const headerRowsHeight = headerRowsCount * headerRowHeight;
-  const clientHeight = gridHeight - headerRowsHeight - summaryRowsCount * summaryRowHeight;
+  const summaryRowsHeight = summaryRowsCount * summaryRowHeight;
+  const clientHeight = gridHeight - headerRowsHeight - summaryRowsHeight;
   const isSelectable = selectedRows != null && onSelectedRowsChange != null;
   const isRtl = direction === 'rtl';
   const leftKey = isRtl ? 'ArrowRight' : 'ArrowLeft';
@@ -412,6 +413,7 @@ function DataGrid<R, SR, K extends Key>(
   const maxColIdx = columns.length - 1;
   const selectedCellIsWithinSelectionBounds = isCellWithinSelectionBounds(selectedPosition);
   const selectedCellIsWithinViewportBounds = isCellWithinViewportBounds(selectedPosition);
+  const totalHeight = headerRowHeight + totalRowHeight + summaryRowsHeight;
 
   /**
    * The identity of the wrapper function is stable so it won't break memoization
@@ -1068,6 +1070,7 @@ function DataGrid<R, SR, K extends Key>(
           gridTemplateRows: templateRows,
           '--rdg-header-row-height': `${headerRowHeight}px`,
           '--rdg-summary-row-height': `${summaryRowHeight}px`,
+          '--rdg-total-height': `${totalHeight}px`,
           '--rdg-sign': isRtl ? -1 : 1,
           ...layoutCssVars
         } as unknown as React.CSSProperties
