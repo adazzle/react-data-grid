726c4f1aa6f39920a9195ab865014ed6ad3b1ad1
Fi1osof <n.lanets@newpg.ru>
2024-04-08 09:32:16 +0300

    Fix styles on last selected row in TreeDataGrid

--- src/DataGrid.tsx
+++ src/DataGrid.tsx
@@ -1191,7 +1191,10 @@ function DataGrid<R, SR, K extends Key>(
               selectedPosition.rowIdx
             ),
             [rowSelected]: isGroupRowFocused,
-            [rowSelectedWithFrozenCell]: isGroupRowFocused && lastFrozenColumnIndex !== -1
+            [rowSelectedWithFrozenCell]:
+              isGroupRowFocused &&
+              lastFrozenColumnIndex !== -1 &&
+              selectedPosition.rowIdx !== maxRowIdx
           })}
           style={{
             gridRowStart: selectedPosition.rowIdx + headerAndTopSummaryRowsCount + 1
