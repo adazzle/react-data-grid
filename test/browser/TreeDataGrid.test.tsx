25516150e74be50e14749fdf5233d54aa55cac56
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-06-04 07:44:18 -0300

    conditional props in test

--- test/browser/TreeDataGrid.test.tsx
+++ test/browser/TreeDataGrid.test.tsx
@@ -126,7 +126,7 @@ function TestGrid({ groupBy, generateGroupId }: TestGridProps) {
       onExpandedGroupIdsChange={setExpandedGroupIds}
       onRowsChange={setRows}
       onPaste={onPaste}
-      generateGroupId={generateGroupId}
+      {...(generateGroupId && { generateGroupId })}
     />
   );
 }
