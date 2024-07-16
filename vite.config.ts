b08c4bd86fc2c18baa20168a408e439bd4d0ff28
Aman Mahajan <amahajan@freewheel.tv>
2024-07-16 11:09:17 -0500

    Remove lodash

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 import type { BrowserCommand } from 'vitest/node';
 
 const isCI = process.env.CI === 'true';
-const isTest = process.env.NODE_ENV === 'test';
 
 // TODO: remove when `userEvent.pointer` is supported
 const resizeColumn: BrowserCommand<[resizeBy: number]> = async (context, resizeBy) => {
