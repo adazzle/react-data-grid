c5b8f74d934efba0c4b1a89fc1124170c869dc6c
Aman Mahajan <amahajan@freewheel.tv>
2024-10-24 13:33:59 -0500

    Add `eslint-plugin-react-hooks-extra`

--- eslint.config.js
+++ eslint.config.js
@@ -379,6 +379,10 @@ export default [
 
       // React Hooks Extra
       // https://eslint-react.xyz/
+      'react-hooks-extra/no-unnecessary-use-callback': 1,
+      'react-hooks-extra/no-unnecessary-use-memo': 1,
+      'react-hooks-extra/no-direct-set-state-in-use-effect': 1,
+      'react-hooks-extra/no-direct-set-state-in-use-layout-effect': 1,
       'react-hooks-extra/prefer-use-state-lazy-initialization': 1,
 
       // SonarJS rules
