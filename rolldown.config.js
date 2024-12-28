cc33d160e6ba681f0219d70dc78c26e03adfb365
Nicolas Stepien <stepien.nicolas@gmail.com>
2024-12-28 02:10:51 +0000

    use output array

--- rolldown.config.js
+++ rolldown.config.js
@@ -4,46 +4,38 @@ import wyw from '@wyw-in-js/rollup';
 import pkg from './package.json' with { type: 'json' };
 import { defineConfig } from 'rolldown';
 
-/**
- * TODO: https://github.com/rolldown/rolldown/issues/3200
- * @type {readonly import('rolldown').OutputOptions[]}
- */
-const outputs = [
-  {
-    dir: 'lib',
-    entryFileNames: 'bundle.js',
-    cssEntryFileNames: 'styles.css',
-    format: 'es',
-    sourcemap: true
-  },
-  {
-    dir: 'lib',
-    entryFileNames: 'bundle.cjs',
-    cssEntryFileNames: 'styles.css',
-    format: 'cjs',
-    sourcemap: true
-  }
-];
-
-export default outputs.map((output) =>
-  defineConfig({
-    input: './src/index.ts',
-    output,
-    platform: 'browser',
-    external: (id) => !id.startsWith('.') && !isAbsolute(id),
-    plugins: [
-      // @ts-expect-error
-      wyw({
-        preprocessor: 'none',
-        /**
-         * @param {string} hash
-         */
-        classNameSlug(hash) {
-          // We add the package version as suffix to avoid style conflicts
-          // between multiple versions of RDG on the same page.
-          return `${hash}${pkg.version.replaceAll('.', '-')}`;
-        }
-      })
-    ]
-  })
-);
+export default defineConfig({
+  input: './src/index.ts',
+  output: [
+    {
+      dir: 'lib',
+      entryFileNames: 'bundle.js',
+      cssEntryFileNames: 'styles.css',
+      format: 'es',
+      sourcemap: true
+    },
+    {
+      dir: 'lib',
+      entryFileNames: 'bundle.cjs',
+      cssEntryFileNames: 'styles.css',
+      format: 'cjs',
+      sourcemap: true
+    }
+  ],
+  platform: 'browser',
+  external: (id) => !id.startsWith('.') && !isAbsolute(id),
+  plugins: [
+    // @ts-expect-error
+    wyw({
+      preprocessor: 'none',
+      /**
+       * @param {string} hash
+       */
+      classNameSlug(hash) {
+        // We add the package version as suffix to avoid style conflicts
+        // between multiple versions of RDG on the same page.
+        return `${hash}${pkg.version.replaceAll('.', '-')}`;
+      }
+    })
+  ]
+});
