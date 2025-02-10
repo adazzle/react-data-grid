ed3ae9262c80194e3bf6a4a33989f55d7cf55b10
GitHub Action <action@github.com>
2025-02-10 12:50:11 +0000

    gh-pages deployment

--- assets/ResizableGrid.lazy-BQGNWwGS.js
+++
@@ -1,2 +0,0 @@
-import{c as t,u as s,j as i}from"./index-z6-w8YfF.js";import{D as n}from"./DataGrid-DSpf7usn.js";import{r as a}from"./renderCoordinates-BnaeBmsm.js";const f=t("/ResizableGrid")({component:l}),c=Array.from({length:100},(e,r)=>r),o=[];for(let e=0;e<50;e++){const r=String(e);o.push({key:r,name:r,renderCell:a})}function l(){const e=s();return i.jsx(n,{columns:o,rows:c,className:"fill-grid",style:{resize:"both"},direction:e})}export{f as Route};
-//# sourceMappingURL=ResizableGrid.lazy-BQGNWwGS.js.map
