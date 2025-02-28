51e0076c85a7ffac4f69a51d733d6fcffa99685d
GitHub Action <action@github.com>
2025-02-28 23:41:32 +0000

    gh-pages deployment

--- assets/MillionCells-DsDojo86.js
+++
@@ -1,2 +0,0 @@
-import{u as t,j as n}from"./index-BHsmfXQu.js";import{D as s}from"./DataGrid-D0KgVg7Z.js";import{r as i}from"./renderCoordinates-BnaeBmsm.js";const l=Array.from({length:1e3},(r,o)=>o),e=[];for(let r=0;r<1e3;r++){const o=String(r);e.push({key:o,name:o,frozen:r<5,width:80,resizable:!0,renderCell:i})}const p=function(){const o=t();return n.jsx(s,{columns:e,rows:l,rowHeight:22,className:"fill-grid",direction:o})};export{p as component};
-//# sourceMappingURL=MillionCells-DsDojo86.js.map
