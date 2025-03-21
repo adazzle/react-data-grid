52b923724b449c82e2f24dba06d1b42bf874e7bc
Aman Mahajan <amahajan@freewheel.tv>
2025-03-21 16:46:22 -0500

    Add back max-width

--- src/utils/renderMeasuringCells.tsx
+++ src/utils/renderMeasuringCells.tsx
@@ -11,11 +11,11 @@ const measuringCellClassname = css`
 `;
 
 export function renderMeasuringCells<R, SR>(viewportColumns: readonly CalculatedColumn<R, SR>[]) {
-  return viewportColumns.map(({ key, idx, minWidth }) => (
+  return viewportColumns.map(({ key, idx, minWidth, maxWidth }) => (
     <div
       key={key}
       className={measuringCellClassname}
-      style={{ gridColumnStart: idx + 1, minWidth }}
+      style={{ gridColumnStart: idx + 1, minWidth, maxWidth }}
       data-measuring-cell-key={key}
     />
   ));
