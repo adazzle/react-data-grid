3d90516e9a8960b6bbe6d18a0009db690636b0f5
GitHub Action <action@github.com>
2025-04-01 15:52:06 +0000

    gh-pages deployment

--- assets/ResizableGrid-B2949_qe.js
+++
@@ -1,2 +0,0 @@
-import{u as e,j as s}from"./index-BQ0x45CO.js";import{D as n}from"./DataGrid-BK7QfkGi.js";import{r as i}from"./renderCoordinates-BnaeBmsm.js";const a=Array.from({length:100},(o,r)=>r),t=[];for(let o=0;o<50;o++){const r=String(o);t.push({key:r,name:r,renderCell:i})}const p=function(){const r=e();return s.jsx(n,{columns:t,rows:a,className:"fill-grid",style:{resize:"both"},direction:r})};export{p as component};
-//# sourceMappingURL=ResizableGrid-B2949_qe.js.map
