b62a059ee262b6052a446e2c5fb52b76f437285d
GitHub Action <action@github.com>
2025-04-10 18:57:54 +0000

    gh-pages deployment

---
+++ assets/CellExpanderFormatter-3YuST0ai.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-Dx6kd565.js";const o="c1tgi1fh";function c({tabIndex:r,expanded:s,onCellExpand:e}){function a(n){(n.key===" "||n.key==="Enter")&&(n.preventDefault(),e())}return t.jsx("div",{className:o,onClick:e,onKeyDown:a,children:t.jsx("span",{tabIndex:r,children:s?"▼":"▶"})})}export{c as C};
+//# sourceMappingURL=CellExpanderFormatter-3YuST0ai.js.map
