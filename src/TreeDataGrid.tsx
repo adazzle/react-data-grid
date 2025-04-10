f185b0ac3b324afc6fd9a2ac11d47553a66221ff
Aman Mahajan <amahajan@freewheel.tv>
2025-04-10 11:40:39 -0500

    Add space

--- src/TreeDataGrid.tsx
+++ src/TreeDataGrid.tsx
@@ -305,7 +305,7 @@ export function TreeDataGrid<R, SR = unknown, K extends Key = Key>({
         // Expand the current group row if it is focused and is in collapsed state
         (event.key === rightKey && !row.isExpanded))
     ) {
-      //prevent scrolling
+      // prevent scrolling
       event.preventDefault();
       event.preventGridDefault();
       toggleGroup(row.id);
