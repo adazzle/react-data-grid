6f54aaba7de49ea79274aeffc860b4e783e6bf56
GitHub Action <action@github.com>
2025-04-11 18:07:40 +0000

    gh-pages deployment

---
+++ assets/CellExpanderFormatter-_dDc2JYf.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-DGLC5Qqh.js";const o="c1tgi1fh";function c({tabIndex:r,expanded:s,onCellExpand:e}){function a(n){(n.key===" "||n.key==="Enter")&&(n.preventDefault(),e())}return t.jsx("div",{className:o,onClick:e,onKeyDown:a,children:t.jsx("span",{tabIndex:r,children:s?"▼":"▶"})})}export{c as C};
+//# sourceMappingURL=CellExpanderFormatter-_dDc2JYf.js.map
