1eda14b43498accfda715bb9e9dd46723ba4f543
Aman Mahajan <amahajan@freewheel.tv>
2025-04-28 18:09:04 -0500

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import './style/layers.css';
 
-import type {
+export {
+  DataGrid,
+  type DataGridProps,
+  type DataGridHandle,
+  type DefaultColumnOptions
+} from './DataGrid';
+export { TreeDataGrid, type TreeDataGridProps } from './TreeDataGrid';
+export { DataGridDefaultRenderersContext } from './DataGridDefaultRenderersContext';
+export { default as Row } from './Row';
+export { default as Cell } from './Cell';
+export * from './Columns';
+export * from './cellRenderers';
+export { default as textEditor } from './editors/textEditor';
+export { default as renderHeaderCell } from './renderHeaderCell';
+export { renderSortIcon, renderSortPriority } from './sortStatus';
+export { useRowSelection, useHeaderRowSelection } from './hooks';
+export type {
   CalculatedColumn,
   CalculatedColumnOrColumnGroup,
   CalculatedColumnParent,
@@ -39,59 +55,3 @@ import type {
   SortColumn,
   SortDirection
 } from './types';
-
-export {
-  DataGrid,
-  type DataGridProps,
-  type DataGridHandle,
-  type DefaultColumnOptions
-} from './DataGrid';
-export { TreeDataGrid, type TreeDataGridProps } from './TreeDataGrid';
-export { DataGridDefaultRenderersContext } from './DataGridDefaultRenderersContext';
-export { default as Row } from './Row';
-export { default as Cell } from './Cell';
-export * from './Columns';
-export * from './cellRenderers';
-export { default as textEditor } from './editors/textEditor';
-export { default as renderHeaderCell } from './renderHeaderCell';
-export { renderSortIcon, renderSortPriority } from './sortStatus';
-export { useRowSelection, useHeaderRowSelection } from './hooks';
-export type {
-  Column,
-  ColumnGroup,
-  ColumnOrColumnGroup,
-  ColumnWidth,
-  ColumnWidths,
-  CalculatedColumn,
-  CalculatedColumnParent,
-  CalculatedColumnOrColumnGroup,
-  RenderCellProps,
-  RenderSummaryCellProps,
-  RenderGroupCellProps,
-  RenderEditCellProps,
-  RenderHeaderCellProps,
-  CellRendererProps,
-  RenderRowProps,
-  RowsChangeData,
-  SelectHeaderRowEvent,
-  SelectRowEvent,
-  FillEvent,
-  SortDirection,
-  SortColumn,
-  ColSpanArgs,
-  RowHeightArgs,
-  RenderCheckboxProps,
-  RenderSortIconProps,
-  RenderSortPriorityProps,
-  RenderSortStatusProps,
-  Renderers,
-  CellPointerEvent,
-  CellPointerDownArgs,
-  CellMouseEvent,
-  CellClickArgs,
-  CellKeyDownArgs,
-  CellKeyboardEvent,
-  CellCopyArgs,
-  CellPasteArgs,
-  CellSelectArgs
-};
