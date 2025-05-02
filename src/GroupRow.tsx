b02f1e25cc13cdad9447f96fc24a87679bfb6d8d
Aman Mahajan <amahajan@freewheel.tv>
2025-05-02 16:17:35 -0500

    Use `onMouseDown`

--- src/GroupRow.tsx
+++ src/GroupRow.tsx
@@ -72,7 +72,7 @@ function GroupedRow<R, SR>({
           selectedCellIdx === -1 && rowSelectedClassname,
           className
         )}
-        onPointerDown={handleSelectGroup}
+        onMouseDown={handleSelectGroup}
         style={getRowStyle(gridRowStart)}
         {...props}
       >
