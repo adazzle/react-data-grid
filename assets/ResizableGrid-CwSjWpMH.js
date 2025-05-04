7bd1d541c42b25e8c8a0a8dad2122e9d3bfe82ec
GitHub Action <action@github.com>
2025-05-04 02:35:01 +0000

    gh-pages deployment

---
+++ assets/ResizableGrid-CwSjWpMH.js
@@ -0,0 +1,2 @@
+import{u as e,j as s}from"./index-CHWLqwqL.js";import{D as n}from"./DataGrid-DN-oQvCc.js";import{r as i}from"./renderCoordinates-BnaeBmsm.js";const a=Array.from({length:100},(o,r)=>r),t=[];for(let o=0;o<50;o++){const r=String(o);t.push({key:r,name:r,renderCell:i})}const p=function(){const r=e();return s.jsx(n,{columns:t,rows:a,className:"fill-grid",style:{resize:"both"},direction:r})};export{p as component};
+//# sourceMappingURL=ResizableGrid-CwSjWpMH.js.map
