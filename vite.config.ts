256839edf2900098c04d6a392ededb7599176558
amahajan <amahajan@freewheel.tv>
2025-05-16 11:35:33 -0500

    increate testTimeout

--- vite.config.ts
+++ vite.config.ts
@@ -82,7 +82,7 @@ export default defineConfig(({ command }) => ({
       include: ['src/**/*.{ts,tsx}'],
       reporter: ['json']
     },
-    testTimeout: isCI ? 10000 : 5000,
+    testTimeout: 20_000,
     restoreMocks: true,
     sequence: {
       shuffle: true
