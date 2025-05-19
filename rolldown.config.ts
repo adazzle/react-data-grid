720a2538c09882a5e1fe4b1007c26f551c708e57
Nicolas Stepien <567105+nstepien@users.noreply.github.com>
2025-05-19 18:23:59 +0100

    Update rolldown, adopt rolldown-plugin-dts (#3787)
    * Update rolldown, adopt rolldown-plugin-dts
    
    * js -> ts
    
    * rm this file
    
    * cleanup
    
    * cleanup
    
    * prettier

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,22 +1,23 @@
-// @ts-check
-import { isAbsolute } from 'node:path';
 import wyw from '@wyw-in-js/rollup';
-import pkg from './package.json' with { type: 'json' };
 import { defineConfig } from 'rolldown';
+import { dts } from 'rolldown-plugin-dts';
+
+import pkg from './package.json' with { type: 'json' };
 
 export default defineConfig({
   input: './src/index.ts',
   output: {
     dir: 'lib',
-    entryFileNames: 'bundle.js',
     cssEntryFileNames: 'styles.css',
     sourcemap: true
   },
   platform: 'browser',
-  external: (id) => !id.startsWith('.') && !isAbsolute(id),
   plugins: [
-    // @ts-expect-error
+    dts({
+      tsconfig: './tsconfig.lib.json'
+    }),
     wyw({
+      exclude: ['**/*.d.ts'],
       preprocessor: 'none',
       classNameSlug(/** @type {string} */ hash) {
         // We add the package version as suffix to avoid style conflicts
