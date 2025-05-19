2c7f48431317a39f68a0a811b160df2136c1cfbb
GitHub Action <action@github.com>
2025-05-19 17:26:11 +0000

    gh-pages deployment

--- assets/ResizableGrid-DnMOSm3_.js
+++
@@ -1,2 +0,0 @@
-import{u as e,j as s}from"./index-DWL4DTos.js";import{D as n}from"./DataGrid-Czk7R4Vl.js";import{r as i}from"./renderCoordinates-BnaeBmsm.js";const a=Array.from({length:100},(o,r)=>r),t=[];for(let o=0;o<50;o++){const r=String(o);t.push({key:r,name:r,renderCell:i})}const p=function(){const r=e();return s.jsx(n,{columns:t,rows:a,className:"fill-grid",style:{resize:"both"},direction:r})};export{p as component};
-//# sourceMappingURL=ResizableGrid-DnMOSm3_.js.map
