b7b77096050efc002773b2074bfdee37463ceaf5
GitHub Action <action@github.com>
2025-06-17 15:48:13 +0000

    gh-pages deployment

---
+++ assets/MillionCells-BRDSoI7Z.js
@@ -0,0 +1,2 @@
+import{u as t,j as n}from"./index-Cjv61NC9.js";import{D as s}from"./DataGrid-CyKO8ptL.js";import{r as i}from"./renderCoordinates-BnaeBmsm.js";const l=Array.from({length:1e3},(r,o)=>o),e=[];for(let r=0;r<1e3;r++){const o=String(r);e.push({key:o,name:o,frozen:r<5,width:80,resizable:!0,renderCell:i})}const p=function(){const o=t();return n.jsx(s,{columns:e,rows:l,rowHeight:22,className:"fill-grid",direction:o})};export{p as component};
+//# sourceMappingURL=MillionCells-BRDSoI7Z.js.map
