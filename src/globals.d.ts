9fa89bdbe2edb98269e1d6ccd07f7ac228f5ca52
amahajan <amahajan@freewheel.tv>
2025-09-04 10:15:58 -0500

    Remove extra fields

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,7 +1,6 @@
 declare module 'react' {
+  // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
   interface CSSProperties {
-    anchorName?: string;
-    positionAnchor?: string;
     [key: `--${string}`]: string | number | undefined;
   }
 }
