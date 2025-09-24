6c77084e77d8e14bfb2d2fa98fe6fb4b776988b4
amahajan <amahajan@freewheel.tv>
2025-09-24 13:24:29 -0500

    `useLayoutEffect` is not needed

--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -1,4 +1,4 @@
-import { useCallback, useLayoutEffect, useRef, useSyncExternalStore } from 'react';
+import { useCallback, useRef, useSyncExternalStore } from 'react';
 
 type Dimensions = [inlineSize: number, blockSize: number, horizontalScrollbarHeight: number];
 
@@ -12,24 +12,22 @@ export function useGridDimensions() {
   const gridRef = useRef<HTMLDivElement>(null);
   const dimensionsRef = useRef(defaultDimensions);
 
-  useLayoutEffect(() => {
-    const { clientWidth, clientHeight, offsetWidth, offsetHeight } = gridRef.current!;
+  const subscribe = useCallback((callback: () => void) => {
+    const element = gridRef.current!;
+    const { clientWidth, clientHeight, offsetWidth, offsetHeight } = element;
     const { width, height } = gridRef.current!.getBoundingClientRect();
     const initialHorizontalScrollbarHeight = offsetHeight - clientHeight;
     const initialWidth = width - offsetWidth + clientWidth;
     const initialHeight = height - initialHorizontalScrollbarHeight;
-
     dimensionsRef.current = [initialWidth, initialHeight, initialHorizontalScrollbarHeight];
-  }, []);
 
-  const subscribe = useCallback((callback: () => void) => {
     const resizeObserver = new ResizeObserver((entries) => {
       const size = entries[0].contentBoxSize[0];
       const { clientHeight, offsetHeight } = gridRef.current!;
       dimensionsRef.current = [size.inlineSize, size.blockSize, offsetHeight - clientHeight];
       callback();
     });
-    resizeObserver.observe(gridRef.current!);
+    resizeObserver.observe(element);
 
     return () => {
       resizeObserver.disconnect();
