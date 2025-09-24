2719e2b5f8fb594cb7e986545f02098474627ab9
amahajan <amahajan@freewheel.tv>
2025-09-24 09:38:13 -0500

    Check column order

--- test/browser/column/frozen.test.ts
+++ test/browser/column/frozen.test.ts
@@ -1,3 +1,5 @@
+import { page } from '@vitest/browser/context';
+
 import type { Column } from '../../../src';
 import { cellClassname, cellFrozenClassname } from '../../../src/style/cell';
 import { getHeaderCellsNew, setup } from '../utils';
@@ -26,14 +28,15 @@ test('frozen column have a specific class, and are stable-sorted before non-froz
   ];
 
   setup({ columns, rows: [] });
+  await expect.element(page.getByRole('row')).toHaveTextContent('col1col3col2col4');
   const [cell1, cell2, cell3, cell4] = getHeaderCellsNew('col1', 'col2', 'col3', 'col4');
 
   await expect
     .element(cell1)
     .toHaveClass(`${cellClassname} ${cellFrozenClassname}`, { exact: true });
-  await expect.element(cell2).toHaveClass(cellClassname, { exact: true });
   await expect
     .element(cell3)
     .toHaveClass(`${cellClassname} ${cellFrozenClassname}`, { exact: true });
+  await expect.element(cell2).toHaveClass(cellClassname, { exact: true });
   await expect.element(cell4).toHaveClass(cellClassname, { exact: true });
 });
