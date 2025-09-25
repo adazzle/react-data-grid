9006c1238089c971415708c5033ea10492accc1a
amahajan <amahajan@freewheel.tv>
2025-09-25 09:01:35 -0500

    Use a class to avoid reading ref duirng rendering

--- src/hooks/useGridDimensions.ts
+++ src/hooks/useGridDimensions.ts
@@ -1,8 +1,38 @@
-import { useCallback, useRef, useSyncExternalStore } from 'react';
+import { useCallback, useRef, useState, useSyncExternalStore } from 'react';
 
 type Dimensions = [inlineSize: number, blockSize: number, horizontalScrollbarHeight: number];
 
 const defaultDimensions: Dimensions = [1, 1, 0];
+
+class GridDimensions {
+  #dimensions = defaultDimensions;
+
+  subscribe(element: HTMLDivElement, callback: () => void) {
+    const { clientWidth, clientHeight, offsetWidth, offsetHeight } = element;
+    const { width, height } = element.getBoundingClientRect();
+    const initialHorizontalScrollbarHeight = offsetHeight - clientHeight;
+    const initialWidth = width - offsetWidth + clientWidth;
+    const initialHeight = height - initialHorizontalScrollbarHeight;
+    this.#dimensions = [initialWidth, initialHeight, initialHorizontalScrollbarHeight];
+
+    const resizeObserver = new ResizeObserver((entries) => {
+      const size = entries[0].contentBoxSize[0];
+      const { clientHeight, offsetHeight } = element;
+      this.#dimensions = [size.inlineSize, size.blockSize, offsetHeight - clientHeight];
+      callback();
+    });
+
+    resizeObserver.observe(element);
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }
+
+  getSnapshot() {
+    return this.#dimensions;
+  }
+}
 
 function getServerSnapshot() {
   return defaultDimensions;
@@ -10,33 +40,18 @@ function getServerSnapshot() {
 
 export function useGridDimensions() {
   const gridRef = useRef<HTMLDivElement>(null);
-  const dimensionsRef = useRef(defaultDimensions);
+  const [gridDimensions] = useState(() => new GridDimensions());
 
-  const subscribe = useCallback((callback: () => void) => {
-    const element = gridRef.current!;
-    const { clientWidth, clientHeight, offsetWidth, offsetHeight } = element;
-    const { width, height } = gridRef.current!.getBoundingClientRect();
-    const initialHorizontalScrollbarHeight = offsetHeight - clientHeight;
-    const initialWidth = width - offsetWidth + clientWidth;
-    const initialHeight = height - initialHorizontalScrollbarHeight;
-    dimensionsRef.current = [initialWidth, initialHeight, initialHorizontalScrollbarHeight];
-
-    const resizeObserver = new ResizeObserver((entries) => {
-      const size = entries[0].contentBoxSize[0];
-      const { clientHeight, offsetHeight } = element;
-      dimensionsRef.current = [size.inlineSize, size.blockSize, offsetHeight - clientHeight];
-      callback();
-    });
-    resizeObserver.observe(element);
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, []);
+  const subscribe = useCallback(
+    (callback: () => void) => {
+      return gridDimensions.subscribe(gridRef.current!, callback);
+    },
+    [gridDimensions]
+  );
 
   const getSnapShot = useCallback(() => {
-    return dimensionsRef.current;
-  }, []);
+    return gridDimensions.getSnapshot();
+  }, [gridDimensions]);
 
   const [inlineSize, blockSize, horizontalScrollbarHeight] = useSyncExternalStore(
     subscribe,
