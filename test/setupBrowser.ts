0d7acb25aa6047c7b14bdbcd7bb508d0eb93a29c
amahajan <amahajan@freewheel.tv>
2025-10-03 14:07:32 -0500

    Remove unused

--- test/setupBrowser.ts
+++ test/setupBrowser.ts
@@ -1,12 +1,11 @@
-import { configure as vitestReactConfigure } from 'vitest-browser-react/pure';
+import { locators } from '@vitest/browser/context';
+import { configure } from 'vitest-browser-react/pure';
 
 // vitest-browser-react also automatically injects render method on the page
 // need to import it so TypeScript can pick up types
 import 'vitest-browser-react';
 
-import { locators } from '@vitest/browser/context';
-
-vitestReactConfigure({
+configure({
   reactStrictMode: true
 });
 
